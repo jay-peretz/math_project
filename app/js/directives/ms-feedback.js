c16c066edd621a9ef18aaa79047f6e860939be2d
Noah Freitas <noah.j.freitas@gmail.com>
2013-04-01 11:51:25 -0400

    Add <ms-feedback>
    It provides feedback sayings in response to user answers.

---
+++ app/js/directives/ms-feedback.js
@@ -0,0 +1,57 @@
+'use strict';
+/*global angular */
+
+angular.module('mathSkills')
+    .directive('msFeedback', [
+        function () {
+            return {
+                controller: [
+                    '$scope',
+                    function ($scope) {
+                        // Sayings arrays.
+                        var correctSayings = [
+                                'Great Job!',
+                                'Groovy',
+                                'You\'re on a roll',
+                                'That\'s right!'
+                            ],
+                            incorrectSayings = [
+                                'Try again.',
+                                'Sorry',
+                                'That\'s incorrect.'
+                            ];
+
+                        // Place scope properties in one top level object,
+                        // since <ms-feedback> does not have isolate scope.
+                        // This prevents it from clobering over anything
+                        // placed on the scope by <ms-panel>.
+                        $scope.feedback = {
+                            message: 'Try again',
+                            labelClass: 'important',
+                            icon: 'remove',
+                            show: false
+                        };
+
+                        $scope.$on('answer', function (e, data) {
+                            // Show the feedback label.
+                            $scope.feedback.show = true;
+
+                            // Set the feedback label content.
+                            if (data.result === 'correct') {
+                                $scope.feedback.message = correctSayings[Math.floor(correctSayings.length * Math.random())];
+                                $scope.feedback.labelClass = 'success';
+                                $scope.feedback.icon = 'ok';
+                            } else {
+                                $scope.feedback.message = incorrectSayings[Math.floor(incorrectSayings.length * Math.random())];
+                                $scope.feedback.labelClass = 'important';
+                                $scope.feedback.icon = 'remove';
+                            }
+                        });
+                    }
+                ],
+                restrict: 'E',
+                scope: false,
+                templateUrl: 'partials/directives/ms-feedback.html'
+            };
+        }
+    ]);
