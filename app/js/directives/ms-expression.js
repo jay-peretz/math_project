273a787d84d6e811ccb242b7d0ea6f3f60cb6cd3
Noah Freitas <noah.j.freitas@gmail.com>
2013-04-05 16:30:40 -0400

    <ms-expression> updates data.label for answer events if null.

--- app/js/directives/ms-expression.js
+++ app/js/directives/ms-expression.js
@@ -10,6 +10,12 @@ angular.module('mathSkills')
                 controller: function ($scope, $element) {
                     // Let the parser determine our template.
                     directiveUtils.compileExpected($scope, $element, true);
+                    // Relabel any events.
+                    $scope.$on('answer', function (e, data) {
+                        if (data.label === undefined) {
+                            data.label = $scope.label;
+                        }
+                    });
                 },
                 restrict: 'E',
                 scope: {
