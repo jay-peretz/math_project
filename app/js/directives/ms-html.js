9b8380e95eb4571b2cd81ab6133082a2c99a5715
Justin Doolaege <justindool@sbcglobal.net>
2013-04-06 18:28:00 -0400

    added ms-html (\html{<div>some text<br><br>more text</div>})

---
+++ app/js/directives/ms-html.js
@@ -0,0 +1,68 @@
+'use strict';
+/*global angular */
+
+angular.module('mathSkills') 
+    .config(['parserProvider', function (parserProvider) {
+        parserProvider.register('html', {
+            directiveTemplate: '<ms-html expected={{expected}} label=html></ms-html>'
+        });
+    }])
+    .directive('msHtml', [
+        'parser',
+        '$compile',
+        function (parser, $compile) {
+            return {
+                controller: function ($scope, $element) {
+                    $scope.controllerId = Math.random().toString();
+                    
+                     // Extract the value and sent size event.
+                    $scope.$watch('expected', function () {
+                        if($scope.expected) {
+                            $scope.string = $scope.expected.slice(6, $scope.expected.length - 1); 
+                            // directiveUtils.size($scope, [$scope.string], 10, 5);    
+                        }
+                    });
+                     
+                    // handle check answer event for parent scope "always correct".
+                    $scope.$on('checkAnswer', function () {
+                        $scope.$emit('answer', {
+                            result: 'correct',
+                            expected: $scope.expected,
+                            answer: $scope.expected,
+                            label: $scope.label
+                        });
+                    });
+    
+                    $scope.$on('checkFocus', function (e) {
+                        // If this event has not been marked as ignored.
+                        if (e.defaultPrevented === false) {
+                            // handle check focus event for parent scope.
+                            // html/string cannot have focus/tab, fire a notFocused event.
+                            $scope.$emit('notFocused', {
+                                controllerId: $scope.controllerId
+                            });
+                        }
+                    });
+    
+                    $scope.$on('checkHelp', function (e) {
+                        // If this event has not been marked as ignored.
+                        if (e.defaultPrevented === false) {
+                            // handle check help event for parent scope.
+                            // string does not use help, fire a notHelped event.
+                            $scope.$emit('notHelped', {
+                                controllerId: $scope.controllerId
+                            }); 
+                        }
+                    });
+                   
+                },
+                restrict: 'E',
+                scope: {
+                    expected: '@', 
+                    label: '@'
+                },
+                replace: true, // replace template
+                template: '<div ng-bind-html-unsafe=string></div>'
+            };
+        }
+    ]);
