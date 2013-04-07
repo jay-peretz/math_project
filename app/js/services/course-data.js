f2a113bace1bd61a6273a677cc7e50f2c8f6d3a2
Justin Doolaege <justindool@sbcglobal.net>
2013-04-07 16:11:32 -0400

    finished 5.2- checkbox still needs a filter for feedback page"

--- app/js/services/course-data.js
+++ app/js/services/course-data.js
@@ -1706,10 +1706,10 @@ angular.module('mathSkills.services')
                                     {
                                         title: 'Choose the Factors of Whole Numbers',
                                         children: [{
-    										title: 'Main Answer',
+                                            title: 'Main Answer',
 											children: [{
-												problem: '\\html{Select the given divisors of 95 .}',
-												answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{F}}{\\chk{3}{F}}{\\chk{5}{T}}{\\chk{7}{F}}{\\chk{10}{F}}}{factors}',
+												problem: '\\html{Select the given divisors of 136 .}',
+												answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{T}}{\\chk{3}{F}}{\\chk{5}{F}}{\\chk{7}{F}}{\\chk{10}{F}}}{factors}',
 												controls: {
 													"checkAnswer": true,
 													"help": true
@@ -1717,8 +1717,118 @@ angular.module('mathSkills.services')
 											}]
 										}]
 									},
-                                    
-                                    
+                                    {
+                                        title: 'Choose the Factors of Whole Numbers',
+                                        children: [{
+                                            title: 'Main Answer',
+                                            children: [{
+												problem: '\\html{Select the given divisors of 190 .}',
+												answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{T}}{\\chk{3}{F}}{\\chk{5}{T}}{\\chk{7}{F}}{\\chk{10}{T}}}{factors}',
+												controls: {
+													"checkAnswer": true,
+													"help": true
+												}
+											}]
+										}]
+									},
+                                    {
+                                        title: 'Choose the Factors of Whole Numbers',
+                                        children: [{
+                                            title: 'Main Answer',
+                                            children: [{
+                                                problem: '\\html{Select the given divisors of 160 .}',
+												answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{T}}{\\chk{3}{F}}{\\chk{5}{T}}{\\chk{7}{F}}{\\chk{10}{T}}}{factors}',
+												controls: {
+													"checkAnswer": true,
+													"help": true
+												}
+											}]
+										}]
+									}, 
+                                    {                                    
+                                        title: 'Choose the Factors of Whole Numbers',
+                                        children: [{
+                                            title: 'Main Answer',
+                                            children: [{
+                                                problem: '\\html{Select the given divisors of 81 .}',
+                                                answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{F}}{\\chk{3}{T}}{\\chk{5}{F}}{\\chk{7}{F}}{\\chk{10}{F}}}{factors}',
+												controls: {
+													"checkAnswer": true,
+													"help": true
+												}
+											}]
+										}]
+									},
+                                    {                                    
+                                        title: 'Choose the Factors of Whole Numbers',
+                                        children: [{
+                                            title: 'Main Answer',
+                                            children: [{
+                                                problem: '\\html{Select the given divisors of 59 .}',
+                                                answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{F}}{\\chk{3}{F}}{\\chk{5}{F}}{\\chk{7}{F}}{\\chk{10}{F}}}{factors}',
+                                                controls: {
+													"checkAnswer": true,
+													"help": true
+												}
+											}]
+										}]
+									}, 
+                                    {                                    
+                                        title: 'Choose the Factors of Whole Numbers',
+                                        children: [{
+                                            title: 'Main Answer',
+                                            children: [{
+                                                problem: '\\html{Select the given divisors of 48 .}',
+                                                answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{T}}{\\chk{3}{T}}{\\chk{5}{F}}{\\chk{7}{F}}{\\chk{10}{F}}}{factors}',
+                                                controls: {
+                                                    "checkAnswer": true,
+													"help": true
+												}
+											}]
+										}]
+									}, 
+                                    {                                    
+                                        title: 'Choose the Factors of Whole Numbers',
+                                        children: [{
+                                            title: 'Main Answer',
+                                            children: [{
+                                                problem: '\\html{Select the given divisors of 145 .}',
+                                                answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{F}}{\\chk{3}{F}}{\\chk{5}{T}}{\\chk{7}{F}}{\\chk{10}{F}}}{factors}',
+                                                controls: {
+                                                    "checkAnswer": true,
+                                                    "help": true
+												}
+											}]
+										}]
+									}, 
+                                    {                                    
+                                        title: 'Choose the Factors of Whole Numbers',
+                                        children: [{
+                                            title: 'Main Answer',
+                                            children: [{
+                                                problem: '\\html{Select the given divisors of 60 .}',
+                                                answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{T}}{\\chk{3}{T}}{\\chk{5}{T}}{\\chk{7}{F}}{\\chk{10}{T}}}{factors}',
+                                                controls: {
+                                                    "checkAnswer": true,
+                                                    "help": true
+                                                }
+											}]
+										}]
+									}, 
+                                    {                                    
+                                        title: 'Choose the Factors of Whole Numbers',
+                                        children: [{
+                                            title: 'Main Answer',
+                                            children: [{
+                                                problem: '\\html{Select the given divisors of 94 .}',
+                                                answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{T}}{\\chk{3}{F}}{\\chk{5}{F}}{\\chk{7}{F}}{\\chk{10}{F}}}{factors}',
+                                                controls: {
+                                                    "checkAnswer": true,
+                                                    "help": true
+                                                }
+                                            }]
+										}]
+									},                                     
                                     
                                 ]
                             },
