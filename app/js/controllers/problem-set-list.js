a009dd946e8d232c8ac2bed071cebfd65c998e25
Noah Freitas <noah.j.freitas@gmail.com>
2013-04-08 10:52:27 -0400

    Divide problem set groups on problem set list page.

--- app/js/controllers/problem-set-list.js
+++ app/js/controllers/problem-set-list.js
@@ -9,5 +9,8 @@ angular.module('mathSkills')
         function (courseData, $routeParams, $scope) {
             $scope.unit = courseData.getUnitData($routeParams.unit);
             $scope.problemSets = $scope.unit.children;
+            $scope.divide = function (ii) {
+                return ii !== 0 && $scope.problemSets[ii].title.split('.')[0] !== $scope.problemSets[ii - 1].title.split('.')[0];
+            };
         }
     ]);
