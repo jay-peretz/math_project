62a3a9383b66e24f4e3f6501ca0e0c9a54985ba0
Justin Doolaege <justindool@sbcglobal.net>
2013-04-13 19:14:01 -0400

    added help to 5.1

--- app/js/services/course-data.js
+++ app/js/services/course-data.js
@@ -2090,11 +2090,11 @@ angular.module('mathSkills.services')
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\str{Is 47 divisible by 7?}',
+												problem: '\\str{Is 39 divisible by 10?}',
 												answer: '\\butgrp{\\row{\\but{YES}{F}}{\\but{NO}{T}}}',
 												controls: {
 													"checkAnswer": false,
-													"help": '\\str{47 is not divisible by 7.}'
+													"help": '\\html{Solution<br><br>The number 10 divides evenly into any whole number which ends in a 0. Because 39 does not end with a 0, it is not divisible by 10.}'
 												}
 											}]
 										}]
@@ -2104,11 +2104,11 @@ angular.module('mathSkills.services')
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\str{Is 26 divisible by 10?}',
+												problem: '\\str{Is 41 divisible by 10?}',
                                                 answer: '\\butgrp{\\row{\\but{YES}{F}}{\\but{NO}{T}}}',
 												controls: {
 													"checkAnswer": false,
-													"help": true
+													"help": '\\html{Solution<br><br>The number 10 divides evenly into any whole number which ends in a 0. Because 41 does not end with a 0, it is not divisible by 10.}'
 												}
 											}]
 										}]
@@ -2118,11 +2118,11 @@ angular.module('mathSkills.services')
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\str{Is 119 divisible by 10?}',
+												problem: '\\str{Is 9 divisible by 7?}',
                                                 answer: '\\butgrp{\\row{\\but{YES}{F}}{\\but{NO}{T}}}',
 												controls: {
 													"checkAnswer": false,
-													"help": true
+													"help": '\\html{Solution<br><br>9 is not divisible by 7.}'
 												}
 											}]
 										}]
@@ -2132,11 +2132,11 @@ angular.module('mathSkills.services')
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\str{Is 9 divisible by 2?}',
+												problem: '\\str{Is 173 divisible by 5?}',
                                                 answer: '\\butgrp{\\row{\\but{YES}{F}}{\\but{NO}{T}}}',
 												controls: {
 													"checkAnswer": false,
-													"help": true
+													"help": '\\html{Solution<br><br>The number 5 divides evenly into any whole number which ends in a 5 or a 0. Because 173 does not end with a 0 or 5, it is not divisible by 5.}'
 												}
 											}]
 										}]
@@ -2146,11 +2146,11 @@ angular.module('mathSkills.services')
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\str{Is 132 divisible by 3?}',
+												problem: '\\str{Is 28 divisible by 7?}',
                                                 answer: '\\butgrp{\\row{\\but{YES}{T}}{\\but{NO}{F}}}',
                                                 controls: {
 													"checkAnswer": false,
-													"help": true
+													"help": '\\html{Solution<br><br>28 is divisible by 7.}'
 												}
 											}]
 										}]
@@ -2160,25 +2160,11 @@ angular.module('mathSkills.services')
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\str{Is 85 divisible by 5?}',
-                                                answer: '\\butgrp{\\row{\\but{YES}{T}}{\\but{NO}{F}}}',
-												controls: {
-													"checkAnswer": false,
-													"help": true
-												}
-											}]
-										}]
-									},
-									{
-										title: 'Divisibility Test on Whole Numbers',
-										children: [{
-											title: 'Main Answer',
-											children: [{
-												problem: '\\str{Is 323 divisible by 5?}',
+												problem: '\\str{Is 275 divisible by 3?}',
                                                 answer: '\\butgrp{\\row{\\but{YES}{F}}{\\but{NO}{T}}}',
 												controls: {
 													"checkAnswer": false,
-													"help": true
+													"help": '\\html{Solution<br><br>The number 3 divides evenly into any whole number whose digits add up to a number divisible by 3. Because the digits of 275 add up to a number that is not divisible by 3, 275 is not divisible by 3.}'
 												}
 											}]
 										}]
@@ -2188,11 +2174,25 @@ angular.module('mathSkills.services')
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\str{Is 109 divisible by 7?}',
+												problem: '\\str{Is 47 divisible by 3?}',
                                                 answer: '\\butgrp{\\row{\\but{YES}{F}}{\\but{NO}{T}}}',
 												controls: {
 													"checkAnswer": false,
-													"help": true
+													"help": '\\html{Solution<br><br>The number 3 divides evenly into any whole number whose digits add up to a number divisible by 3. Because the digits of 47 add up to a number that is not divisible by 3, 47 is not divisible by 3.}'
+												}
+											}]
+										}]
+									},
+									{
+										title: 'Divisibility Test on Whole Numbers',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\str{Is 181 divisible by 5?}',
+                                                answer: '\\butgrp{\\row{\\but{YES}{F}}{\\but{NO}{T}}}',
+												controls: {
+													"checkAnswer": false,
+													"help": '\\html{Solution<br><br>The number 5 divides evenly into any whole number which ends in a 5 or a 0. Because 181 does not end with a 0 or 5, it is not divisible by 5.}'
 												}
 											}]
 										}]
@@ -2202,11 +2202,11 @@ angular.module('mathSkills.services')
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\str{Is 346 divisible by 3?}',
-                                                answer: '\\butgrp{\\row{\\but{YES}{F}}{\\but{NO}{T}}}',
+												problem: '\\str{Is 384 divisible by 2?}',
+                                                answer: '\\butgrp{\\row{\\but{YES}{T}}{\\but{NO}{F}}}',
 												controls: {
 													"checkAnswer": false,
-													"help": true
+													"help": '\\html{Solution<br><br>The number 2 divides evenly into any even number. Because 384 is even, 2 divides into it evenly. (Rember: an even number is a number whose one\'s place is a 0, 2, 4, 6, or 8).}'
                                                 }
                                             }]
                                         }]
@@ -2216,11 +2216,11 @@ angular.module('mathSkills.services')
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\str{Is 384 divisible by 2?}',
-                                                answer: '\\butgrp{\\row{\\but{YES}{T}}{\\but{NO}{F}}}',
+												problem: '\\str{Is 165 divisible by 2?}',
+                                                answer: '\\butgrp{\\row{\\but{YES}{F}}{\\but{NO}{T}}}',
 												controls: {
 													"checkAnswer": false,
-													"help": true
+													"help": '\\html{Solution<br><br>The number 2 divides evenly into any even number. Because 165 is odd, 2 does not divide into it evenly. (Rember: an even number is a number whose one\'s place is a 0, 2, 4, 6, or 8).}'
                                                 }
                                             }]
                                         }]
