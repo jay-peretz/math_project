490d46f5e9fa3de8e72aac0fe698e5311e76f0cb
markafoote <mark@getthatwebsite.com>
2013-04-14 21:01:51 -0700

    2A.2 problem 16 horizontal graph, partially implemented

---
+++ app/js/directives/ms-graph-horizontal.js
@@ -0,0 +1,96 @@
+'use strict';
+	
+angular.module('mathSkills') 
+	.config(['parserProvider', function (parserProvider) {
+        parserProvider.register('horgraph', {
+            argTemplates: [{
+                name: 'horgraphtitle'
+            },{
+                name: 'horgraphdata'
+            },{
+                name: 'horgraphvertical'
+            },{
+                name: 'horgraphbottom'
+            }],
+            directiveTemplate: '<ms-horgraph expected={{expected}}></ms-horgraph>'
+        });
+    }])
+  .directive('msHorgraph', [
+	'parser',
+	function (parser) {
+		return {
+			restrict: 'E',
+			scope: {
+				expected: '@',
+				label: '@'
+			},
+			controller: function ($scope, $element) {
+				$scope.answer = '';
+                $scope.controllerId = Math.random().toString();
+				$scope.answercorrect = "";
+				$scope.horgraphtitle = [];
+				$scope.graphTitleIn = "";
+				$scope.horgraphdata = [];
+				$scope.graphvertical = [];
+				$scope.horgraphbottom = [];
+				
+				var	horgraphdataIn = [];
+				
+			 // Extract the value for $scope.graphlinesIn
+				$scope.$watch('expected', function () {
+					if ($scope.expected) {
+						parser.parse($scope.expected, { scope: $scope });
+					}
+				});
+				
+				$scope.$watch('horgraphtitle', function () {
+					if (typeof $scope.horgraphtitle === "string") {
+						$scope.graphTitleIn = $scope.horgraphtitle;
+					}
+				});
+				
+				$scope.$watch('horgraphdata', function () {
+					if (typeof $scope.horgraphdata === "string") {
+						horgraphdataIn = JSON.parse($scope.horgraphdata);
+					}
+				});
+					
+				$scope.$watch('horgraphvertical', function () {
+					if (typeof $scope.horgraphvertical === "string") {
+						$scope.graphverticalIn = $scope.horgraphvertical;
+					}
+				});
+				
+				$scope.$watch('horgraphbottom', function () {
+					if (typeof $scope.horgraphbottom === "string") {
+						$scope.graphBottomText = $scope.horgraphbottom;
+					}
+									
+					function plotAccordingToChoices() {
+						var data = [];
+						
+						for (var ii = 0, len = horgraphdataIn.length; ii < len; ii += 1) {
+							data.push(horgraphdataIn[ii]);
+						}
+						
+						if (data.length > 0)
+							$.plot($("#placeholder"), data, {
+								xaxis: {
+									xaxis: { tickDecimals: 5 },
+									max: 50
+								},
+								 yaxis: { 
+								 	mode: "categories"
+								 }
+							});
+					}
+				
+					plotAccordingToChoices();
+
+							
+							
+				});
+			},
+			templateUrl: 'partials/directives/ms-graph.html'
+		};
+	}]);
