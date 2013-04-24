5a24b0d9137760e21014236f72d37f388d370c02
Noah Freitas <noah.j.freitas@gmail.com>
2013-04-24 15:00:48 -0400

    <ms-css> tests now pass.

--- test/unit/directives/ms-css.js
+++ test/unit/directives/ms-css.js
@@ -1,3 +1,5 @@
+/*global afterEach, angular, beforeEach, describe, expect, inject, it */
+
 describe('<ms-css>', function () {
     var element = {},
         elScope = {};
@@ -6,17 +8,16 @@ describe('<ms-css>', function () {
     
     beforeEach(inject(function ($rootScope, $compile) {
         var template = angular.element('<ms-css expected="\\css{\\str{stringTest}}{classTest}" label=css></ms-css>');
-        elScope =   $rootScope.$new();
-        element = $compile(template)(elScope);
+        element = $compile(template)($rootScope.$new());
 		jQuery("body").append(element);
         $rootScope.$digest();
     }));
+
 	afterEach(function() {
 		jQuery(element).remove();
 	});
     
     describe('when ms-css tag element is present', function () {
-
         it('should be add a class to the wrapper', inject(function ($rootScope) {
             expect(jQuery("ms-css > div").hasClass('classTest')).toBe(true);
         }));
@@ -26,12 +27,11 @@ describe('<ms-css>', function () {
 		}));
 		
 		it('it should decorate answer events coming up the scope chain', inject(function ($rootScope) {
-			$rootScope.$on('answer',function(e,data) {
+			$rootScope.$on('answer', function (e, data) {
 				expect(data.expected).toBe("\\css{\\str{stringTest}}{classTest}");
 				expect(data.answer).toBe("\\css{testanswer}{classTest}");
 			});
-			elScope.$new().$emit('answer',{answer:"testanswer"});
-			$rootScope.$digest();
+			$rootScope.$$childHead.$$childHead.$emit('answer', { answer: 'testanswer' });
 		}));
     });
 });
