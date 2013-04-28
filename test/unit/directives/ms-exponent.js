d664b2ddad95479d0be795c6246c9621d517752d
Justin Doolaege <justindool@sbcglobal.net>
2013-04-28 01:42:45 -0400

    ms-exponent unit test (needs some work)

--- test/unit/directives/ms-exponent.js
+++ test/unit/directives/ms-exponent.js
@@ -1,5 +1,29 @@
-/*global describe, it */
+/*global angular, beforeEach, describe, expect, inject, it, spyOn */
 
 describe('<ms-exponent>', function () {
-    it('should call four directiveUtils methods.');
+    var element;
+
+    beforeEach(module('mathSkills'));
+    
+    beforeEach(inject(function ($compile, $rootScope) {
+        var template = angular.element('<ms-exponent expected="\\exp{\\input{3}}{\\input{2}}"></ms-exponent>');
+        element = $compile(template)($rootScope.$new());
+        $rootScope.$digest();
+    }));
+
+    it('should compile', function () {
+        expect(element).toBeDefined();
+    });
+
+    it('should call four directiveUtils methods.', inject(function (directiveUtils) {
+        //spyOn(directiveUtils, 'aggregateChildAnswers');
+        //spyOn(directiveUtils, 'compileExpected');
+        // spyOn(directiveUtils, 'routeHelp');
+        // spyOn(directiveUtils, 'routeFocus');
+        
+        //expect(directiveUtils.aggregateChildAnswers).toHaveBeenCalled();
+        //expect(directiveUtils.compileExpected).toHaveBeenCalled();
+        // expect(directiveUtils.routeHelp).toHaveBeenCalled();
+        // expect(directiveUtils.routeFocus).toHaveBeenCalled();
+    }));
 });
