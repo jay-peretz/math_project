e6ea1dd5f95d92e5e32359208d55debad6ef7dff
Justin Doolaege <justindool@sbcglobal.net>
2013-04-28 14:20:07 -0400

    ms-event-label unit test (needs directivesUtils spyOn work)

--- test/unit/directives/ms-event-label.js
+++ test/unit/directives/ms-event-label.js
@@ -1,6 +1,32 @@
-/*global describe, it */
+/*global angular, beforeEach, describe, expect, inject, it, spyOn */
 
 describe('<ms-event-label>', function () {
-    it('should call directiveUtils.compileExpected');
-    it('should change the label property of the data answer event object to $scope.label');
+    var element;
+
+    beforeEach(module('mathSkills'));
+    
+    beforeEach(inject(function ($compile, $rootScope) {
+        var template = angular.element('<ms-event-label expected="\\frac{\\input{1}}{\\input{2}}" label="test"></ms-event-label>');
+        element = $compile(template)($rootScope.$new());
+        $rootScope.$digest();
+    }));
+
+    it('should compile', function () {
+        expect(element).toBeDefined();
+    });
+    
+     it('should change the label property of the data answer event object to $scope.label', inject(function ($rootScope) {
+        $rootScope.$on('answer', function (e, data) {
+                expect(data.label).toBe('test');
+            });
+
+            $rootScope.$broadcast('checkAnswer');
+            $rootScope.$digest();
+    }));
+
+    it('should call directiveUtils.compileExpected.', inject(function (directiveUtils, $rootScope) {
+        spyOn(directiveUtils, 'compileExpected');
+        
+        //expect(directiveUtils.compileExpected).toHaveBeenCalled();
+    }));
 });
