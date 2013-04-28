fafa19969059f9d7e2f8206f996c39432dfef2ae
Justin Doolaege <justindool@sbcglobal.net>
2013-04-27 22:03:16 -0400

    ms-checkboxlabel unit test

--- test/unit/directives/ms-checkboxlabel.js
+++ test/unit/directives/ms-checkboxlabel.js
@@ -1,6 +1,45 @@
-/*global describe, it */
+/*global angular, beforeEach, describe, expect, inject, it, jasmine */
 
 describe('<ms-checkboxlabel>', function () {
-    it('should check all true checkboxes.');
-    it('should not check false checkboxes.');
+    var element;
+
+    beforeEach(module('mathSkills'));
+
+    describe('when value should be unchecked', function () {
+        beforeEach(inject(function ($rootScope, $compile) {
+            var template = angular.element('<ms-checkboxlabel expected="\\chk{Test Label}{F}" label="checkbox"></ms-checkboxlabel>');
+            element = $compile(template)($rootScope.$new());
+            $rootScope.$digest();
+        }));
+        
+        it('should display the checkbox unchecked', function () {        
+            expect(jQuery(element).find('input').is(':checked')).toBe(false);
+        });
+    });
+    
+    describe('when value should be checked', function () {
+        beforeEach(inject(function ($rootScope, $compile) {
+            var template = angular.element('<ms-checkboxlabel expected="\\chk{Test Label}{T}" label="checkbox"></ms-checkboxlabel>');
+            element = $compile(template)($rootScope.$new());
+            $rootScope.$digest();
+        }));
+        
+        it('should display the checkbox checked', function () {
+            expect(jQuery(element).find('input').is(':checked')).toBe(true);
+        }); 
+    });
+    
+    describe('when exspected id undefined', function () {
+        
+        beforeEach(inject(function ($rootScope, $compile) {
+            var template = angular.element('<ms-checkboxlabel label="checkbox"></ms-checkboxlabel>');
+            element = $compile(template)($rootScope.$new());
+            $rootScope.$digest();
+        }));
+        
+        it('should compile without an expected attribute', function () {
+            expect(element).toBeDefined();
+        });
+        
+    });
 });
