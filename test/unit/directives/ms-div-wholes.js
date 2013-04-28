37a1ef747ab33169946d963699d00b1266dae11b
Justin Doolaege <justindool@sbcglobal.net>
2013-04-28 01:26:25 -0400

    ms-div-wholes unit test (needs some work)

--- test/unit/directives/ms-div-wholes.js
+++ test/unit/directives/ms-div-wholes.js
@@ -1,5 +1,29 @@
-/*global describe, it */
+/*global angular, beforeEach, describe, expect, inject, it, spyOn */
 
 describe('<ms-div-wholes>', function () {
-    it('should call four directiveUtils methods.');
+    var element;
+
+    beforeEach(module('mathSkills'));
+    
+    beforeEach(inject(function ($compile, $rootScope) {
+        var template = angular.element('<ms-div-wholes expected="\\divwholes{\\input{1}}{\\input{2}}"></ms-div-wholes>');
+        element = $compile(template)($rootScope.$new());
+        $rootScope.$digest();
+    }));
+
+    it('should compile', function () {
+        expect(element).toBeDefined();
+    });
+
+    it('should call four directiveUtils methods.', inject(function (directiveUtils) {
+        //spyOn(directiveUtils, 'aggregateChildAnswers');
+        //spyOn(directiveUtils, 'compileExpected');
+        // spyOn(directiveUtils, 'routeHelp');
+        // spyOn(directiveUtils, 'routeFocus');
+        
+        //expect(directiveUtils.aggregateChildAnswers).toHaveBeenCalled();
+        //expect(directiveUtils.compileExpected).toHaveBeenCalled();
+        // expect(directiveUtils.routeHelp).toHaveBeenCalled();
+        // expect(directiveUtils.routeFocus).toHaveBeenCalled();
+    }));
 });
