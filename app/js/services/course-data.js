bb41dc2ae0acbedf568a338d94a4c2b94ac2ca95
Justin Doolaege <justindool@sbcglobal.net>
2013-05-26 20:53:37 -0400

    9.1 Add and Subtract Like Fractions

--- app/js/services/course-data.js
+++ app/js/services/course-data.js
@@ -5890,11 +5890,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{4}}{\\str{6}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{8}}{\\str{12}}}',
-                                            answer: '\\butgrp{\\row{\\but{True}{T}}{\\but{False}{F}}}',
+                                            problem: '\\row{\\frac{\\str{2}}{\\str{4}}}{\\sign{-}}{\\frac{\\str{1}}{\\str{4}}',
+                                            answer: '\\grp{\\sign{=}}{\\frac{\\input{1}}{\\input{4}}}',
                                             controls: {
-                                                "checkAnswer": false,
-                                                "help": '\\css{\\rowgrp{\\html{4 · 12\xA0 = \xA08 · 6<br><br>}}{\\html{\xA0 48\xA0 = \xA048}}}{equivalent-fractions}'
+                                                "checkAnswer": true,
+                                                "help": '\\row{\\frac{\\str{2}}{\\str{4}}}{\\sign{-}}{\\frac{\\str{1}}{\\str{4}}}{\\sign{=}}{\\frac{\\str{1}}{\\str{4}}}'
                                             }
                                         }]
                                     }]
@@ -5904,11 +5904,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{14}}{\\str{16}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{20}}{\\str{35}}}',
-                                            answer: '\\butgrp{\\row{\\but{True}{F}}{\\but{False}{T}}}',
+                                            problem: '\\row{\\frac{\\str{6}}{\\str{8}}}{\\sign{-}}{\\frac{\\str{2}}{\\str{8}}',
+                                            answer: '\\grp{\\sign{=}}{\\frac{\\input{1}}{\\input{2}}}',
                                             controls: {
-                                                "checkAnswer": false,
-                                                "help": '\\css{\\rowgrp{\\html{14 · 35\xA0 \u2260 \xA020 · 16<br><br>}}{\\html{490\xA0 \u2260 \xA0320}}}{equivalent-fractions}'
+                                                "checkAnswer": true,
+                                                "help": '\\row{\\frac{\\str{6}}{\\str{8}}}{\\sign{-}}{\\frac{\\str{2}}{\\str{8}}}{\\sign{=}}{\\frac{\\str{1}}{\\str{2}}}'
                                             }
                                         }]
                                     }]
@@ -5918,11 +5918,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{14}}{\\str{16}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{16}}{\\str{36}}}',
-                                            answer: '\\butgrp{\\row{\\but{True}{F}}{\\but{False}{T}}}',
+                                            problem: '\\row{\\frac{\\str{6}}{\\str{9}}}{\\sign{\+}}{\\frac{\\str{2}}{\\str{9}}',
+                                            answer: '\\grp{\\sign{=}}{\\frac{\\input{8}}{\\input{9}}}',
                                             controls: {
-                                                "checkAnswer": false,
-                                                "help": '\\css{\\rowgrp{\\html{14 · 36\xA0 \u2260 \xA016 · 16<br><br>}}{\\html{504\xA0 \u2260 \xA0256}}}{equivalent-fractions}'
+                                                "checkAnswer": true,
+                                                "help": '\\row{\\frac{\\str{6}}{\\str{9}}}{\\sign{\+}}{\\frac{\\str{2}}{\\str{9}}}{\\sign{=}}{\\frac{\\str{8}}{\\str{9}}}'
                                             }
                                         }]
                                     }]
@@ -5932,11 +5932,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{6}}{\\str{9}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{10}}{\\str{15}}}',
-                                            answer: '\\butgrp{\\row{\\but{True}{T}}{\\but{False}{F}}}',
+                                            problem: '\\row{\\frac{\\str{15}}{\\str{25}}}{\\sign{\+}}{\\frac{\\str{9}}{\\str{25}}',
+                                            answer: '\\grp{\\sign{=}}{\\frac{\\input{24}}{\\input{25}}}',
                                             controls: {
-                                                "checkAnswer": false,
-                                                "help": '\\css{\\rowgrp{\\html{6 · 15\xA0 = \xA010 · 9<br><br>}}{\\html{90\xA0 = \xA090}}}{equivalent-fractions}'
+                                                "checkAnswer": true,
+                                                "help": '\\row{\\frac{\\str{15}}{\\str{25}}}{\\sign{\+}}{\\frac{\\str{9}}{\\str{25}}}{\\sign{=}}{\\frac{\\str{24}}{\\str{25}}}'
                                             }
                                         }]
                                     }]
@@ -5946,11 +5946,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{15}}{\\str{18}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{8}}{\\str{20}}}',
-                                            answer: '\\butgrp{\\row{\\but{True}{F}}{\\but{False}{T}}}',
+                                            problem: '\\row{\\frac{\\str{2}}{\\str{6}}}{\\sign{\+}}{\\frac{\\str{3}}{\\str{6}}',
+                                            answer: '\\grp{\\sign{=}}{\\frac{\\input{5}}{\\input{6}}}',
                                             controls: {
-                                                "checkAnswer": false,
-                                                "help": '\\css{\\rowgrp{\\html{15 · 20\xA0 \u2260 \xA08 · 18<br><br>}}{\\html{300\xA0 \u2260 \xA0144}}}{equivalent-fractions}'
+                                                "checkAnswer": true,
+                                                "help": '\\row{\\frac{\\str{2}}{\\str{6}}}{\\sign{\+}}{\\frac{\\str{3}}{\\str{6}}}{\\sign{=}}{\\frac{\\str{5}}{\\str{6}}}'
                                             }
                                         }]
                                     }]
@@ -5960,11 +5960,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{3}}{\\str{6}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{15}}{\\str{X}}}',
-                                            answer: '\\row{\\str{X \xA0= \xA0}}{\\input{30}}',
+                                            problem: '\\row{\\frac{\\str{12}}{\\str{13}}}{\\sign{-}}{\\frac{\\str{4}}{\\str{13}}',
+                                            answer: '\\grp{\\sign{=}}{\\frac{\\input{8}}{\\input{13}}}',
                                             controls: {
                                                 "checkAnswer": true,
-                                                "help": '\\css{\\rowgrp{\\html{3 · X\xA0 = \xA06 · 15<br><br>}}{\\html{X\xA0 = \xA0(6 · 15)\xA0 \xF7 \xA03<br><br>}}{\\html{X\xA0 = \xA030}}}{equivalent-fractions}'
+                                                "help": '\\row{\\frac{\\str{12}}{\\str{13}}}{\\sign{-}}{\\frac{\\str{4}}{\\str{13}}}{\\sign{=}}{\\frac{\\str{8}}{\\str{13}}}'
                                             }
                                         }]
                                     }]
@@ -5974,11 +5974,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{76}}{\\str{X}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{19}}{\\str{21}}}',
-                                            answer: '\\row{\\str{X \xA0= \xA0}}{\\input{84}}',
+                                            problem: '\\row{\\frac{\\str{28}}{\\str{42}}}{\\sign{-}}{\\frac{\\str{9}}{\\str{42}}',
+                                            answer: '\\grp{\\sign{=}}{\\frac{\\input{19}}{\\input{42}}}',
                                             controls: {
                                                 "checkAnswer": true,
-                                                "help": '\\css{\\rowgrp{\\html{76 · 21\xA0 = \xA0X · 19<br><br>}}{\\html{(76 · 21)\xA0 \xF7 \xA019 = X<br><br>}}{\\html{84\xA0 = \xA0X}}}{equivalent-fractions}'
+                                                "help": '\\row{\\frac{\\str{28}}{\\str{42}}}{\\sign{-}}{\\frac{\\str{9}}{\\str{42}}}{\\sign{=}}{\\frac{\\str{19}}{\\str{42}}}'
                                             }
                                         }]
                                     }]
@@ -5988,11 +5988,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{27}}{\\str{26}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{X}}{\\str{104}}}',
-                                            answer: '\\row{\\str{X \xA0= \xA0}}{\\input{108}}',
+                                            problem: '\\row{\\frac{\\str{40}}{\\str{66}}}{\\sign{-}}{\\frac{\\str{15}}{\\str{66}}',
+                                            answer: '\\grp{\\sign{=}}{\\frac{\\input{25}}{\\input{66}}}',
                                             controls: {
                                                 "checkAnswer": true,
-                                                "help": '\\css{\\rowgrp{\\html{27 · 104\xA0 = \xA026 · X<br><br>}}{\\html{(27 · 104)\xA0 \xF7 \xA026 =\xA0X<br><br>}}{\\html{108\xA0 = \xA0X}}}{equivalent-fractions}'
+                                                "help": '\\row{\\frac{\\str{40}}{\\str{66}}}{\\sign{-}}{\\frac{\\str{15}}{\\str{66}}}{\\sign{=}}{\\frac{\\str{25}}{\\str{66}}}'
                                             }
                                         }]
                                     }]
@@ -6002,11 +6002,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{25}}{\\str{45}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{75}}{\\str{X}}}',
-                                            answer: '\\row{\\str{X \xA0= \xA0}}{\\input{135}}',
+                                            problem: '\\row{\\frac{\\str{30}}{\\str{49}}}{\\sign{\+}}{\\frac{\\str{30}}{\\str{49}}',
+                                            answer: '\\grp{\\sign{=}}{\\mixed{\\input{1}}{\\frac{\\input{11}}{\\input{49}}}}',
                                             controls: {
                                                 "checkAnswer": true,
-                                                "help": '\\css{\\rowgrp{\\html{25 · X\xA0 = \xA045 · 75<br><br>}}{\\html{X\xA0 = \xA0(45 · 75)\xA0 \xF7 \xA025<br><br>}}{\\html{X\xA0 = \xA0135}}}{equivalent-fractions}'
+                                                "help": '\\row{\\frac{\\str{30}}{\\str{49}}}{\\sign{\+}}{\\frac{\\str{30}}{\\str{49}}}{\\sign{=}}{\\mixed{\\str{1}}{\\frac{\\str{11}}{\\str{49}}}}'
                                             }
                                         }]
                                     }]
@@ -6016,11 +6016,11 @@ title: 'Main Answer',
                                     children: [{
                                         title: 'Main Answer',
                                         children: [{
-                                            problem: '\\rowgrp{\\str{Are these fractions equivalent?}}{\\row{\\frac{\\str{19}}{\\str{38}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{X}}{\\str{152}}}',
-                                            answer: '\\row{\\str{X \xA0= \xA0}}{\\input{76}}',
+                                            problem: '\\row{\\frac{\\str{30}}{\\str{77}}}{\\sign{\+}}{\\frac{\\str{35}}{\\str{77}}',
+                                            answer: '\\grp{\\sign{=}}{\\frac{\\input{65}}{\\input{77}}}',
                                             controls: {
                                                 "checkAnswer": true,
-                                                "help": '\\css{\\rowgrp{\\html{19 · 152\xA0 = \xA038 · X<br><br>}}{\\html{(19 · 152)\xA0 \xF7 \xA038\xA0 = \xA0X<br><br>}}{\\html{76\xA0 = \xA0X}}}{equivalent-fractions}'
+                                                "help": '\\row{\\frac{\\str{30}}{\\str{77}}}{\\sign{\+}}{\\frac{\\str{35}}{\\str{77}}}{\\sign{=}}{\\frac{\\str{65}}{\\str{77}}}'
                                             }
                                         }]
                                     }]
