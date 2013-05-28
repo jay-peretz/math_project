31ab5b55dfc15b5f9cf51bd318196c1c79250408
markafoote <mark@getthatwebsite.com>
2013-05-28 08:24:40 -0700

    finish percents 18.2 change a decimal to a fraction

--- app/js/services/course-data.js
+++ app/js/services/course-data.js
@@ -8473,11 +8473,11 @@ title: 'Main Answer',
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{8.41}}}',
-												answer: '\\row{\\input{841}}{\\str{%}}',
+												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{0.6}}}',
+												answer: '\\row{\\input{60}}{\\str{%}}',
 												controls: {
 													"checkAnswer": true,
-													"help": '\\rowgrp{\\row{\\decimaltopercent{8.41}{100}}{\\css{\\str{Answer:  841%}}{help-answer-text help-answer-margin-right}}}'
+													"help": '\\rowgrp{\\row{\\decimaltopercent{0.6}{100}}{\\css{\\str{Answer:  60%}}{help-answer-text help-answer-margin-right}}}'
 												}
 											}]
 										}]
@@ -8487,11 +8487,11 @@ title: 'Main Answer',
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{8.41}}}',
-												answer: '\\row{\\input{841}}{\\str{%}}',
+												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{0.66}}}',
+												answer: '\\row{\\input{66}}{\\str{%}}',
 												controls: {
 													"checkAnswer": true,
-													"help": '\\rowgrp{\\row{\\decimaltopercent{8.41}{100}}{\\css{\\str{Answer:  841%}}{help-answer-text help-answer-margin-right}}}'
+													"help": '\\rowgrp{\\row{\\decimaltopercent{0.66}{100}}{\\css{\\str{Answer:  66%}}{help-answer-text help-answer-margin-right}}}'
 												}
 											}]
 										}]
@@ -8501,11 +8501,11 @@ title: 'Main Answer',
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{8.41}}}',
-												answer: '\\row{\\input{841}}{\\str{%}}',
+												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{0.54}}}',
+												answer: '\\row{\\input{54}}{\\str{%}}',
 												controls: {
 													"checkAnswer": true,
-													"help": '\\rowgrp{\\row{\\decimaltopercent{8.41}{100}}{\\css{\\str{Answer:  841%}}{help-answer-text help-answer-margin-right}}}'
+													"help": '\\rowgrp{\\row{\\decimaltopercent{0.54}{100}}{\\css{\\str{Answer:  54%}}{help-answer-text help-answer-margin-right}}}'
 												}
 											}]
 										}]
@@ -8515,11 +8515,11 @@ title: 'Main Answer',
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{8.41}}}',
-												answer: '\\row{\\input{841}}{\\str{%}}',
+												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{7.4}}}',
+												answer: '\\row{\\input{740}}{\\str{%}}',
 												controls: {
 													"checkAnswer": true,
-													"help": '\\rowgrp{\\row{\\decimaltopercent{8.41}{100}}{\\css{\\str{Answer:  841%}}{help-answer-text help-answer-margin-right}}}'
+													"help": '\\rowgrp{\\row{\\decimaltopercent{7.4}{100}}{\\css{\\str{Answer:  740%}}{help-answer-text help-answer-margin-right}}}'
 												}
 											}]
 										}]
@@ -8529,11 +8529,11 @@ title: 'Main Answer',
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{8.41}}}',
-												answer: '\\row{\\input{841}}{\\str{%}}',
+												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{0.7758}}}',
+												answer: '\\row{\\input{77.58}}{\\str{%}}',
 												controls: {
 													"checkAnswer": true,
-													"help": '\\rowgrp{\\row{\\decimaltopercent{8.41}{100}}{\\css{\\str{Answer:  841%}}{help-answer-text help-answer-margin-right}}}'
+													"help": '\\rowgrp{\\row{\\decimaltopercent{0.7758}{100}}{\\css{\\str{Answer:  77.58%}}{help-answer-text help-answer-margin-right}}}'
 												}
 											}]
 										}]
@@ -8543,11 +8543,11 @@ title: 'Main Answer',
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{8.41}}}',
-												answer: '\\row{\\input{841}}{\\str{%}}',
+												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{0.463}}}',
+												answer: '\\row{\\input{46.3}}{\\str{%}}',
 												controls: {
 													"checkAnswer": true,
-													"help": '\\rowgrp{\\row{\\decimaltopercent{8.41}{100}}{\\css{\\str{Answer:  841%}}{help-answer-text help-answer-margin-right}}}'
+													"help": '\\rowgrp{\\row{\\decimaltopercent{0.463}{100}}{\\css{\\str{Answer:  46.3%}}{help-answer-text help-answer-margin-right}}}'
 												}
 											}]
 										}]
@@ -8557,11 +8557,11 @@ title: 'Main Answer',
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{8.41}}}',
-												answer: '\\row{\\input{841}}{\\str{%}}',
+												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{0.8769}}}',
+												answer: '\\row{\\input{87.69}}{\\str{%}}',
 												controls: {
 													"checkAnswer": true,
-													"help": '\\rowgrp{\\row{\\decimaltopercent{8.41}{100}}{\\css{\\str{Answer:  841%}}{help-answer-text help-answer-margin-right}}}'
+													"help": '\\rowgrp{\\row{\\decimaltopercent{0.8769}{100}}{\\css{\\str{Answer:  87.69%}}{help-answer-text help-answer-margin-right}}}'
 												}
 											}]
 										}]
@@ -8571,11 +8571,11 @@ title: 'Main Answer',
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{8.41}}}',
-												answer: '\\row{\\input{841}}{\\str{%}}',
+												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{1.432}}}',
+												answer: '\\row{\\input{143.2}}{\\str{%}}',
 												controls: {
 													"checkAnswer": true,
-													"help": '\\rowgrp{\\row{\\decimaltopercent{8.41}{100}}{\\css{\\str{Answer:  841%}}{help-answer-text help-answer-margin-right}}}'
+													"help": '\\rowgrp{\\row{\\decimaltopercent{1.432}{100}}{\\css{\\str{Answer:  143.2%}}{help-answer-text help-answer-margin-right}}}'
 												}
 											}]
 										}]
@@ -8585,11 +8585,11 @@ title: 'Main Answer',
 										children: [{
 											title: 'Main Answer',
 											children: [{
-												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{8.41}}}',
-												answer: '\\row{\\input{841}}{\\str{%}}',
+												problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{0.5}}}',
+												answer: '\\row{\\input{50}}{\\str{%}}',
 												controls: {
 													"checkAnswer": true,
-													"help": '\\rowgrp{\\row{\\decimaltopercent{8.41}{100}}{\\css{\\str{Answer:  841%}}{help-answer-text help-answer-margin-right}}}'
+													"help": '\\rowgrp{\\row{\\decimaltopercent{0.5}{100}}{\\css{\\str{Answer:  50%}}{help-answer-text help-answer-margin-right}}}'
 												}
                                             }]
                                         }]
