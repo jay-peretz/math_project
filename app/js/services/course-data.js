6235ef3105a1c8afeb31f97903146931af432c06
markafoote <mark@getthatwebsite.com>
2013-06-02 16:25:58 -0700

    change 18.4 to 19.1 (whoops), start 19.2

--- app/js/services/course-data.js
+++ app/js/services/course-data.js
@@ -8743,8 +8743,8 @@ title: 'Main Answer',
                                         }]
                                     }]					
 							}, {
-                                title: '18.4 Change a Fraction to a Percent (directly)',
-                                path: '18.4-change-a-percent-to-a-decimal',
+                                title: '19.1 Change a Fraction to a Percent (directly)',
+                                path: '19.1-change-a-percent-to-a-decimal',
                                 children: [
                                     									{
 										title: 'Change a Fraction to a Percent (directly)',
@@ -8874,6 +8874,150 @@ title: 'Main Answer',
 									},
 									{
 										title: 'Change a Fraction to a Percent (directly)',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{2}}{\\str{9}}}',
+												answer: '\\row{\\mixed{\\input{22}}{\\frac{\\input{2}}{\\input{9}}}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\row{\\frac{\\str{2}}{\\str{9}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{22}}{\\frac{\\fracstr{2}}{\\fracstr{9}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{22}}{\\frac{\\fracstr{2}}{\\fracstr{9}}}}{label_like}}{css{\\str{%}}{label_like}}'
+												}
+                                            }]
+                                        }]
+                                    }]					
+							}, {
+                                title: '19.2 Change a Fraction to a Decimal, then to a  Percent',
+                                path: '19.2-change-a-fraction-to-a-decimal-then-to-a-percent',
+                                children: [
+                                    									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{4}}{\\str{5}}}',
+												answer: '\\row{\\input{80}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\rowgrp{\\row{\\frac{\\str{4}}{\\str{5}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{80%}}{\\css{\\str{Answer:  80%}}{help-answer-text help-answer-margin-right}}}'
+												}
+											}]
+										}]
+									},
+									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{1}}{\\str{10}}}',
+												answer: '\\row{\\input{10}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\rowgrp{\\row{\\frac{\\str{1}}{\\str{10}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{10%}}{\\css{\\str{Answer:  10%}}{help-answer-text help-answer-margin-right}}}'
+												}
+											}]
+										}]
+									},
+									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{49}}{\\str{50}}}',
+												answer: '\\row{\\input{98}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\rowgrp{\\row{\\frac{\\str{49}}{\\str{50}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{98%}}{\\css{\\str{Answer:  98%}}{help-answer-text help-answer-margin-right}}}'
+												}
+											}]
+										}]
+									},
+									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{9}}{\\str{5}}}',
+												answer: '\\row{\\input{180}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\rowgrp{\\row{\\frac{\\str{9}}{\\str{5}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{180%}}{\\css{\\str{Answer:  180%}}{help-answer-text help-answer-margin-right}}}'
+												}
+											}]
+										}]
+									},
+									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{16}}{\\str{10}}}',
+												answer: '\\row{\\input{160}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\rowgrp{\\row{\\frac{\\str{16}}{\\str{10}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{160%}}{\\css{\\str{Answer:  160%}}{help-answer-text help-answer-margin-right}}}'
+												}
+                                            }]
+                                        }]
+                                    },
+									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{6}}{\\str{7}}}',
+												answer: '\\row{\\mixed{\\input{85}}{\\frac{\\input{5}}{\\input{7}}}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\row{\\frac{\\str{6}}{\\str{7}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{85}}{\\frac{\\fracstr{5}}{\\fracstr{7}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{85}}{\\frac{\\fracstr{5}}{\\fracstr{7}}}}{label_like}}{css{\\str{%}}{label_like}}'
+												}
+											}]
+										}]
+									},
+									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{3}}{\\str{11}}}',
+												answer: '\\row{\\mixed{\\input{27}}{\\frac{\\input{3}}{\\input{11}}}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\row{\\frac{\\str{3}}{\\str{11}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{27}}{\\frac{\\fracstr{3}}{\\fracstr{11}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{27}}{\\frac{\\fracstr{3}}{\\fracstr{11}}}}{label_like}}{css{\\str{%}}{label_like}}'
+												}
+											}]
+										}]
+									},
+									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{2}}{\\str{3}}}',
+												answer: '\\row{\\mixed{\\input{66}}{\\frac{\\input{2}}{\\input{3}}}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\row{\\frac{\\str{2}}{\\str{3}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{66}}{\\frac{\\fracstr{2}}{\\fracstr{3}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{66}}{\\frac{\\fracstr{2}}{\\fracstr{3}}}}{label_like}}{css{\\str{%}}{label_like}}'
+												}
+											}]
+										}]
+									},
+									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
+										children: [{
+											title: 'Main Answer',
+											children: [{
+												problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{1}}{\\str{15}}}',
+												answer: '\\row{\\mixed{\\input{6}}{\\frac{\\input{2}}{\\input{3}}}}{\\str{%}}',
+												controls: {
+													"checkAnswer": true,
+													"help": '\\row{\\frac{\\str{1}}{\\str{15}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{6}}{\\frac{\\fracstr{2}}{\\fracstr{3}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{6}}{\\frac{\\fracstr{2}}{\\fracstr{3}}}}{label_like}}{css{\\str{%}}{label_like}}'
+												}
+											}]
+										}]
+									},
+									{
+										title: 'Change a Fraction to a Decimal, then to a  Percent',
 										children: [{
 											title: 'Main Answer',
 											children: [{
