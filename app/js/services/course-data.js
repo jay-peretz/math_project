57d80014af12a391f7037476d632b06d03223f34
Noah Freitas <noah.j.freitas@gmail.com>
2013-06-03 15:43:40 -0400

    Add 9.2 Find LCM.

--- app/js/services/course-data.js
+++ app/js/services/course-data.js
@@ -6026,7 +6026,251 @@ title: 'Main Answer',
                                     }]
                                 },
         					]
-						},{
+						}, {
+                            title: '9.2 Find the LCM',
+                            path: '9.2-find-the-lcm',
+                            children: [{
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{1}}{\\str{2}}}{\\str{ and }}{\\frac{\\str{2}}{\\str{4}}}',
+                                        answer: '\\input{4}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 2 and 4.}',
+                                        answer: '\\findlcm{2}{4}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }, {
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{4}}{\\str{6}}}{\\str{ and }}{\\frac{\\str{1}}{\\str{2}}}',
+                                        answer: '\\input{6}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 6 and 2.}',
+                                        answer: '\\findlcm{6}{2}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }, {
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{1}}{\\str{4}}}{\\str{ and }}{\\frac{\\str{3}}{\\str{8}}}',
+                                        answer: '\\input{8}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 4 and 8.}',
+                                        answer: '\\findlcm{4}{8}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }, {
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{5}}{\\str{6}}}{\\str{ and }}{\\frac{\\str{5}}{\\str{9}}}',
+                                        answer: '\\input{18}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 6 and 9.}',
+                                        answer: '\\findlcm{6}{9}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }, {
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{5}}{\\str{12}}}{\\str{ and }}{\\frac{\\str{5}}{\\str{18}}}',
+                                        answer: '\\input{36}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 12 and 18.}',
+                                        answer: '\\findlcm{12}{18}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }, {
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{5}}{\\str{10}}}{\\str{ and }}{\\frac{\\str{9}}{\\str{15}}}',
+                                        answer: '\\input{30}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 10 and 15.}',
+                                        answer: '\\findlcm{10}{15}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }, {
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{3}}{\\str{4}}}{\\str{ , }}{\\frac{\\str{7}}{\\str{8}}}{\\str{ and }}{\\frac{\\str{5}}{\\str{6}}',
+                                        answer: '\\input{24}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 4, 8, and 6.}',
+                                        answer: '\\findlcm{4}{8}{6}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }, {
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{8}}{\\str{6}}}{\\str{ , }}{\\frac{\\str{7}}{\\str{15}}}{\\str{ and }}{\\frac{\\str{3}}{\\str{5}}',
+                                        answer: '\\input{30}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 6, 15, and 5.}',
+                                        answer: '\\findlcm{6}{15}{5}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }, {
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{7}}{\\str{24}}}{\\str{ , }}{\\frac{\\str{4}}{\\str{6}}}{\\str{ and }}{\\frac{\\str{3}}{\\str{18}}',
+                                        answer: '\\input{72}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 24, 6, and 18.}',
+                                        answer: '\\findlcm{24}{6}{18}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }, {
+                                title: 'Find the LCM',
+                                children: [{
+                                    title: 'Main Answer',
+                                    children: [{
+                                        problem: '\\row{\\str{Find the LCM of }}{\\frac{\\str{4}}{\\str{15}}}{\\str{ , }}{\\frac{\\str{7}}{\\str{18}}}{\\str{ and }}{\\frac{\\str{9}}{\\str{9}}',
+                                        answer: '\\input{90}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false,
+                                            "workbook": true
+                                        }
+                                    }]
+                                }, {
+                                    title: 'Workbook',
+                                    children: [{
+                                        problem: '\\str{Find the LCM of 15, 18, and 9.}',
+                                        answer: '\\findlcm{15}{18}{9}',
+                                        controls: {
+                                            "checkAnswer": true,
+                                            "help": false
+                                        }
+                                    }]
+                                }]
+                            }]
+                        }, {
                             title: '9.3 Build an Equivalent Fraction',
                             path: '9.3-build-an-equivalent-fraction',
                             children: [{
