427fd95706135fbc0c37dc6587f2e06fcb5e6127
markafoote <mark@getthatwebsite.com>
2013-06-05 11:27:48 -0700

    fractions 10.3, first problem

--- app/data/data10_3.js
+++ app/data/data10_3.js
@@ -8,33 +8,33 @@ angular.module('mathSkills').value('data10_3', {
         children: [{
             title: 'Main Answer',
             children: [{
-                problem: '\xA0',
-                answer: '\\css{\\rowgrp{\\row{\\sign{\xA0 \xA0  \xA0 \xA0}}{\\frac{\\str{4}}{\\str{5}}}}{\\row{\\str{\xA0}}}{\\row{\\sign{\xA0 \xA0 + \xA0}}{\\frac{\\str{3}}{\\str{4}}}}{\\row{\\str{\xA0}}}{\\row{\\sign{\xA0 \xA0 \xA0 \xA0 \xA0}}{\\frac{\\input{31}}{\\input{20}}}}}{centerTableText4}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
+                problem: '\\str{\xA0}',
+				answer: '\\css{\\rowgrp{grp{\\sign{\xA0 \xA0}}{\\frac{\\str{4}}{\\str{5}}}}{grp{\\sign{+}}{\\frac{\\str{3}}{\\str{4}}}}{grp{\\sign{\xA0 \xA0 \xA0}}{\\frac{\\input{31}}{\\input{20}}}}}{centerTableText3}',
+				controls: {
+					"checkAnswer": true,
+					"help": false,
+					"workbook": true
+				}
             }]
         }, {
             title: 'Workbook',
             children: [{
                 problem: '\\rowgrp{\\row{\\str{Find the lowest common denominator (LCD) of the fractions:}}}',
-                answer: '\\css{\\rowgrp{\\row{\\sign{\xA0 \xA0  \xA0 \xA0}}{\\frac{\\str{4}}{\\str{5}}}}{\\row{\\str{\xA0}}}{\\row{\\sign{\xA0 \xA0 + \xA0}}{\\frac{\\str{3}}{\\str{4}}}}{\\row{\\str{\xA0}}}{\\row{\\sign{\xA0 \xA0 \xA0 \xA0 \xA0}}{\\input{20}}}}{centerTableText4}',
+                answer: '\\css{\\rowgrp{\\grp{\\sign{\xA0 \xA0  \xA0 \xA0}}{\\frac{\\str{4}}{\\str{5}}}}{\\grp{\\sign{\xA0 \xA0 + \xA0}}{\\frac{\\str{3}}{\\str{4}}}}{\\grp{\\sign{\xA0 \xA0 \xA0 \xA0 \xA0}}{\\input{20}}}}{centerTableText3}',
                 controls: {
                     "checkAnswer": true,
                     "help": false
                 },
             }, {
                 problem: '\\str{Use the LCD to convert the fractions: }',
-                answer: '\\rowgrp{\\row{\\sign{\xA0 \xA0 \xA0 \xA0 }}{\\frac{\\str{4}}{\\str{5}}}{\\sign{\xA0 \xA0 = \xA0 \xA0}}{\\frac{\\input{16}}{\\str{20}}}}{\\row{\\str{\xA0}}}{\\row{\\sign{\xA0 \xA0 + \xA0}}{\\frac{\\str{3}}{\\str{4}}}{\\sign{\xA0 \xA0 = \xA0 \xA0}}{\\frac{\\input{15}}{\\str{20}}}}',
+                answer: '\\rowgrp{\\grp{\\sign{\xA0 \xA0 \xA0 \xA0 }}{\\frac{\\str{4}}{\\str{5}}}{\\sign{\xA0 \xA0 = \xA0 \xA0}}{\\frac{\\input{16}}{\\str{20}}}}{\\grp{\\str{\xA0}}}{\\grp{\\sign{\xA0 \xA0 + \xA0}}{\\frac{\\str{3}}{\\str{4}}}{\\sign{\xA0 \xA0 = \xA0 \xA0}}{\\frac{\\input{15}}{\\str{20}}}}',
                 controls: {
                     "checkAnswer": true,
                     "help": false
                 }
             }, {
                 problem: '\\str{Add the fractions:}',
-                answer: '\\css{\\rowgrp{\\row{\\sign{\xA0 \xA0  \xA0 \xA0}}{\\frac{\\str{16}}{\\str{20}}}}{\\row{\\str{\xA0}}}{\\row{\\sign{\xA0 \xA0 + \xA0}}{\\frac{\\str{15}}{\\str{20}}}}{\\row{\\str{\xA0}}}{\\row{\\sign{\xA0 \xA0 \xA0 \xA0 \xA0}}{\\frac{\\input{31}}{\\input{20}}}}}{centerTableText4}',
+                answer: '\\css{\\rowgrp{\\grp{\\sign{\xA0 \xA0 \xA0 \xA0}}{\\frac{\\str{16}}{\\str{20}}}}{\\grp{\\str{\xA0}}}{\\grp{\\sign{\xA0 \xA0 + \xA0}}{\\frac{\\str{15}}{\\str{20}}}}{\\grp{\\str{\xA0}}}{\\grp{\\sign{\xA0 \xA0 \xA0}}{\\frac{\\input{31}}{\\input{20}}}}}{centerTableText4}',
                 controls: {
                     "checkAnswer": true,
                     "help": false
