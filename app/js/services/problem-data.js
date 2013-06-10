00c5556d1792f7e417e464b7c6ee6767df3984df
Justin Doolaege <justindool@sbcglobal.net>
2013-06-09 21:29:23 -0400

    fixed privateData Var

--- app/js/services/problem-data.js
+++ app/js/services/problem-data.js
@@ -1,6 +1,6 @@
 'use strict';
 /*global angular */
-var privateData = {};
+var privateProblemData = {};
     
 angular.module('mathSkills.services')
     .run([
@@ -10,7 +10,10 @@ angular.module('mathSkills.services')
             
             // Store answer events.
             $rootScope.$on('answer', function (e, data) {  //console.log('problem answer', data);
-                privateData.previousCorrect = data.result === 'correct' ? true : false;
+                if (privateProblemData === undefined) {
+                    privateProblemData = {};
+                }
+                privateProblemData.previousCorrect = data.result === 'correct' ? true : false;
                 $rootScope.$broadcast('recompile');
             });
         }
@@ -27,24 +30,24 @@ angular.module('mathSkills.services')
             var problemData = {
                 
                 setObject: function (data) {
-                    privateData = data;
+                    privateProblemData = data;
                     //console.log("set data-data", privateData);
                 },
                 
                 setData: function (data, key) {
-                    privateData[key] = data;
+                    privateProblemData[key] = data;
                     $rootScope.$broadcast('recompile');
                     //console.log("set data-key", privateData[key]);
                 },
                 
                 getData: function (key) {
-                    return privateData[key];
+                    return privateProblemData[key];
                 },
                 
                 compile: function (expectedString){
-                    for (var symbol in privateData) {
-                        if (privateData.hasOwnProperty(symbol)) {
-                            expectedString = expectedString.replace(new RegExp('\\$\\$' + symbol, 'g'), privateData[symbol]);
+                    for (var symbol in privateProblemData) {
+                        if (privateProblemData.hasOwnProperty(symbol)) {
+                            expectedString = expectedString.replace(new RegExp('\\$\\$' + symbol, 'g'), privateProblemData[symbol]);
                         }
                     }
                     //console.log("compile string", expectedString);
@@ -52,12 +55,12 @@ angular.module('mathSkills.services')
                 },
                  
                 flip: function () { 
-                    if(privateData.flip){
-                        if (privateData.flip[0].indexOf(privateData.index) >= 0){
-                            for (var ii = 1; ii < privateData.flip.length; ii++ ){
-                                var work = privateData[privateData.flip[ii][0]];
-                                privateData[privateData.flip[ii][0]] = privateData[privateData.flip[ii][1]];
-                                privateData[privateData.flip[ii][1]] = work;
+                    if(privateProblemData.flip){
+                        if (privateProblemData.flip[0].indexOf(privateProblemData.index) >= 0){
+                            for (var ii = 1; ii < privateProblemData.flip.length; ii++ ){
+                                var work = privateProblemData[privateProblemData.flip[ii][0]];
+                                privateProblemData[privateProblemData.flip[ii][0]] = privateProblemData[privateProblemData.flip[ii][1]];
+                                privateProblemData[privateProblemData.flip[ii][1]] = work;
                                 $rootScope.$broadcast('recompile');
                             }
                         }
@@ -73,20 +76,20 @@ angular.module('mathSkills.services')
                  * @return {Boolean} Whether the passed in index is correct.
                  */
                 index: function (ii) {
-                    if (privateData.index === undefined) {
-                        privateData.index = ii;
+                    if (privateProblemData.index === undefined) {
+                        privateProblemData.index = ii;
                         this.flip();
                     }
-                    return privateData.index;
+                    return privateProblemData.index;
                 },
                 getIndex: function () {
                     var deferred = $q.defer();
 
                     $timeout(function () {
-                        if (privateData.index === undefined) {
+                        if (privateProblemData.index === undefined) {
                             deferred.resolve(0);
                         } else {
-                            deferred.resolve(privateData.index);
+                            deferred.resolve(privateProblemData.index);
                         }
                     }, 0);
 
