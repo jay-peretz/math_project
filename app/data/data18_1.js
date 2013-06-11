6ecc5da628c2510dbfa2de4609f8da17d9e04de6
markafoote <mark@getthatwebsite.com>
2013-06-11 16:02:07 -0700

    convert 18_1 to newer data format

--- app/data/data18_1.js
+++ app/data/data18_1.js
@@ -1,202 +1,100 @@
 /*global angular */
 
-angular.module('mathSkills').value('data18_1', {
-    title: '18.1 Decimal & Fraction Form of Percents',
-    path: '18.1-decimal-fraction-form-percents',
-    children: [{
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent fractional value (reduced to lowest terms) of this decimal:}}}{\\row{\\str{4.8}}}',
-                answer: '\\mixed{\\input{4}}{\\frac{\\input{4}}{\\input{5}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}{\\row{\\percentsdecimalfrac{\\str{4.8}}{decimalNotPercent}{percentAsFraction}}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\mixed{\\str{4}}{\\frac{\\fracstr{4}}{\\fracstr{5}}}}{help-answer-text-tight}}'
+angular.module('mathSkills').service('data18_1', function () {
+    var ret = {
+			title: '18.1 Decimal & Fraction Form of Percents',
+			path: '18.1-decimal-fraction-form-percents',
+            children: []
+        },
+        template = {
+			main: {
+                title: 'Decimal & Fraction Form of Percents',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+                         problem: '\\rowgrp{\\row{\\str{Write the equivalent fractional value (reduced to lowest terms) of this decimal:}}}{\\row{\\str{$decimal}}}',
+						answer: '\\mixed{\\input{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp{\\css{\\str{Write the decimal place value as the denominator of the fraction:}}{label_like}}{\\row{\\mixed{\\str{$helpWhole}}{\\frac{\\fracstr{$helpNum}}{\\fracstr{$helpDen}}}}{\\sign{\xA0 \xA0 = \xA0 \xA0}}{\\mixed{\\str{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\mixed{\\str{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}{help-answer-text-tight}}'
+                		}
+                    }]
+                }]
+            },
+			second: {
+                title: 'Decimal & Fraction Form of Percents',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+                         problem: '\\rowgrp{\\row{\\str{Write the equivalent decimal value of this fraction:}}}{\\row{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}}',
+						answer: '\\input{$answerDecimal}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp{\\row{\\css{\\str{Divide the numerator of the fraction by the denominator.}}{help-answer-text-tight}}}{\\row{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}{\\str{\xA0 means \xA0 $fractionNum }}{\\css{\\str{\xF7}}{bigger}}{\\str{\xA0 $fractionDen}}{\\css{\\str{\xA0 Answer: \xA0 $answerDecimal}}{help-answer-text}}}'
+						}
+                    }]
+                }]
+            },
+			third: {
+                title: 'Decimal & Fraction Form of Percents',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+                       	problem: '\\rowgrp{\\row{\\str{Express the decimal percent as a fractional percent:}}}{\\row{\\str{$decimalPercent %}}}',
+						answer: '\\row{\\mixed{input{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}}{\\str{%}}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}{\\row{\\str{$decimalPercent %}}{\\sign{\xA0 \xA0 = \xA0 \xA0}}{\\mixed{\\str{$helpWhole}}{\\frac{\\fracstr{$helpNum}}{\\fracstr{$helpDen}}}}{\\sign{\xA0 \xA0 = \xA0 \xA0}}{\\mixed{\\str{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\mixed{\\str{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}}'
+						}
+                    }]
+                }]
+            },
+			fourth: {
+                title: 'Decimal & Fraction Form of Percents',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+                       	problem: '\\rowgrp{\\row{\\str{Express the fractional precent as a decimal percent:}}}{\\row{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}{\\str{%}}}',
+						answer: '\\row{\\input{$answerDecimal}}{\\str{%}}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp{\\row{\\css{\\str{Divide the numerator of the fraction by the denominator.}}{help-answer-text-tight}}}{\\row{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}{\\str{% \xA0 }}{\\str{\xA0 means \xA0 $fractionNum }}{\\css{\\str{\xF7}}{bigger}}{\\str{\xA0 $fractionDen}}{\\str{ \xA0 percent}}}{\\row{\\css{\\str{\xA0 Answer: \xA0 $answerDecimal %}}{help-answer-text}}}'
+						}
+                    }]
+                }]
+            }
+        },
+        data = [
+            { decimal: '4.8', answerWhole: 4, answerNum: 4, answerDen: 5, helpWhole: 4, helpNum: 8, helpDen: 10, template: 'main' },
+            { decimal: '1.125', answerWhole: 1, answerNum: 1, answerDen: 8, helpWhole: 1, helpNum: 125, helpDen: 1000, template: 'main' },
+            { decimal: '3.2', answerWhole: 3, answerNum: 1, answerDen: 5, helpWhole: 3, helpNum: 2, helpDen: 10, template: 'main' },
+            { decimal: '2.5', answerWhole: 2, answerNum: 1, answerDen: 2, helpWhole: 2, helpNum: 5, helpDen: 10, template: 'main' },
+            { decimal: '1.75', answerWhole: 1, answerNum: 3, answerDen: 4, helpWhole: 1, helpNum: 75, helpDen: 100, template: 'main' },
+           	{ fractionNum: '7', fractionDen: '8', answerDecimal: '0.875', template: 'second' },
+           	{ fractionNum: '3', fractionDen: '5', answerDecimal: '0.6', template: 'second' },
+            { fractionNum: '1', fractionDen: '10', answerDecimal: '0.1', template: 'second' },
+            { fractionNum: '3', fractionDen: '8', answerDecimal: '0.375', template: 'second' },
+            { fractionNum: '9', fractionDen: '10', answerDecimal: '0.9', template: 'second' },
+			{ decimalPercent: '1.375', answerWhole: 1, answerNum: 3, answerDen: 8, helpWhole: 1, helpNum: 375, helpDen: 1000, template: 'third' },
+			{ decimalPercent: '2.6', answerWhole: 2, answerNum: 3, answerDen: 5, helpWhole: 2, helpNum: 6, helpDen: 10, template: 'third' },
+			{ decimalPercent: '3.875', answerWhole: 3, answerNum: 7, answerDen: 8, helpWhole: 3, helpNum: 875, helpDen: 1000, template: 'third' },
+			{ fractionWhole: '0', fractionNum: '5', fractionDen: '8', answerDecimal: '0.625', template: 'fourth' },
+			{ fractionWhole: '0', fractionNum: '1', fractionDen: '2', answerDecimal: '0.50', template: 'fourth' },
+        ],
+        interpolate = function (obj, data) {
+            var string = JSON.stringify(obj);
+            for (var symbol in data) {
+                if (data.hasOwnProperty(symbol)) {
+                    string = string.replace(new RegExp('\\$' + symbol, 'g'), data[symbol]);
                 }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent fractional value (reduced to lowest terms) of this decimal:}}}{\\row{\\str{0.125}}}',
-                answer: '\\frac{\\input{1}}{\\input{8}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}{\\row{\\percentsdecimalfrac{\\str{0.125}}{decimalNotPercent}{percentAsFraction}}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{1}}{\\fracstr{8}}}{help-answer-text-tight}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent fractional value (reduced to lowest terms) of this decimal:}}}{\\row{\\str{3.2}}}',
-                answer: '\\mixed{\\input{3}}{\\frac{\\input{1}}{\\input{5}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}{\\row{\\percentsdecimalfrac{\\str{3.2}}{decimalNotPercent}{percentAsFraction}}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\mixed{\\str{3}}{\\frac{\\fracstr{1}}{\\fracstr{5}}}}{help-answer-text-tight}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent fractional value (reduced to lowest terms) of this decimal:}}}{\\row{\\str{0.5}}}',
-                answer: '\\frac{\\input{1}}{\\input{2}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}{\\row{\\percentsdecimalfrac{\\str{0.5}}{decimalNotPercent}{percentAsFraction}}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{1}}{\\fracstr{2}}}{help-answer-text-tight}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent fractional value (reduced to lowest terms) of this decimal:}}}{\\row{\\str{0.75}}}',
-                answer: '\\frac{\\input{3}}{\\input{4}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}{\\row{\\percentsdecimalfrac{\\str{0.75}}{decimalNotPercent}{percentAsFraction}}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{3}}{\\fracstr{4}}}{help-answer-text-tight}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent decimal value of this fraction:}}}{\\row{\\frac{\\str{7}}{\\str{8}}}}',
-                answer: '\\input{0.875}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Divide the numerator of the fraction by the denominator.}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\percentsdecimalfrac{\\frac{\\str{7}}{\\str{8}}}{percentAsFraction}{decimalNotPercent}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent decimal value of this fraction:}}}{\\row{\\mixed{\\str{3}}{\\frac{\\str{2}}{\\str{5}}}}}',
-                answer: '\\input{3.4}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Divide the numerator of the fraction by the denominator.}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\percentsdecimalfrac{\\mixed{\\str{3}}{\\frac{\\str{2}}{\\str{5}}}}{percentAsFraction}{decimalNotPercent}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent decimal value of this fraction:}}}{\\row{\\frac{\\str{1}}{\\str{10}}}}',
-                answer: '\\input{0.1}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Divide the numerator of the fraction by the denominator.}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\percentsdecimalfrac{\\frac{\\str{1}}{\\str{10}}}{percentAsFraction}{decimalNotPercent}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent decimal value of this fraction:}}}{\\row{\\mixed{\\str{1}}{\\frac{\\str{3}}{\\str{8}}}}}',
-                answer: '\\input{1.375}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Divide the numerator of the fraction by the denominator.}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\percentsdecimalfrac{\\mixed{\\str{1}}{\\frac{\\str{3}}{\\str{8}}}}{percentAsFraction}{decimalNotPercent}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Write the equivalent decimal value of this fraction:}}}{\\row{\\frac{\\str{1}}{\\str{4}}}}',
-                answer: '\\input{0.25}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Divide the numerator of the fraction by the denominator.}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\percentsdecimalfrac{\\frac{\\str{1}}{\\str{4}}}{percentAsFraction}{decimalNotPercent}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Express the decimal percent as a fractional percent:}}}{\\row{\\str{0.875%}}}',
-                answer: '\\row{\\frac{\\input{7}}{\\input{8}}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}{\\row{\\percentsdecimalfrac{\\str{0.875}}{decimalNotPercent}{percentAsFraction}}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{7}}{\\fracstr{8}}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Express the decimal percent as a fractional percent:}}}{\\row{\\str{0.75%}}}',
-                answer: '\\row{\\frac{\\input{3}}{\\input{4}}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}{\\row{\\percentsdecimalfrac{\\str{0.75}}{decimalNotPercent}{percentAsFraction}}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{3}}{\\fracstr{4}}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Express the decimal percent as a fractional percent:}}}{\\row{\\str{0.875%}}}',
-                answer: '\\row{\\frac{\\input{7}}{\\input{8}}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}{\\row{\\percentsdecimalfrac{\\str{0.875}}{decimalNotPercent}{percentAsFraction}}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{7}}{\\fracstr{8}}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Express the fractional precent as a decimal percent:}}}{\\row{\\mixed{\\str{1}}{\\frac{\\str{3}}{\\str{8}}}}{\\str{%}}}',
-                answer: '\\row{\\input{1.375}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Divide the numerator of the fraction by the denominator.}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\percentsdecimalfrac{\\mixed{\\str{1}}{\\frac{\\str{3}}{\\str{8}}}}{percentAsFraction}{decimalNotPercent}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal & Fraction Form of Percents',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Express the fractional precent as a decimal percent:}}}{\\row{\\frac{\\str{9}}{\\str{10}}}{\\str{%}}}',
-                answer: '\\row{\\input{0.9}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Divide the numerator of the fraction by the denominator.}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\percentsdecimalfrac{\\frac{\\str{9}}{\\str{10}}}{percentAsFraction}{decimalNotPercent}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}}}'
-                }
-            }]
-        }]
-    }]
+            }
+            return JSON.parse(string);
+        };
+
+    ret.children = data.map(function (problem) {
+        return interpolate(template[problem.template], problem);
+    });
+
+    return ret;
 });
+
