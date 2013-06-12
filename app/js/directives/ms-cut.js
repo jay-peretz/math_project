b556bd2c7da21b5e899b42e99659da30f003cde3
Justin Doolaege <justindool@sbcglobal.net>
2013-06-12 16:00:15 -0400

    added ms-cut.js file

---
+++ app/js/directives/ms-cut.js
@@ -0,0 +1,42 @@
+'use strict';
+/*global angular */
+
+angular.module('mathSkills')
+    .config(['parserProvider', function (parserProvider) {
+        parserProvider.register('cut', {
+            directiveTemplate: '<ms-cut-set expected={{expected}} label=lable></ms-cut-set>'
+        });
+    }])
+    .directive('msCut', [
+        'parser',
+        '$compile',
+        'problemData',
+        '$filter',
+        function (parser, $compile, problemData, $filter) {
+            return {
+                controller: function ($scope, $element) {
+                    // Extract the args array.
+                    $scope.$watch('expected', function () {
+                        if ($scope.expected){
+                            $scope.myargs = parser.extractTag($scope.expected).args;
+                            var x = $filter('replace');
+                            problemData.setData(x($scope.myargs[0], 'input', 'str'), $scope.myargs[1]);
+                        }
+                    });
+
+                    $scope.$on('answer', function (e, data) {
+                        //var val = parser.extractTag(data.answer).args[0]; //console.log('set answer', val);
+                        //if (data.result === "correct"){
+                            //problemData.setData(val, $scope.myargs[1]);
+                        //}
+                    });
+                },
+                restrict: 'E',
+                scope: {
+                    expected: '@',
+                    label: '@'
+                },
+                template: '<div><ms-expression expected={{myargs[0]}}></ms-expression></div>'
+            };
+        }
+    ]);
