44c011f664a3058785fb2514b01bca8be96a72e2
markafoote <mark@getthatwebsite.com>
2013-06-12 11:08:27 -0700

    improve 18_1

--- app/data/data18_1.js
+++ app/data/data18_1.js
@@ -32,15 +32,16 @@ angular.module('mathSkills').service('data18_1', function () {
                 children: [{
                     title: 'Main Answer',
                     children: [{
-                         problem: '\\rowgrp{\\row{\\str{Write the equivalent decimal value of this fraction:}}}{\\row{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}}',
-						answer: '\\input{$answerDecimal}',
+						problem: '\\rowgrp{\\row{\\str{Write the equivalent decimal value of this fraction:}}}{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}}',
+						answer: '\\input{$answerDecimal}',						
 						controls: {
 							"checkAnswer": true,
 							"help": '\\rowgrp'
 							+'{\\row{\\css{\\str{Divide the numerator of the fraction by the denominator.}}{help-answer-text-tight}}}'
-							+'{\\row{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}{\\str{\xA0 means \xA0 $fractionNum }}'
-							+'{\\css{\\str{\xF7}}{bigger}}{\\str{\xA0 $fractionDen}}'
-							+'{\\css{\\str{\xA0 Answer: \xA0 $answerDecimal}}{help-answer-text}}}'
+							+'{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}}'
+							+'{\\row{\\str{the fraction \xA0}}{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}'
+							+'{\\str{\xA0 means \xA0 $fractionNum }}{\\css{\\str{\xF7}}{bigger}}{\\str{\xA0 $fractionDen}}}'
+							+'{\\row{\\css{\\str{\xA0 Answer: \xA0 $answerDecimal}}{help-answer-text}}}'
 						}
                     }]
                 }]
@@ -51,7 +52,7 @@ angular.module('mathSkills').service('data18_1', function () {
                     title: 'Main Answer',
                     children: [{
                        	problem: '\\rowgrp{\\row{\\str{Express the decimal percent as a fractional percent:}}}{\\row{\\str{$decimalPercent %}}}',
-						answer: '\\row{\\mixed{input{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}}{\\str{%}}',
+						answer: '\\grp{\\mixed{\\input{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}}{\\str{%}}',
 						controls: {
 							"checkAnswer": true,
 							"help": '\\rowgrp'
@@ -69,13 +70,13 @@ angular.module('mathSkills').service('data18_1', function () {
                 children: [{
                     title: 'Main Answer',
                     children: [{
-                       	problem: '\\rowgrp{\\row{\\str{Express the fractional precent as a decimal percent:}}}{\\row{\\mixed{\\str{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}{\\str{%}}}',
+                       	problem: '\\rowgrp{\\row{\\str{Express the fractional precent as a decimal percent:}}}{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}{\\str{%}}}',
 						answer: '\\row{\\input{$answerDecimal}}{\\str{%}}',
 						controls: {
 							"checkAnswer": true,
 							"help": '\\rowgrp'
 							+'{\\row{\\css{\\str{Divide the numerator of the fraction by the denominator.}}{help-answer-text-tight}}}'
-							+'{\\row{\\mixed{\\str{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}{\\str{%}}}'
+							+'{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}{\\str{%}}}'
 							+'{\\row{\\str{the fraction \xA0}}{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}'
 							+'{\\str{\xA0 means \xA0 $fractionNum }}{\\css{\\str{\xF7}}{bigger}}{\\str{\xA0 $fractionDen}}}'
 							+'{\\row{\\css{\\str{\xA0 Answer: \xA0 $answerDecimal %}}{help-answer-text}}}'
@@ -90,16 +91,16 @@ angular.module('mathSkills').service('data18_1', function () {
             { decimal: '3.2', answerWhole: 3, answerNum: 1, answerDen: 5, helpWhole: 3, helpNum: 2, helpDen: 10, template: 'main' },
             { decimal: '2.5', answerWhole: 2, answerNum: 1, answerDen: 2, helpWhole: 2, helpNum: 5, helpDen: 10, template: 'main' },
             { decimal: '1.75', answerWhole: 1, answerNum: 3, answerDen: 4, helpWhole: 1, helpNum: 75, helpDen: 100, template: 'main' },
-           	{ fractionNum: '7', fractionDen: '8', answerDecimal: '0.875', template: 'second' },
-           	{ fractionNum: '3', fractionDen: '5', answerDecimal: '0.6', template: 'second' },
-            { fractionNum: '1', fractionDen: '10', answerDecimal: '0.1', template: 'second' },
-            { fractionNum: '3', fractionDen: '8', answerDecimal: '0.375', template: 'second' },
-            { fractionNum: '9', fractionDen: '10', answerDecimal: '0.9', template: 'second' },
+           	{ fractionWhole: '1', fractionNum: '7', fractionDen: '8', answerDecimal: '1.875', template: 'second' },
+           	{ fractionWhole: '&nbsp;', fractionNum: '3', fractionDen: '5', answerDecimal: '0.6', template: 'second' },
+            { fractionWhole: '3', fractionNum: '1', fractionDen: '10', answerDecimal: '3.1', template: 'second' },
+            { fractionWhole: '2', fractionNum: '3', fractionDen: '8', answerDecimal: '2.375', template: 'second' },
+            { fractionWhole: '&nbsp;', fractionNum: '9', fractionDen: '10', answerDecimal: '0.9', template: 'second' },
 			{ decimalPercent: '1.375', answerWhole: 1, answerNum: 3, answerDen: 8, helpWhole: 1, helpNum: 375, helpDen: 1000, template: 'third' },
 			{ decimalPercent: '2.6', answerWhole: 2, answerNum: 3, answerDen: 5, helpWhole: 2, helpNum: 6, helpDen: 10, template: 'third' },
 			{ decimalPercent: '3.875', answerWhole: 3, answerNum: 7, answerDen: 8, helpWhole: 3, helpNum: 875, helpDen: 1000, template: 'third' },
-			{ fractionWhole: '\xA0', fractionNum: '5', fractionDen: '8', answerDecimal: '0.625', template: 'fourth' },
-			{ fractionWhole: '2', fractionNum: '1', fractionDen: '2', answerDecimal: '2.50', template: 'fourth' },
+			{ fractionWhole: '&nbsp;', fractionNum: '5', fractionDen: '8', answerDecimal: '0.625', template: 'fourth' },
+			{ fractionWhole: '2', fractionNum: '1', fractionDen: '2', answerDecimal: '2.5', template: 'fourth' },
         ],
         interpolate = function (obj, data) {
             var string = JSON.stringify(obj);
