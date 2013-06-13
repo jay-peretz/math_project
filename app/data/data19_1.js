c42e72b0ed55dbb9c81fd44612c4113de3b07803
markafoote <mark@getthatwebsite.com>
2013-06-13 14:01:00 -0700

    finish 19_1

--- app/data/data19_1.js
+++ app/data/data19_1.js
@@ -1,137 +1,135 @@
+
 /*global angular */
 
-angular.module('mathSkills').value('data19_1', {
-    title: '19.1 Change a Fraction to a Percent (directly)',
-    path: '19.1-change-a-percent-to-a-decimal',
-    children: [{
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{4}}{\\str{5}}}',
-                answer: '\\row{\\input{80}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\frac{\\str{4}}{\\str{5}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{80%}}{\\css{\\str{Answer:  80%}}{help-answer-text help-answer-margin-right}}}'
+angular.module('mathSkills').service('data19_1', function () {
+    var ret = {
+			title: '19.1 Change a Fraction to a Percent (directly)',
+			path: '19.1-change-a-percent-to-a-decimal',
+            children: []
+        },
+        template = {
+			main: {
+                title: 'Change a Fraction to a Percent (directly)',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp'
+						+'{\\grp{\\str{Change the fraction to a percent (directly):}}}'
+						+'{\\grp{\\html{&nbsp;}}}'
+						+'{\\grp{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}}',
+						answer: '\\grp{\\input{$answerPercent}}{\\str{%}}',	
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\row{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}'
+							+'{\\css{\\html{&times;}}{bigger}}{\\str{\xA0 100}}'
+							+'{\\sign{\xA0 = \xA0}}'
+							+'{\\str{$answerPercent}}{\\str{%}}}'
+							+'{\\row{\\html{&nbsp;}}}'
+							+'{\\row{\\css{\\str{\xA0 Answer: \xA0 $answerPercent %}}{help-answer-text}}}'
+						}
+					}]
+                }]
+            },
+			second: {
+                title: 'Decimal & Fraction Form of Percents',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\cut'
+						+'{\\rowgrp'
+						+'{\\grp{\\str{Change the fraction to a mixed number percentage:}}}'
+						+'{\\grp{\\html{&nbsp;}}}'
+						+'{\\grp{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}}}'
+						+'{fractionMixedPercentProblem}',
+						answer: '\\cut{\\grp{\\mixed{\\input{$answerFractionWhole}}{\\frac{\\input{$answerFractionNum}}{\\input{$answerFractionDen}}}}{\\str{%}}}}}{fractionMixedPercentAnswer}',		
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}'
+							+'{\\sign{ \xA0 = \xA0 }}'
+							+'{\\mixed{\\html{$answerFractionWhole}}{\\frac{\\fracstr{$answerFractionNum}}{\\fracstr{$answerFractionDen}}}}{\\str{%}}}'
+							+'{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}'
+							+'{css{\\mixed{\\html{$answerFractionWhole}}{\\frac{\\fracstr{$answerFractionNum}}{\\fracstr{$answerFractionDen}}}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}}'
+						}
+						
+					}]
+                }]
+            },
+			third: {
+                title: 'Decimal & Fraction Form of Percents',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\cut'
+						+'{\\rowgrp'
+						+'{\\grp{\\str{Change the fraction to a mixed number percentage:}}}'
+						+'{\\grp{\\html{&nbsp;}}}'
+						+'{\\grp{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}}}'
+						+'{fractionMixedPercentProblem}',
+						answer: '\\cut'
+						+'{\\grp{\\mixed{\\input{$answerFractionWhole}}{\\frac{\\input{$answerFractionNum}}{\\input{$answerFractionDen}}}}'
+						+'{\\str{%}}}'
+						+'{fractionMixedPercentAnswer}',		
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}'
+							+'{\\sign{ \xA0 = \xA0 }}'
+							+'{\\mixed{\\html{$answerFractionWhole}}{\\frac{\\fracstr{$answerFractionNum}}{\\fracstr{$answerFractionDen}}}}{\\str{%}}}'
+							+'{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}'
+							+'{css{\\mixed{\\html{$answerFractionWhole}}{\\frac{\\fracstr{$answerFractionNum}}{\\fracstr{$answerFractionDen}}}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}}'
+						}
+						
+					}, {
+						problem: '\\rowgrp'
+						+'{$$fractionMixedPercentProblem}'
+						+'{\\grp{str{\xA0 Answer:}}{$$fractionMixedPercentAnswer}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\str{Change the mixed number percentage to a decimal percentage (round the decimal to the tenths place):}}}',
+						answer: '\\grp{\\input{$percentDecimal}}{\\str{%}}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}'
+							+'{\\sign{ \xA0 = \xA0 }}'
+							+'{\\mixed{\\html{$answerFractionWhole}}{\\frac{\\fracstr{$answerFractionNum}}{\\fracstr{$answerFractionDen}}}}{\\str{%}}'
+							+'{\\sign{ \xA0 = \xA0 }}'
+							+'{\\str{$percentDecimal}}{\\str{%}}}'
+							+'{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}'
+							+'{css{\\str{$percentDecimal}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}}'
+						}
+					}]
+                }]
+            }
+        },
+        data = [
+            { fractionWhole: '&nbsp;', fractionNum: '4', fractionDen: '5', answerPercent: '80', template: 'main' },
+			{ fractionWhole: '&nbsp;', fractionNum: '1', fractionDen: '10', answerPercent: '10', template: 'main' },
+			{ fractionWhole: '&nbsp;', fractionNum: '49', fractionDen: '50', answerPercent: '98', template: 'main' },
+			{ fractionWhole: '&nbsp;', fractionNum: '9', fractionDen: '5', answerPercent: '180', template: 'main' },
+			{ fractionWhole: '&nbsp;', fractionNum: '16', fractionDen: '10', answerPercent: '160', template: 'main' },
+			{ fractionWhole: '&nbsp;', fractionNum: '6', fractionDen: '7', answerFractionWhole: '85', answerFractionNum: '5', answerFractionDen: '7', template: 'second' },
+			{ fractionWhole: '&nbsp;', fractionNum: '3', fractionDen: '11', answerFractionWhole: '27', answerFractionNum: '3', answerFractionDen: '11', template: 'second' },
+			{ fractionWhole: '&nbsp;', fractionNum: '2', fractionDen: '3', answerFractionWhole: '66', answerFractionNum: '2', answerFractionDen: '3', percentDecimal: '66.7', template: 'third' },
+			{ fractionWhole: '&nbsp;', fractionNum: '1', fractionDen: '12', answerFractionWhole: '8', answerFractionNum: '1', answerFractionDen: '3', percentDecimal: '8.3', template: 'third' },
+			{ fractionWhole: '&nbsp;', fractionNum: '2', fractionDen: '9', answerFractionWhole: '22', answerFractionNum: '2', answerFractionDen: '9', percentDecimal: '22.2', template: 'third' },
+        ],
+        interpolate = function (obj, data) {
+            var string = JSON.stringify(obj);
+            for (var symbol in data) {
+                if (data.hasOwnProperty(symbol)) {
+                    string = string.replace(new RegExp('(\\$)?\\$' + symbol, 'g'), function ($0, $1) { return $1?$0 : data[symbol]; }); //console.log(string); 
                 }
-            }]
-        }]
-    }, {
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{1}}{\\str{10}}}',
-                answer: '\\row{\\input{10}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\frac{\\str{1}}{\\str{10}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{10%}}{\\css{\\str{Answer:  10%}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{49}}{\\str{50}}}',
-                answer: '\\row{\\input{98}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\frac{\\str{49}}{\\str{50}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{98%}}{\\css{\\str{Answer:  98%}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{9}}{\\str{5}}}',
-                answer: '\\row{\\input{180}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\frac{\\str{9}}{\\str{5}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{180%}}{\\css{\\str{Answer:  180%}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a percent (directly):}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{16}}{\\str{10}}}',
-                answer: '\\row{\\input{160}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\frac{\\str{16}}{\\str{10}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\str{160%}}{\\css{\\str{Answer:  160%}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{6}}{\\str{7}}}',
-                answer: '\\row{\\mixed{\\input{85}}{\\frac{\\input{5}}{\\input{7}}}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{6}}{\\str{7}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{85}}{\\frac{\\fracstr{5}}{\\fracstr{7}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{85}}{\\frac{\\fracstr{5}}{\\fracstr{7}}}}{label_like}}{css{\\str{%}}{label_like}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{3}}{\\str{11}}}',
-                answer: '\\row{\\mixed{\\input{27}}{\\frac{\\input{3}}{\\input{11}}}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{3}}{\\str{11}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{27}}{\\frac{\\fracstr{3}}{\\fracstr{11}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{27}}{\\frac{\\fracstr{3}}{\\fracstr{11}}}}{label_like}}{css{\\str{%}}{label_like}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{2}}{\\str{3}}}',
-                answer: '\\row{\\mixed{\\input{66}}{\\frac{\\input{2}}{\\input{3}}}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{2}}{\\str{3}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{66}}{\\frac{\\fracstr{2}}{\\fracstr{3}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{66}}{\\frac{\\fracstr{2}}{\\fracstr{3}}}}{label_like}}{css{\\str{%}}{label_like}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{1}}{\\str{15}}}',
-                answer: '\\row{\\mixed{\\input{6}}{\\frac{\\input{2}}{\\input{3}}}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{1}}{\\str{15}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{6}}{\\frac{\\fracstr{2}}{\\fracstr{3}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{6}}{\\frac{\\fracstr{2}}{\\fracstr{3}}}}{label_like}}{css{\\str{%}}{label_like}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Fraction to a Percent (directly)',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change the fraction to a mixed-number percentage:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{2}}{\\str{9}}}',
-                answer: '\\row{\\mixed{\\input{22}}{\\frac{\\input{2}}{\\input{9}}}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{2}}{\\str{9}}}{\\html{<span>&nbsp; &times; &nbsp;</span>}}{\\str{ 100 }}{\\sign{\xA0 = \xA0}}{\\mixed{\\str{22}}{\\frac{\\fracstr{2}}{\\fracstr{9}}}}{\\str{%}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\mixed{\\fracstr{22}}{\\frac{\\fracstr{2}}{\\fracstr{9}}}}{label_like}}{css{\\str{%}}{label_like}}'
-                }
-            }]
-        }]
-    }]
+            }
+            var ret = JSON.parse(string);
+            ret.data = angular.copy(data);
+            return ret;
+        };
+
+    ret.children = data.map(function (problem) {
+        return interpolate(template[problem.template], problem);
+    });
+    
+    return ret;
 });
