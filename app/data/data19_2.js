5d3d92a15726e7a733542d1329a0bb15b1b1cf1d
markafoote <mark@getthatwebsite.com>
2013-06-13 10:39:47 -0700

    19_2 finished

--- app/data/data19_2.js
+++ app/data/data19_2.js
@@ -21,6 +21,7 @@ angular.module('mathSkills').service('data19_2', function () {
 							+'{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}}'
 							+'{\\row{\\str{the fraction \xA0}}{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}'
 							+'{\\str{\xA0 means \xA0 $fractionNum }}{\\css{\\str{\xF7}}{bigger}}{\\str{\xA0 $fractionDen}}}'
+							+'{\\row{\\html{&nbsp;}}}'
 							+'{\\row{\\css{\\str{\xA0 Answer: \xA0 $answerDecimal}}{help-answer-text}}}'
 						}
 						
@@ -29,11 +30,70 @@ angular.module('mathSkills').service('data19_2', function () {
 						+'{$$fractionDecimalProblem}'
 						+'{\\row{str{\xA0 Answer:}}{$$fractionDecimalAnswer}}'
 						+'{\\html{&nbsp;}}'
-						+'{\\str{Now change the decimal to a percentage:}}',
+						+'{\\str{Change the decimal to a percentage:}}',
 						answer: '\\grp{\\input{$answerPercent}}{\\str{%}}',
 						controls: {
 							"checkAnswer": true,
 							"help": '\\rowgrp{\\row{\\percentarrows{$answerDecimal}{100}{decimal}{percent}}{\\row{\\html{&nbsp;}}}{\\row{\\css{\\str{Answer: $answerPercent %}}{help-answer-text help-text-margin-width}}}'
+						}
+					}]
+                }]
+            },
+			second: {
+                title: 'Decimal & Fraction Form of Percents',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\cut'
+						+'{\\rowgrp'
+						+'{\\grp{\\str{Change the fraction to decimal:}}}'
+						+'{\\grp{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}}}'
+						+'{fractionDecimalProblem}',
+						answer: '\\cut{\\input{$answerDecimal}}{fractionDecimalAnswer}',	
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\row{\\css{\\str{Divide the numerator of the fraction by the denominator.}}{help-answer-text-tight}}}'
+							+'{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}}'
+							+'{\\row{\\str{the fraction \xA0}}{\\frac{\\str{$fractionNum}}{\\str{$fractionDen}}}'
+							+'{\\str{\xA0 means \xA0 $fractionNum }}{\\css{\\str{\xF7}}{bigger}}{\\str{\xA0 $fractionDen}}}'
+							+'{\\row{\\html{&nbsp;}}}'
+							+'{\\row{\\css{\\str{\xA0 Answer: \xA0 $answerDecimal}}{help-answer-text}}}'
+						}
+						
+					}, {
+						problem: '\\cut{'
+						+'\\rowgrp'
+						+'{$$fractionDecimalProblem}'
+						+'{\\row{str{\xA0 Answer:}}{$$fractionDecimalAnswer}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\str{Change the decimal to a percentage:}}}' 
+						+'{decimalPercentProblem}',
+						answer: '\\cut{\\grp{\\input{$answerPercent}}{\\str{%}}}{decimalPercentAnswer}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\row{\\percentarrows{$answerDecimal}{100}{decimal}{percent}}'
+							+'{\\row{\\html{&nbsp;}}}'
+							+'{\\row{\\css{\\str{Answer: $answerPercent %}}{help-answer-text help-text-margin-width}}}'
+						}
+					}, {
+						problem: '\\rowgrp'
+						+'{$$decimalPercentProblem}'
+						+'{\\row{str{\xA0 Answer:}}{$$decimalPercentAnswer}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\str{Change the decimal percentage to a fractional or mixed number percentage:}}',
+						answer: '\\grp{\\mixed{\\input{$fractionPercentWhole}}{\\frac{\\input{$fractionPercentNum}}{\\input{$fractionPercentDen}}}}{\\str{%}}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\row{\\str{Write the decimal place value as the denominator of the fraction.}}}'
+							+'{\\row{\\str{$answerPercent}}{\\str{%}}{\\sign{\xA0 = \xA0}}'
+							+'{\\mixed{\\str{$helpWhole}}{\\frac{\\fracstr{$helpNum}}{\\fracstr{$helpDen}}}}{\\str{%}}'
+							+'{\\sign{ \xA0 = \xA0 }}'
+							+'{\\mixed{\\str{$fractionPercentWhole}}{\\frac{\\fracstr{$fractionPercentNum}}{\\fracstr{$fractionPercentDen}}}}{\\str{%}}}'
+							+'{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}'
+							+'{css{\\mixed{\\str{$fractionPercentWhole}}{\\frac{\\fracstr{$fractionPercentNum}}{\\fracstr{$fractionPercentDen}}}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}}'
 						}
 					}]
                 }]
@@ -45,11 +105,11 @@ angular.module('mathSkills').service('data19_2', function () {
 			{ fractionWhole: '&nbsp;', fractionNum: '5', fractionDen: '10', answerDecimal: '0.5', answerPercent: '50', template: 'main' },
 			{ fractionWhole: '&nbsp;', fractionNum: '29', fractionDen: '50', answerDecimal: '0.58', answerPercent: '58', template: 'main' },
 			{ fractionWhole: '&nbsp;', fractionNum: '38', fractionDen: '20', answerDecimal: '1.9', answerPercent: '190', template: 'main' },
-			{ fractionWhole: '&nbsp;', fractionNum: '79', fractionDen: '160', answerDecimal: '0.49375', answerPercent: '49.375', template: 'main' },
-			{ fractionWhole: '&nbsp;', fractionNum: '71', fractionDen: '160', answerDecimal: '0.44375', answerPercent: '44.375', template: 'main' },
-			{ fractionWhole: '&nbsp;', fractionNum: '771', fractionDen: '800', answerDecimal: '0.96375', answerPercent: '96.375', template: 'main' },
-			{ fractionWhole: '&nbsp;', fractionNum: '2', fractionDen: '125', answerDecimal: '0.016', answerPercent: '1.6', template: 'main' },
-			{ fractionWhole: '&nbsp;', fractionNum: '189', fractionDen: '200', answerDecimal: '0.945', answerPercent: '94.5', template: 'main' },
+			{ fractionWhole: '&nbsp;', fractionNum: '79', fractionDen: '160', answerDecimal: '0.49375', answerPercent: '49.375', fractionPercentWhole: '49', fractionPercentNum: '3', fractionPercentDen: '8', helpWhole: '49', helpNum: '375', helpDen: '1000', template: 'second' },
+			{ fractionWhole: '&nbsp;', fractionNum: '11', fractionDen: '16', answerDecimal: '0.6875', answerPercent: '68.75', fractionPercentWhole: '68', fractionPercentNum: '3', fractionPercentDen: '4', helpWhole: '68', helpNum: '75', helpDen: '100', template: 'second' },
+			{ fractionWhole: '&nbsp;', fractionNum: '771', fractionDen: '800', answerDecimal: '0.96375', answerPercent: '96.375', fractionPercentWhole: '96', fractionPercentNum: '3', fractionPercentDen: '8', helpWhole: '96', helpNum: '375', helpDen: '1000', template: 'second' },
+			{ fractionWhole: '&nbsp;', fractionNum: '2', fractionDen: '125', answerDecimal: '0.016', answerPercent: '1.6', fractionPercentWhole: '1', fractionPercentNum: '3', fractionPercentDen: '5', helpWhole: '1', helpNum: '16', helpDen: '1000', template: 'second' },
+			{ fractionWhole: '&nbsp;', fractionNum: '189', fractionDen: '200', answerDecimal: '0.945', answerPercent: '94.5', fractionPercentWhole: '94', fractionPercentNum: '1', fractionPercentDen: '2', helpWhole: '94', helpNum: '5', helpDen: '10', template: 'second' }
         ],
         interpolate = function (obj, data) {
             var string = JSON.stringify(obj);
