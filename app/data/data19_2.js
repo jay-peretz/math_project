7ea4eeee4a7a338b8f68e9dddb3c75255b9e73fc
Justin Doolaege <justindool@sbcglobal.net>
2013-06-13 00:59:56 -0400

    19.2 updated interpolate to new 5143var in data19_2.js

--- app/data/data19_2.js
+++ app/data/data19_2.js
@@ -57,15 +57,17 @@ angular.module('mathSkills').service('data19_2', function () {
             var string = JSON.stringify(obj);
             for (var symbol in data) {
                 if (data.hasOwnProperty(symbol)) {
-                    string = string.replace(new RegExp('\\$' + symbol, 'g'), data[symbol]);
+                    string = string.replace(new RegExp('(\\$)?\\$' + symbol, 'g'), function ($0, $1) { return $1?$0 : data[symbol]; }); //console.log(string); 
                 }
             }
-            return JSON.parse(string);
+            var ret = JSON.parse(string);
+            ret.data = angular.copy(data);
+            return ret;
         };
 
     ret.children = data.map(function (problem) {
         return interpolate(template[problem.template], problem);
     });
-
+    
     return ret;
 });
