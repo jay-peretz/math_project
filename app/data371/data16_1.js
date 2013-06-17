a4dab1a29f38b9ede111bca41d8c2f7e461f77bf
markafoote <mark@getthatwebsite.com>
2013-06-17 13:17:04 -0700

    continued work on 16_1

--- app/data371/data16_1.js
+++ app/data371/data16_1.js
@@ -14,8 +14,8 @@ angular.module('mathSkills').service('data16_1', function () {
 				children: [{
 					title: 'Main Answer',
 					children: [{
-						problem: '\\butgrp{\\col{\\row{\\str{Choose the reduced ratio with the same meaning as: }}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{$problemNum}}{\\str{$problemDen}}}}{\\row{\\str{\xA0}}}{\\row{\\but{A}{F}}{\\str{\xA0 \xA0 $button1a to $button1b}}}{\\row{\\but{B}{F}}{\\str{\xA0 \xA0 $button2a to $button2b}}}{\\row{\\but{C}{F}}{\\str{\xA0 \xA0 $button3a to $button3b}}}{\\row{\\but{D}{T}}{\\str{\xA0 \xA0 $button4a to $button4b}}}}}',
-						answer: 'A',
+						problem: '\\col{\\row{\\str{Choose the reduced ratio with the same meaning as: }}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{$problemNum}}{\\str{$problemDen}}}}',
+						answer: '\\css{\\butgrp{\\col{\\row{\\str{\xA0}}}{\\row{\\but{A}{F}}{\\str{\xA0 \xA0 $button1a to $button1b}}}{\\row{\\but{B}{F}}{\\str{\xA0 \xA0 $button2a to $button2b}}}{\\row{\\but{C}{F}}{\\str{\xA0 \xA0 $button3a to $button3b}}}{\\row{\\but{D}{T}}{\\str{\xA0 \xA0 $button4a to $button4b}}}}}{ chart-buttons-margin}}',
 						controls: {
 							"checkAnswer": true,
 							"help": '\\str{D: $problemNum to $problemDen}'
@@ -28,27 +28,19 @@ angular.module('mathSkills').service('data16_1', function () {
                 children: [{
                     title: 'Main Answer',
                     children: [{
-						problem: '\\rowgrp'
-						+'{\\grp{\\str{Change the fraction to a mixed number percentage:}}}'
-						+'{\\grp{\\html{&nbsp;}}}'
-						+'{\\grp{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}}',
-						answer: '\\grp{\\mixed{\\input{$answerFractionWhole}}{\\frac{\\input{$answerFractionNum}}{\\input{$answerFractionDen}}}}{\\str{%}}}}',		
+						problem: '\\col{\\row{\\str{Choose the reduced ratio with the same meaning as: }}}{\\row{\\str{\xA0}}}{\\row{\\str{ $problem1 to $problem2 }}',
+						answer: '\\css{\\butgrp{\\col{\\row{\\str{\xA0}}}{\\row{\\but{A}{F}}{\\frac{\\str{$labelNum1}}{\\str{$labelDen1}}}}{\\row{\\but{B}{F}}{\\frac{\\str{$labelNum2}}{\\str{$labelDen2}}}}{\\row{\\but{C}{F}}{\\frac{\\str{$labelNum3}}{\\str{$labelDen3}}}}{\\row{\\but{D}{T}}{\\frac{\\str{$labelNum4}}{\\str{$labelDen4}}}}}}{ chart-buttons-margin}}',
 						controls: {
 							"checkAnswer": true,
-							"help": '\\rowgrp'
-							+'{\\row{\\mixed{\\html{$fractionWhole}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}}'
-							+'{\\sign{ \xA0 = \xA0 }}'
-							+'{\\mixed{\\html{$answerFractionWhole}}{\\frac{\\fracstr{$answerFractionNum}}{\\fracstr{$answerFractionDen}}}}{\\str{%}}}'
-							+'{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}'
-							+'{css{\\mixed{\\html{$answerFractionWhole}}{\\frac{\\fracstr{$answerFractionNum}}{\\fracstr{$answerFractionDen}}}}{help-answer-text-tight}}{css{\\str{%}}{help-answer-text-tight}}}'
+							"help": '\\str{D: $problemNum to $problemDen}'
 						}
-						
 					}]
                 }]
             }
         },
         data = [
             { problemNum: '1', problemDen: '5', button1a: '2', button1b: '4', button2a: '2', button2b: '3', button3a: '4', button3b: '5', button4a: '1', button4b: '5', template: 'main' },
+			{ problem1: '7', problem2: '8', labelNum1: '2', labelDen1: '4', labelNum2: '2', labelDen2: '4', labelNum3: '2', labelDen3: '4', labelNum4: '2', labelDen4: '4', template: 'main' },
         ],
         interpolate = function (obj, data) {
             var string = JSON.stringify(obj);
