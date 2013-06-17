75f5a4943d706faa517d69ce08e86adb8553b0e2
Jay Peretz <jay@mytowncontractor.com>
2013-06-17 12:46:56 -0700

    added problem e.1 in 372

--- app/data372/data_e_1.js
+++ app/data372/data_e_1.js
@@ -1,9 +1,104 @@
 /*global angular */
 
-angular.module('mathSkills').value('data_e_1', {
-    title: 'E.1 Multiplication of Fractions',
-    path: 'E.1-multiplication-of-fractions',
-    children: [{
-        title: 'Multiplication of Fractions',
-        children: [] }]
+angular.module('mathSkills').service('data_e_1', function () {
+    var ret = {
+              title: 'E.1 Multiplication of Fractions',
+              path: 'E.1-multiplication-of-fractions',
+              children: []
+        },
+        template = {
+            main: {
+        title: 'Multiplying Fractions',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\row{\\html{Solve &nbsp;}}{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&times;}}{\\frac{\\str{$n2}}{\\str{$d2}}}',
+                answer: '\\frac{\\input{$nr}}{\\input{$dr}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Multiply the fractions together.}',
+                answer: '\\multiplyfracs{\\frac{\\str{$n1}}{\\str{$d1}}}{\\frac{\\str{$n2}}{\\str{$d2}}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": '\\row{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&times;}}{\\frac{\\str{$n2}}{\\str{$d2}}}{\\sign{&equals;}}{\\frac{\\str{$na}}{\\str{$da}}}'
+                }
+            }, {
+                problem: '\\str{Reduce the fraction to its lowest terms.}',
+                answer: '\\prev{feedbackData.getPrevious()}{\\reducefrac{\\frac{\\str{$na}}{\\str{$da}}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": '\\row{\\frac{\\str{$na}}{\\str{$da}}}{\\sign{&equals;}}{\\frac{\\str{$nr}}{\\str{$dr}}}'
+                }
+            }]
+        }]
+    } ,
+	reduced: {
+        title: 'Multiplying Fractions',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\row{\\html{Solve &nbsp;}}{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&times;}}{\\frac{\\str{$n2}}{\\str{$d2}}}',
+                answer: '\\frac{\\input{$na}}{\\input{$da}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Multiply the fractions together.}',
+                answer: '\\multiplyfracs{\\frac{\\str{$n1}}{\\str{$d1}}}{\\frac{\\str{$n2}}{\\str{$d2}}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": '\\row{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&times;}}{\\frac{\\str{$n2}}{\\str{$d2}}}{\\sign{&equals;}}{\\frac{\\str{$na}}{\\str{$da}}}'
+                }
+            }, {
+                problem: '\\str{Reduce the fraction to its lowest terms.}',
+                answer: '\\prev{feedbackData.getPrevious()}{\\reducefrac{\\frac{\\str{$na}}{\\str{$da}}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": '\\row{\\frac{\\str{$na}}{\\str{$da}}}{\\str{ is fully simplified.}}'
+                }
+            }]
+        }]
+    }
+        },
+        data = [
+			{n1:1,d1:4,n2:2,d2:6,na:2,da:24,nr:1,dr:12,template:'main'},
+			{n1:2,d1:4,n2:3,d2:8,na:3,da:16,template:'reduced'},
+			{n1:3,d1:5,n2:2,d2:10,na:6,da:50,nr:3,dr:25,template:'main'},
+			{n1:3,d1:4,n2:5,d2:8,na:15,da:32,template:'reduced'},
+			{n1:2,d1:10,n2:3,d2:6,na:6,da:60,nr:1,dr:10,template:'main'},
+			{n1:5,d1:6,n2:1,d2:2,na:5,da:12,template:'reduced'},
+			{n1:1,d1:4,n2:6,d2:8,na:6,da:32,nr:3,dr:16,template:'main'},
+			{n1:1,d1:4,n2:1,d2:8,na:1,da:32,template:'reduced'},
+			{n1:3,d1:4,n2:3,d2:6,na:9,da:24,nr:3,dr:8,template:'main'},
+			{n1:3,d1:5,n2:1,d2:2,na:3,da:10,template:'reduced'}			
+        ],
+        interpolate = function (obj, data) {
+            var string = JSON.stringify(obj);
+            for (var symbol in data) {
+                if (data.hasOwnProperty(symbol)) {
+                    string = string.replace(new RegExp('(\\$)?\\$' + symbol, 'g'), function ($0, $1) { return $1?$0 : data[symbol]; }); //console.log(string); 
+                }
+            }
+            var ret = JSON.parse(string);
+            ret.data = angular.copy(data);
+            return ret;
+        };
+
+    ret.children = data.map(function (problem) {
+        return interpolate(template[problem.template], problem);
+    });
+    
+    return ret;
 });
