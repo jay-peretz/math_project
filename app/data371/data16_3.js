9e882c1df12a5f42d9993d7cf9fb471f14f5d0c3
markafoote <mark@getthatwebsite.com>
2013-06-19 13:28:25 -0700

    16.3 prob 5 preliminaries

--- app/data371/data16_3.js
+++ app/data371/data16_3.js
@@ -246,10 +246,39 @@ angular.module('mathSkills').service('data16_3',['dataUtils', function (dataUtil
                     title: 'Main Answer',
                     children: [{
 						problem: '\\col{\\row{\\str{Solve for X:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\grp{\\html{&nbsp;}}{\\wholepart{X}}{\\str{$label1}}{\\html{&nbsp;}}}{\\grp{\\html{&nbsp;}}{\\frac{\\str{$problemNum2}}{\\str{$problemDen2}}}{\\str{$label2}}{\\html{&nbsp;}}}}{\\sign{\xA0 = \xA0}}{\\frac{\\grp{\\html{&nbsp;}}{\\frac{\\str{$problemNum3}}{\\str{$problemDen3}}}{\\str{$label3}}{\\html{&nbsp;}}}{\\grp{\\html{&nbsp;}}{\\frac{\\str{$problemNum4}}{\\str{$problemDen4}}}{\\str{$label4}}{\\html{&nbsp;}}}}}',
-						answer: '\\css{\\mixed{\\whole{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}}{margin-left-small}',
+						answer: '\\css{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}{margin-left-small}',
 						controls: {
 							"checkAnswer": true,
-							"help": '\\rowgrp{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{$problemNum1}}{\\str{$problemDen1}}}{\\sign{&sdot;}}{\\frac{\\str{$problemNum4}}{\\str{$problemDen4}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{$problemNum2}}{\\str{$problemDen2}}}{\\sign{&sdot;}}{\\frac{\\str{$problemNum3}}{\\str{$problemDen3}}}}{\\row{\\html{&nbsp;}}}{\\row{\\frac{\\str{$productNumLeft}}{\\str{$productDenLeft}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{$productNumRight}}{\\str{$productDenRight}}}}{\\row{\\html{&nbsp;}}}{\\row{\\str{$helpNum1}}{\\sign{&sdot;}}{\\str{$helpDen2}}{\\sign{\xA0 = \xA0}}{\\str{$helpDen1}}{\\sign{&sdot;}}{\\str{$helpNum2}}}{\\row{\\html{&nbsp;}}}{\\row{\\str{$productLeft}}{\\sign{\xA0 = \xA0}}{\\str{$productRight}}}{\\row{\\html{&nbsp;}}}{\\css{\\row{\\str{Answer: \xA0 }}{\\str{ $answerFirst }}}{help-answer-text}}{\\row{\\html{&nbsp;}}}'
+							"help": '\\rowgrp'
+							+'{\\row{\\str{\xA0}}}'
+							+'{\\row{\\wholepart{X}}'
+							+'{\\sign{&sdot;}}'
+							+'{\\frac{\\str{$problemNum4}}{\\str{$problemDen4}}}'
+							+'{\\sign{\xA0 = \xA0}}'
+							+'{\\frac{\\str{$problemNum2}}{\\str{$problemDen2}}}'
+							+'{\\sign{&sdot;}}'
+							+'{\\frac{\\str{$problemNum3}}{\\str{$problemDen3}}}}'
+							+'{\\row{\\html{&nbsp;}}}'
+							+'{\\row{\\wholepart{X}}'
+							+'{\\sign{&sdot;}}'
+							+'{\\frac{\\str{$problemNum4}}{\\str{$problemDen4}}}'
+							+'{\\sign{\xA0 = \xA0}}'
+							+'{\\frac{\\str{$productNumRight}}{\\str{$productDenRight}}}}'
+							+'{\\row{\\html{&nbsp;}}}'
+							+'{\\row{\\wholepart{X}}'
+							+'{\\sign{\xA0 = \xA0}}'
+							+'{\\frac{\\str{$productNumRight}}{\\str{$productDenRight}}}'
+							+'{\\sign{&divide;}}'
+							+'{\\frac{\\str{$problemNum4}}{\\str{$problemDen4}}}}'
+							+'{\\row{\\html{&nbsp;}}}'
+							+'{\\row{\\wholepart{X}}'
+							+'{\\sign{\xA0 = \xA0}}'
+							+'{\\mixed{\\str{$answerWhole}}{\\frac{\\str{$answerNum}}{\\str{$answerDen}}}}}'
+							+'{\\row{\\html{&nbsp;}}}'
+							+'{\\css{\\row{\\str{Answer: \xA0 }}'
+							+'{\\mixed{\\str{$answerWhole}}{\\frac{\\str{$answerNum}}{\\str{$answerDen}}}}}'
+							+'{help-answer-text}}'
+							+'{\\row{\\html{&nbsp;}}}'
 						}
 					}]
                 }]
@@ -261,7 +290,7 @@ angular.module('mathSkills').service('data16_3',['dataUtils', function (dataUtil
 			/*{ problemNum1: '3', problemDen1: '4', problemNum2: 'X', problemDen2: '8', answer: '6', template: 'xUpperRight' },*/
 			{ problemNum1: '4', problemDen1: '13', problemNum2: '2', problemDen2: 'X', answer: '6.5', template: 'xLowerRight' },
 			{ problemNum1: '8', problemDen1: '3', problemNum2: '16', problemDen2: 'X', answer: '6', label1: 'dyslexics', label2: 'math teachers', label3: 'dyslexics', label4: 'math teachers', template: 'xLowerRightFraction' },
-			{ problemNum1: '1', problemDen1: '', problemNum2: '2', problemDen2: '3', problemNum3: '5', problemDen3: '12', problemNum4: '5', problemDen4: '9', label1: 'apple', label2: 'orange',  label3: 'apple', label4: 'orange', productNumLeft: '5', productDenLeft: '18', equalOrNot: '=',  productNumRight: '10', productDenRight: '36', helpNum1: '5', helpDen1: '18', helpNum2: '10', helpDen2: '36', productLeft: '180', productRight: '180', answerWhole: '', answerNum: '', answerDen: '', template: 'xUpperLeftFractionFraction' },
+			{ problemNum1: '1', problemDen1: '', problemNum2: '2', problemDen2: '3', problemNum3: '5', problemDen3: '12', problemNum4: '5', problemDen4: '9', label1: 'apple', label2: 'orange',  label3: 'apple', label4: 'orange', productNumLeft: '', productDenLeft: '', productNumRight: '10', productDenRight: '36', answerWhole: '\xA0', answerNum: '1', answerDen: '2', template: 'xUpperLeftFractionFraction' },
         ];
     return dataUtils.build(ret, template, data);
 }]);
