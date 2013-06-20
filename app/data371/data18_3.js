abfb7dc0cdb1eaf09163bf95b0d1fc2ea9a0563d
markafoote <mark@getthatwebsite.com>
2013-06-20 09:14:09 -0700

    put 18_3 in new format, restart 20_1

--- app/data371/data18_3.js
+++ app/data371/data18_3.js
@@ -1,137 +1,55 @@
 /*global angular */
 
-angular.module('mathSkills').value('data18_3', {
-    title: '18.3 Change a Percent to a Decimal',
-    path: '18.3-change-a-percent-to-a-decimal',
-    children: [{
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this percent to a decimal:}}}{\\row{\\str{\xA0}}}{\\row{\\str{9}}{\\str{%}}',
-                answer: '\\row{\\input{0.09}}',
+angular.module('mathSkills').service('data18_3', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '18.3 Change a Percent to a Decimal',
+			path: '18.3-change-a-percent-to-a-decimal',
+            children: []
+        },
+        template = {
+            main: {
+                title: 'Change a Percent to a Decimal',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+                         problem: '\\rowgrp{\\row{\\str{Change this percent to a decimal:}}}{\\row{\\str{\xA0}}}{\\row{\\str{$startNumber}}{\\str{%}}',
+						answer: '\\row{\\input{$answer}}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp{\\row{\\percentarrows{$startNumber}{0.01}{percent}{decimal}}{\\css{\\str{Answer:  $answer}}{help-answer-text help-answer-margin-right}}}'
+						}
+                    }]
+                }]
+            },
+			fraction: {
+                title: 'Change a Percent to a Decimal',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+                         problem: '\\rowgrp{\\row{\\str{Change this fraction percent to a decimal percent:}}}{\\row{\\str{\xA0}}}{\\row{\\mixed{\\html{$problemWhole}}{\\frac{\\str{$problemNum}}{\\str{$problemDen}}}}{\\str{%}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round the decimal percent to the hundredths place.}}}',
+                answer: '\\row{\\input{$answer}}{\\str{%}}',
                 controls: {
                     "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\percentarrows{9}{0.01}{percent}{decimal}}{\\css{\\str{Answer:  0.09}}{help-answer-text help-answer-margin-right}}}'
+                    "help": '\\rowgrp{\\row{\\mixed{\\html{$problemWhole}}{\\frac{\\str{$problemNum}}{\\str{$problemDen}}}}{\\str{%}}{\\sign{ = }}{\\str{$answer%}}{\\css{\\str{Answer:  $answer%}}{help-answer-text help-answer-margin-right}}}'
                 }
-            }]
-        }]
-    }, {
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this percent to a decimal:}}}{\\row{\\str{\xA0}}}{\\row{\\str{327}}{\\str{%}}',
-                answer: '\\row{\\input{3.27}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\percentarrows{327}{0.01}{percent}{decimal}}{\\css{\\str{Answer:  3.27}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this percent to a decimal:}}}{\\row{\\str{\xA0}}}{\\row{\\str{85}}{\\str{%}}',
-                answer: '\\row{\\input{0.85}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\percentarrows{85}{0.01}{percent}{decimal}}{\\css{\\str{Answer:  0.85}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this percent to a decimal:}}}{\\row{\\str{\xA0}}}{\\row{\\str{55.6}}{\\str{%}}',
-                answer: '\\row{\\input{0.556}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\percentarrows{55.6}{0.01}{percent}{decimal}}{\\css{\\str{Answer:  0.556}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this percent to a decimal:}}}{\\row{\\str{\xA0}}}{\\row{\\str{67.85}}{\\str{%}}',
-                answer: '\\row{\\input{0.6785}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\percentarrows{67.85}{0.01}{percent}{decimal}}{\\css{\\str{Answer:  .6785}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this fraction percent to a decimal percent:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{5}}{\\str{7}}}{\\str{%}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round the decimal percent to the hundredths place.}}}',
-                answer: '\\row{\\input{0.71}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\frac{\\str{5}}{\\str{7}}}{\\str{%}}{\\sign{=}}{\\str{0.71%}}{\\css{\\str{Answer:  0.71%}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this fraction percent to a decimal percent:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{4}}{\\str{5}}}{\\str{%}}}',
-                answer: '\\row{\\input{0.8}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\frac{\\str{4}}{\\str{5}}}{\\str{%}}{\\sign{=}}{\\str{0.8%}}{\\css{\\str{Answer:  0.8%}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this fraction percent to a decimal percent:}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\str{2}}{\\str{3}}}{\\str{%}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round the decimal percent to the hundredths place.}}}',
-                answer: '\\row{\\input{0.67}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\frac{\\str{2}}{\\str{3}}}{\\str{%}}{\\sign{=}}{\\str{0.67%}}{\\css{\\str{Answer:  0.67%}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this fraction percent to a decimal percent:}}}{\\row{\\str{\xA0}}}{\\row{\\mixed{\\str{1}}{\\frac{\\fracstr{1}}{\\fracstr{4}}}}{\\str{%}}}',
-                answer: '\\row{\\input{1.25}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\mixed{\\str{1}}{\\frac{\\fracstr{1}}{\\fracstr{4}}}}{\\str{%}}{\\sign{=}}{\\str{1.25%}}{\\css{\\str{Answer:  1.25%}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change a Percent to a Decimal',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change this fraction percent to a decimal percent:}}}{\\row{\\str{\xA0}}}{\\row{\\mixed{\\str{1}}{\\frac{\\fracstr{1}}{\\fracstr{12}}}}{\\str{%}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round the decimal percent to the hundredths place.}}}',
-                answer: '\\row{\\input{1.08}}{\\str{%}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\mixed{\\str{1}}{\\frac{\\fracstr{1}}{\\fracstr{12}}}}{\\str{%}}{\\sign{=}}{\\str{1.08%}}{\\css{\\str{Answer:  1.08%}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }]
-});
+                    }]
+                }]
+            }
+        },
+        data = [
+            { startNumber: '9', answer: '0.09', template: 'main'  },
+			{ startNumber: '327', answer: '3.27', template: 'main'  },
+			{ startNumber: '85', answer: '0.85', template: 'main'  },
+			{ startNumber: '55.6', answer: '0.556', template: 'main'  },
+			{ startNumber: '67.85', answer: '0.6785', template: 'main'  },
+			{ problemWhole: '&nbsp;', problemNum: '5', problemDen: '7',  answer: '0.71', template: 'fraction'  },
+			{ problemWhole: '&nbsp;', problemNum: '4', problemDen: '5',  answer: '0.8', template: 'fraction'  },
+			{ problemWhole: '&nbsp;', problemNum: '2', problemDen: '3',  answer: '0.67', template: 'fraction'  },
+			{ problemWhole: '1', problemNum: '1', problemDen: '4',  answer: '1.25', template: 'fraction'  },
+			{ problemWhole: '1', problemNum: '1', problemDen: '12',  answer: '1.08', template: 'fraction'  },
+        ];
+
+
+    return dataUtils.build(desc, template, data);
+}]);
+
