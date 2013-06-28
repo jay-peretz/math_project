2956c393be05ec80450a6974bc13c0e51a21b7c7
Jay Peretz <jay@mytowncontractor.com>
2013-06-28 13:02:04 -0700

    started a.1

--- app/data372/data_a_1.js
+++ app/data372/data_a_1.js
@@ -1,5 +1,3 @@
-/*global angular */
-
 angular.module('mathSkills').service('data_a_1', ['dataUtils', function (dataUtils) {
     var desc = {
             title: 'A.1 Setting Up Expressions',
@@ -12,17 +10,8 @@ angular.module('mathSkills').service('data_a_1', ['dataUtils', function (dataUti
 				children: [{
 					title: 'Main Answer',
 					children: [{
-						problem: '\\row{\\str{$instructions}}',
-						answer: '\\butgrp'
-						+'{\\col{\\row{\\html{&nbsp;}}}'
-						+'{\\row{\\but{A}{$buttonValueA}}'	
-						+'{\\grp{\\html{\xA0 \xA0 $buttonLabelA}}}}'					
-						+'{\\row{\\but{B}{$buttonValueB}}'
-						+'{\\grp{\\html{\xA0 \xA0 $buttonLabelB}}}}'						
-						+'{\\row{\\but{C}{$buttonValueC}}'	
-						+'{\\grp{\\html{\xA0 \xA0 $buttonLabelC}}}}'				
-						+'{\\row{\\but{D}{$buttonValueD}}'
-						+'{\\grp{\\html{\xA0 \xA0 $buttonLabelD}}}}}',
+						problem: '\\rowgrp{\\str{Check all correct answers that apply.}}{\\str{\xA0}}{\\str{$instructions}}',
+						answer: '\\rowgrp{\\row{\\chk{$buttonLabelA}{$buttonValueA}}}{\\row{\\chk{$buttonLabelB}{$buttonValueB}}}{\\row{\\chk{$buttonLabelC}{$buttonValueC}}}{\\row{\\chk{$buttonLabelD}{$buttonValueD}}',
 						
 						controls: {
 							"checkAnswer": true,
@@ -35,8 +24,8 @@ angular.module('mathSkills').service('data_a_1', ['dataUtils', function (dataUti
 		       }
         },
         data = [
-			{ instructions: 'Seven plus four, then multiply by three',  buttonValueA: 'F', buttonLabelA: '7 + (4 + 3)', buttonValueB: 'T', buttonLabelB: '(7 + 4) * 3', buttonValueC: 'F', buttonLabelC: '7 + 4 * 3', buttonValueD: 'F', buttonLabelD: '(7 + 4) (3)',  template: 'expressions' },
-			{}		
-        ];
+		{instructions: 'Seven plus four, then multiply by three',  buttonValueA: 'F', buttonLabelA: '7 + (4 • 3)', buttonValueB: 'T', buttonLabelB: '(7 + 4) • 3', buttonValueC: 'F', buttonLabelC: '7 + 4 • 3', buttonValueD: 'T', buttonLabelD: '(7 + 4) (3)',  template: 'expressions' },
+		{instructions: 'Start with seven, then add the product of four and three.',  buttonValueA: 'T', buttonLabelA: '7 + (4 • 3)', buttonValueB: 'F', buttonLabelB: '(7 + 4) • 3', buttonValueC: 'F', buttonLabelC: '7 + 4 • 3', buttonValueD: 'F', buttonLabelD: '(7 + 4) (3)',  template: 'expressions' }
+		];
     return dataUtils.build(desc, template, data);
 }]);
