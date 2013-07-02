ce1cd8ab24dd4e86378bd0cfea914752c1e8ca1a
markafoote <mark@getthatwebsite.com>
2013-07-01 20:17:01 -0700

    1.3 to new format

--- app/data371/data1_3.js
+++ app/data371/data1_3.js
@@ -1,268 +1,67 @@
+
 /*global angular */
 
-angular.module('mathSkills').value('data1_3', {
-    title: '1.3 Comparing Whole Numbers',
-    path: '1.3-comparing-whole-numbers',
-    children: [{
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{80 \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 91}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{80 is LESS THAN 91}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{25}{33}{20}{35}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{X or 25 is LESS THAN Y or 33}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{85 \xA0}}{\\but{>}{T}}{\\str{\xA0 or \xA0}}{\\but{<}{F}}{\\str{\xA0 80}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{85 is GREATER THAN 80}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{53 \xA0}}{\\but{>}{T}}{\\str{\xA0 or \xA0}}{\\but{<}{F}}{\\str{\xA0 50}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{53 is GREATER THAN 50}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{T}}{\\str{\xA0 or \xA0}}{\\but{<}{F}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{56}{48}{45}{60}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{X or 56 is GREATER THAN Y or 48}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{54 \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 60}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{54 is LESS THAN 60}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{86 \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 93}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{86 is LESS THAN 93}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{T}}{\\str{\xA0 or \xA0}}{\\but{<}{F}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{86}{77}{72}{88}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{86 is MORE THAN 77}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{47 \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 57}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{47 is LESS THAN 57}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{83}{84}{72}{88}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{83 is LESS THAN 84}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{69 \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 70}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{69 is LESS THAN 70}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{15}{18}{10}{25}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{15 is LESS THAN 18}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{T}}{\\str{\xA0 or \xA0}}{\\but{<}{F}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{84}{78}{72}{88}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{84 is GREATER THAN 78}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{50 \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 53}}',
-                answer: '',
+angular.module('mathSkills').service('data1_3', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '1.3 Comparing Whole Numbers',
+			path: '1.3-comparing-whole-numbers',
+            children: []
+        },
+	        template = {
+            first: {
+                title: 'Comparing Whole Numbers',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\butgrp{\\row{\\str{$firstNumber \xA0}}{\\but{>}{$firstButton}}{\\str{\xA0 or \xA0}}{\\but{<}{$secondButton}}{\\str{\xA0 $secondNumber}}}',
+						answer: '',
+						controls: {
+							"checkAnswer": false,
+							"help": '\\str{$firstNumber is $statement $secondNumber}'
+						}
+                    }]
+                }]
+           },
+			second: {
+                title: 'Comparing Whole Numbers',
+				children: [{
+					title: 'Main Answer',
+					children: [{
+						problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{$firstButton}}{\\str{\xA0 or \xA0}}{\\but{<}{$secondButton}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{$firstNumber}{$secondNumber}{$startGraph}{$endGraph}}}',
+						answer: '',
+						controls: {
+							"checkAnswer": false,
+							"help": '\\str{X or $firstNumber is $statement Y or $secondNumber}'
+						}
+					}]
+                }]
+            }
+        },
+        data = [
+            { firstNumber: '80', secondNumber: '91', firstButton: 'F', secondButton: 'T', statement: 'LESS THAN', template: 'first' },
+			{ firstButton: 'F', secondButton: 'T', firstNumber: '25', secondNumber: '33', startGraph: '20', endGraph: '35', statement: 'LESS THAN', template: 'second' },
+            { firstNumber: '85', secondNumber: '80', firstButton: 'T', secondButton: 'F', statement: 'GREATER THAN', template: 'first' },
+			{ firstNumber: '53', secondNumber: '50', firstButton: 'T', secondButton: 'F', statement: 'GREATER THAN', template: 'first' },
+			{ firstButton: 'T', secondButton: 'F', firstNumber: '56', secondNumber: '48', startGraph: '45', endGraph: '60', statement: 'GREATER THAN', template: 'second' },
+			{ firstNumber: '54', secondNumber: '60', firstButton: 'F', secondButton: 'T', statement: 'LESS THAN', template: 'first' },
+			{ firstNumber: '85', secondNumber: '93', firstButton: 'F', secondButton: 'T', statement: 'LESS THAN', template: 'first' },
+			{ firstButton: 'T', secondButton: 'F', firstNumber: '86', secondNumber: '77', startGraph: '72', endGraph: '88', statement: 'MORE THAN', template: 'second' },
+			{ firstNumber: '47', secondNumber: '57', firstButton: 'F', secondButton: 'T', statement: 'LESS THAN', template: 'first' },
+			{ firstButton: 'F', secondButton: 'T', firstNumber: '83', secondNumber: '84', startGraph: '72', endGraph: '88', statement: 'LESS THAN', template: 'second' },
+			{ firstNumber: '69', secondNumber: '70', firstButton: 'F', secondButton: 'T', statement: 'LESS THAN', template: 'first' },
+			{ firstButton: 'F', secondButton: 'T', firstNumber: '15', secondNumber: '18', startGraph: '10', endGraph: '25', statement: 'LESS THAN', template: 'second' },
+			{ firstButton: 'T', secondButton: 'F', firstNumber: '84', secondNumber: '78', startGraph: '72', endGraph: '88', statement: 'MORE THAN', template: 'second' },
+			{ firstNumber: '50', secondNumber: '53', firstButton: 'F', secondButton: 'T', statement: 'LESS THAN', template: 'first' },
+			{ firstButton: 'F', secondButton: 'T', firstNumber: '18', secondNumber: '25', startGraph: '15', endGraph: '30', statement: 'LESS THAN', template: 'second' },
+			{ firstNumber: '54', secondNumber: '60', firstButton: 'F', secondButton: 'T', statement: 'LESS THAN', template: 'first' },
+			{ firstNumber: '86', secondNumber: '93', firstButton: 'F', secondButton: 'T', statement: 'LESS THAN', template: 'first' },
+			{ firstNumber: '95', secondNumber: '87', firstButton: 'T', secondButton: 'F', statement: 'GREATER THAN', template: 'first' },
+			{ firstButton: 'T', secondButton: 'F', firstNumber: '52', secondNumber: '48', startGraph: '45', endGraph: '60', statement: 'GREATER THAN', template: 'second' },
+			{ firstNumber: '26', secondNumber: '16', firstButton: 'T', secondButton: 'F', statement: 'GREATER THAN', template: 'first' },
+        ];
 
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{50 is LESS THAN 53}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{18}{25}{15}{30}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{50 is LESS THAN 53}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{54 \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 60}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{54 is LESS THAN 60}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{F}}{\\str{\xA0 or \xA0}}{\\but{<}{T}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{86}{93}{85}{98}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{86 is LESS THAN 93}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{95 \xA0}}{\\but{>}{T}}{\\str{\xA0 or \xA0}}{\\but{<}{F}}{\\str{\xA0 77}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{95 is GREATER THAN 77}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\col{\\butgrp{\\row{\\str{X \xA0}}{\\but{>}{T}}{\\str{\xA0 or \xA0}}{\\but{<}{F}}{\\str{\xA0 Y}}}}{\\row{\\str{\xA0}}}{\\row{\\comparewholesgraph{52}{48}{45}{60}}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{52 is GREATER THAN 48}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Comparing Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\butgrp{\\row{\\str{26 \xA0}}{\\but{>}{T}}{\\str{\xA0 or \xA0}}{\\but{<}{F}}{\\str{\xA0 15}}',
-                answer: '',
-                controls: {
-                    "checkAnswer": false,
-                    "help": '\\str{26 is GREATER THAN 16}'
-                }
-            }]
-        }]
-    }]
-});
+	return dataUtils.build(desc, template, data);
+}]);
+
+
+
