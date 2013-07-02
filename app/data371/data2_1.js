62f09f8236b80f6cb965a92ea342f57954408c00
markafoote <mark@getthatwebsite.com>
2013-07-02 13:31:39 -0700

    update 2.1 with dataUtils.build, remove styling of help text (except answer)

--- app/data371/data2_1.js
+++ app/data371/data2_1.js
@@ -1,126 +1,7 @@
 /*global angular */
+/*
 
-angular.module('mathSkills').value('data2_1', {
-    title: '2.1 Rounding Whole Numbers',
-    path: '2.1-rounding-whole-numbers',
-    children: [{
-        title: 'Rounding Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{tens place: }}}{\\row{\\str{471}}}',
-                answer: '\\input{470}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{471}{1}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Rounding Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{thousands place: }}}{\\row{\\str{1346}}}',
-                answer: '\\input{1000}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{1346}{3}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Rounding Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{tens place: }}}{\\row{\\str{684}}}',
-                answer: '\\input{680}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{684}{1}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Rounding Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{tens place: }}}{\\row{\\str{959}}}',
-                answer: '\\input{960}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{959}{1}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Rounding Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{hundreds place: }}}{\\row{\\str{461}}}',
-                answer: '\\input{500}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{461}{2}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Rounding Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{ten thousands place: }}}{\\row{\\str{3990495}}}',
-                answer: '\\input{3990000}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{3990495}{4}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Rounding Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{ten thousands place: }}}{\\row{\\str{27523018}}}',
-                answer: '\\input{27520000}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{27523018}{4}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Rounding Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{hundred thousands place: }}}{\\row{\\str{563548}}}',
-                answer: '\\input{600000}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{563548}{5}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Rounding Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{tens place: }}}{\\row{\\str{230179}}}',
-                answer: '\\input{230180}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{230179}{1}{showanswer}'
-                }
-            }]
-        }]
-    }, {
+  
         title: 'Rounding Whole Numbers',
         children: [{
             title: 'Main Answer',
@@ -135,3 +16,48 @@ angular.module('mathSkills').value('data2_1', {
         }]
     }]
 });
+*/
+
+/*global angular */
+
+angular.module('mathSkills').service('data2_1', ['dataUtils', function (dataUtils) {
+    var desc = {
+		    title: '2.1 Rounding Whole Numbers',
+			path: '2.1-rounding-whole-numbers',
+            children: []
+        },
+	        template = {
+            first: {
+                title: 'Rounding Whole Numbers',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp{\\row{\\str{Round this whole number to the}}}{\\row{\\str{$placeValue place: }}}{\\row{\\str{$problemNumber}}}',
+						answer: '\\input{$answerNumber}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\placevalue{$problemNumber}{$roundPosition}{showanswer}'
+						}
+                    }]
+                }]
+           }
+        },
+        data = [
+            { placeValue: 'tens', problemNumber: '471', answerNumber: '470', roundPosition: '1', template: 'first' },
+			{ placeValue: 'thousands', problemNumber: '1346', answerNumber: '1000', roundPosition: '3', template: 'first' },
+			{ placeValue: 'tens', problemNumber: '684', answerNumber: '680', roundPosition: '1', template: 'first' },
+			{ placeValue: 'tens', problemNumber: '959', answerNumber: '960', roundPosition: '1', template: 'first' },
+			{ placeValue: 'hundreds', problemNumber: '461', answerNumber: '500', roundPosition: '2', template: 'first' },
+			{ placeValue: 'ten thousands', problemNumber: '3990495', answerNumber: '3990000', roundPosition: '4', template: 'first' },
+			{ placeValue: 'ten thousands', problemNumber: '27523018', answerNumber: '27520000', roundPosition: '4', template: 'first' },
+			{ placeValue: 'hundred thousands', problemNumber: '563548', answerNumber: '600000', roundPosition: '5', template: 'first' },
+			{ placeValue: 'tens', problemNumber: '230179', answerNumber: '230180', roundPosition: '1', template: 'first' },
+			{ placeValue: 'hundreds', problemNumber: '49952', answerNumber: '50000', roundPosition: '2', template: 'first' }
+        ];
+
+	return dataUtils.build(desc, template, data);
+}]);
+
+
+
+
