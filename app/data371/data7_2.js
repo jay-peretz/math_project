abe8811fca63ad3075ce24a811ee266843700da6
markafoote <mark@getthatwebsite.com>
2013-07-03 16:00:43 -0700

    convert 7.2 to dataUtils.build format

--- app/data371/data7_2.js
+++ app/data371/data7_2.js
@@ -1,137 +1,103 @@
 /*global angular */
 
-angular.module('mathSkills').value('data7_2', {
-    title: '7.2 Find the Reciprocal of a Fraction',
-    path: '7.2-find-the-reciprocal-of-a-fraction',
-    children: [{
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\frac{\\str{1}}{\\str{4}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\input{4}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{1}}{\\str{4}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{4}}{\\str{1}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\str{ 4}}{label_like}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\frac{\\str{2}}{\\str{3}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\frac{\\input{3}}{\\input{2}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{2}}{\\str{3}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{3}}{\\str{2}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\frac{\\str{3}}{\\str{2}}}{label_like}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\frac{\\str{9}}{\\str{10}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\frac{\\input{10}}{\\input{9}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{9}}{\\str{10}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{10}}{\\str{9}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\frac{\\str{10}}{\\str{9}}}{label_like}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\frac{\\str{13}}{\\str{30}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\frac{\\input{30}}{\\input{13}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{13}}{\\str{30}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{30}}{\\str{13}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\frac{\\str{30}}{\\str{13}}}{label_like}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\frac{\\str{1}}{\\str{2}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\input{2}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{1}}{\\str{2}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{2}}{\\str{1}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\str{ 2}}{label_like}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\frac{\\str{3}}{\\str{10}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\frac{\\input{10}}{\\input{3}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{3}}{\\str{10}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{10}}{\\str{3}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\frac{\\str{10}}{\\str{3}}}{label_like}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\frac{\\str{6}}{\\str{4}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\frac{\\input{2}}{\\input{3}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{6}}{\\str{4}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{4}}{\\str{6}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\frac{\\fracstr{2}}{\\fracstr{3}}}{label_like}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\frac{\\str{3}}{\\str{4}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\frac{\\input{4}}{\\input{3}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{3}}{\\str{4}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{4}}{\\str{3}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\frac{\\str{4}}{\\str{3}}}{label_like}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\mixed{\\str{1}}{\\frac{\\fracstr{4}}{\\fracstr{6}}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\frac{\\input{3}}{\\input{5}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\mixed{\\str{1}}{\\frac{\\fracstr{4}}{\\fracstr{6}}}}{\\str{\xA0 equals \xA0}}{\\frac{\\str{10}}{\\str{6}}}}{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{10}}{\\str{6}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{6}}{\\str{10}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\frac{\\fracstr{3}}{\\fracstr{5}}}{label_like}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Find the Reciprocal of a Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the reciprocal of \xA0}}{\\mixed{\\str{1}}{\\frac{\\fracstr{5}}{\\fracstr{6}}}}{\\str{ ?}}}{\\row{\\str{Simplify if necessary.}}}}',
-                answer: '\\frac{\\input{6}}{\\input{11}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\mixed{\\str{1}}{\\frac{\\fracstr{5}}{\\fracstr{6}}}}{\\str{\xA0 equals \xA0}}{\\frac{\\str{11}}{\\str{6}}}}{\\row{\\str{The reciprocal of \xA0}}{\\frac{\\str{11}}{\\str{6}}}{\\str{\xA0 is \xA0}}{\\frac{\\str{6}}{\\str{11}}}{css{\\str{answer:  \xA0}}{label_like margin-left-small}}{css{\\frac{\\fracstr{6}}{\\fracstr{11}}}{label_like}}}'
-                }
-            }]
-        }]
-    }]
-});
+angular.module('mathSkills').service('data7_2', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '7.2 Find the Reciprocal of a Fraction',
+			path: '7.2-find-the-reciprocal-of-a-fraction',
+            children: []
+        },
+	        template = {
+				integerAnswer: {
+					title: 'Find the Reciprocal of a Fraction',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\rowgrp'
+							+'{\\row{\\str{What is the reciprocal of \xA0}}'
+							+'{\\mixed{\\html{$problemWhole}}{\\frac{\\fracstr{$problemNum}}{\\fracstr{$problemDen}}}}'
+							+'{\\str{ ?}}}'
+							+'{\\row{\\str{Simplify if necessary.}}}}',
+							answer: '\\mixed{\\input{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\row{\\str{The reciprocal of \xA0}}'
+								+'{\\frac{\\fracstr{$helpNumber}}{\\fracstr{$problemDen}}}'
+								+'{\\str{\xA0 is \xA0}}'
+								+'{\\frac{\\fracstr{$problemDen}}{\\fracstr{$helpNumber}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{answer:  \xA0}}'
+								+'{\\str{$answerWhole}}}{help-answer-text}}'
+							}
+						}]
+					}]
+			   },
+			   fractionAnswer: {
+					title: 'Find the Reciprocal of a Fraction',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\rowgrp'
+							+'{\\row{\\str{What is the reciprocal of \xA0}}'
+							+'{\\frac{\\fracstr{$problemNum}}{\\fracstr{$problemDen}}}'
+							+'{\\str{ ?}}}'
+							+'{\\row{\\str{Simplify if necessary.}}}}',
+							answer: '\\frac{\\input{$answerNum}}{\\input{$answerDen}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\row{\\str{The reciprocal of \xA0}}'
+								+'{\\frac{\\fracstr{$problemNum}}{\\fracstr{$problemDen}}}'
+								+'{\\str{\xA0 is \xA0}}'
+								+'{\\frac{\\fracstr{$problemDen}}{\\fracstr{$problemNum}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{answer:  \xA0}}'
+								+'{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}{help-answer-text}}'
+							}
+						}]
+					}]
+			   },
+			   mixedSimplified: {
+					title: 'Find the Reciprocal of a Fraction',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\rowgrp'
+							+'{\\row{\\str{What is the reciprocal of \xA0}}'
+							+'{\\mixed{\\html{$problemWhole}}{\\frac{\\fracstr{$problemNum}}{\\fracstr{$problemDen}}}}'
+							+'{\\str{ ?}}}'
+							+'{\\row{\\str{Simplify if necessary.}}}}',
+							answer: '\\frac{\\input{$answerNum}}{\\input{$answerDen}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\row{\\mixed{\\html{$problemWhole}}{\\frac{\\fracstr{$problemNum}}{\\fracstr{$problemDen}}}}'
+								+'{\\str{\xA0 equals \xA0}}'
+								+'{\\frac{\\fracstr{$helpNumber}}{\\fracstr{$problemDen}}}}'
+								+'{\\row{\\str{The reciprocal of \xA0}}'
+								+'{\\frac{\\fracstr{$helpNumber}}{\\fracstr{$problemDen}}}'
+								+'{\\str{\xA0 is \xA0}}'
+								+'{\\frac{\\fracstr{$problemDen}}{\\fracstr{$helpNumber}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{answer:  \xA0}}'
+								+'{\\mixed{\\html{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}}{help-answer-text}}'
+							}
+						}]
+					}]
+				}
+		},
+        data = [
+			{ problemWhole: '', problemNum: '1', problemDen: '4', answerWhole: '4', answerNum: '', answerDen: '', helpNumber: '1', template: 'integerAnswer' },
+			{ problemNum: '2', problemDen: '3', answerNum: '3', answerDen: '2', template: 'fractionAnswer' },
+            { problemWhole: '1', problemNum: '4', problemDen: '6', answerWhole: '', answerNum: '3', answerDen: '5', helpNumber: '10', template: 'mixedSimplified' },
+			{ problemNum: '9', problemDen: '10', answerNum: '10', answerDen: '9', template: 'fractionAnswer' },
+			{ problemNum: '13', problemDen: '30', answerNum: '30', answerDen: '13', template: 'fractionAnswer' },
+			{ problemWhole: '1', problemNum: '5', problemDen: '6', answerWhole: '', answerNum: '6', answerDen: '11', helpNumber: '11', template: 'mixedSimplified' },
+			{ problemWhole: '', problemNum: '1', problemDen: '2', answerWhole: '2', answerNum: '', answerDen: '', helpNumber: '1', template: 'integerAnswer' },
+			{ problemNum: '3', problemDen: '10', answerNum: '10', answerDen: '3', template: 'fractionAnswer' },
+			{ problemNum: '6', problemDen: '4', answerNum: '2', answerDen: '3', template: 'fractionAnswer' },
+			{ problemWhole: '2', problemNum: '3', problemDen: '8', answerWhole: '', answerNum: '8', answerDen: '19', helpNumber: '19', template: 'mixedSimplified' },
+        ];
+
+	return dataUtils.build(desc, template, data);
+}]);
