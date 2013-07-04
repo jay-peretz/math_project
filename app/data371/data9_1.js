4de097317af292cc513b54318a6c406429831297
markafoote <mark@getthatwebsite.com>
2013-07-04 12:44:39 -0700

    convert 9.1 to use dataUtils.build format

--- app/data371/data9_1.js
+++ app/data371/data9_1.js
@@ -1,137 +1,137 @@
 /*global angular */
 
-angular.module('mathSkills').value('data9_1', {
-    title: '9.1 Add and Subtract Like Fractions',
-    path: '9.1-add-and-subtract-like-fractions  ',
-    children: [{ //----1
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{2}}{\\str{4}}}{\\sign{-}}{\\frac{\\str{1}}{\\str{4}}',
-                answer: '\\grp{\\sign{=}}{\\frac{\\input{1}}{\\input{4}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{2}}{\\str{4}}}{\\sign{-}}{\\frac{\\str{1}}{\\str{4}}}{\\sign{=}}{\\frac{\\str{1}}{\\str{4}}}'
-                }
-            }]
-        }]
-    }, { //----2
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{6}}{\\str{8}}}{\\sign{-}}{\\frac{\\str{2}}{\\str{8}}',
-                answer: '\\grp{\\sign{=}}{\\frac{\\input{1}}{\\input{2}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{6}}{\\str{8}}}{\\sign{-}}{\\frac{\\str{2}}{\\str{8}}}{\\sign{=}}{\\frac{\\str{1}}{\\str{2}}}'
-                }
-            }]
-        }]
-    }, { //----3
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{6}}{\\str{9}}}{\\sign{+}}{\\frac{\\str{2}}{\\str{9}}',
-                answer: '\\grp{\\sign{=}}{\\frac{\\input{8}}{\\input{9}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{6}}{\\str{9}}}{\\sign{+}}{\\frac{\\str{2}}{\\str{9}}}{\\sign{=}}{\\frac{\\str{8}}{\\str{9}}}'
-                }
-            }]
-        }]
-    }, { //----4
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{15}}{\\str{25}}}{\\sign{+}}{\\frac{\\str{9}}{\\str{25}}',
-                answer: '\\grp{\\sign{=}}{\\frac{\\input{24}}{\\input{25}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{15}}{\\str{25}}}{\\sign{+}}{\\frac{\\str{9}}{\\str{25}}}{\\sign{=}}{\\frac{\\str{24}}{\\str{25}}}'
-                }
-            }]
-        }]
-    }, { //----5
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{2}}{\\str{6}}}{\\sign{+}}{\\frac{\\str{3}}{\\str{6}}',
-                answer: '\\grp{\\sign{=}}{\\frac{\\input{5}}{\\input{6}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{2}}{\\str{6}}}{\\sign{+}}{\\frac{\\str{3}}{\\str{6}}}{\\sign{=}}{\\frac{\\str{5}}{\\str{6}}}'
-                }
-            }]
-        }]
-    }, { //----6
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{12}}{\\str{13}}}{\\sign{-}}{\\frac{\\str{4}}{\\str{13}}',
-                answer: '\\grp{\\sign{=}}{\\frac{\\input{8}}{\\input{13}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{12}}{\\str{13}}}{\\sign{-}}{\\frac{\\str{4}}{\\str{13}}}{\\sign{=}}{\\frac{\\str{8}}{\\str{13}}}'
-                }
-            }]
-        }]
-    }, { //----7
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{28}}{\\str{42}}}{\\sign{-}}{\\frac{\\str{9}}{\\str{42}}',
-                answer: '\\grp{\\sign{=}}{\\frac{\\input{19}}{\\input{42}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{28}}{\\str{42}}}{\\sign{-}}{\\frac{\\str{9}}{\\str{42}}}{\\sign{=}}{\\frac{\\str{19}}{\\str{42}}}'
-                }
-            }]
-        }]
-    }, { //----8
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{40}}{\\str{66}}}{\\sign{-}}{\\frac{\\str{15}}{\\str{66}}',
-                answer: '\\grp{\\sign{=}}{\\frac{\\input{25}}{\\input{66}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{40}}{\\str{66}}}{\\sign{-}}{\\frac{\\str{15}}{\\str{66}}}{\\sign{=}}{\\frac{\\str{25}}{\\str{66}}}'
-                }
-            }]
-        }]
-    }, { //----9
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{30}}{\\str{49}}}{\\sign{+}}{\\frac{\\str{30}}{\\str{49}}',
-                answer: '\\grp{\\sign{=}}{\\mixed{\\input{1}}{\\frac{\\input{11}}{\\input{49}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{30}}{\\str{49}}}{\\sign{+}}{\\frac{\\str{30}}{\\str{49}}}{\\sign{=}}{\\mixed{\\str{1}}{\\frac{\\str{11}}{\\str{49}}}}'
-                }
-            }]
-        }]
-    }, { //----10
-        title: 'Add and Subtract Like Fractions',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\frac{\\str{30}}{\\str{77}}}{\\sign{+}}{\\frac{\\str{35}}{\\str{77}}',
-                answer: '\\grp{\\sign{=}}{\\frac{\\input{65}}{\\input{77}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\str{30}}{\\str{77}}}{\\sign{+}}{\\frac{\\str{35}}{\\str{77}}}{\\sign{=}}{\\frac{\\str{65}}{\\str{77}}}'
-                }
-            }]
-        }]
-    }, ]
-});
+angular.module('mathSkills').service('data9_1', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '9.1 Add and Subtract Like Fractions',
+			path: '9.1-add-and-subtract-like-fractions  ',
+            children: []
+        },
+	        template = {
+				answerDirect: {
+					title: 'Add and Subtract Like Fractions',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\row'
+							+'{\\frac{\\str{$firstNum}}{\\str{$firstDen}}}'
+							+'{\\sign{$operation}}'
+							+'{\\frac{\\str{$secondNum}}{\\str{$secondDen}}',
+							answer: '\\grp{\\sign{=}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\row{\\frac{\\str{$firstNum}}{\\str{$firstDen}}}'
+								+'{\\sign{$operation}}'
+								+'{\\frac{\\str{$secondNum}}{\\str{$secondDen}}}'
+								+'{\\sign{ = }}'
+								+'{\\frac{\\str{$answerNum}}{\\str{$answerDen}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{Answer:  \xA0}}'
+								+'{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}'
+								+'{help-answer-text}}'
+							}
+						}]
+					}]
+			   },
+			answerSimplified: {
+					title: 'Add and Subtract Like Fractions',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\row'
+							+'{\\frac{\\str{$firstNum}}{\\str{$firstDen}}}'
+							+'{\\sign{$operation}}'
+							+'{\\frac{\\str{$secondNum}}{\\str{$secondDen}}',
+							answer: '\\grp{\\sign{=}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\row{\\frac{\\str{$firstNum}}{\\str{$firstDen}}}'
+								+'{\\sign{$operation}}'
+								+'{\\frac{\\str{$secondNum}}{\\str{$secondDen}}}'
+								+'{\\sign{ = }}'
+								+'{\\frac{\\str{$helpNum}}{\\str{$firstDen}}}'
+								+'{\\sign{=}}'
+								+'{\\frac{\\str{$answerNum}}{\\str{$answerDen}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{Answer:  \xA0}}'
+								+'{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}'
+								+'{help-answer-text}}'
+							}
+						}]
+					}]
+			   },
+			   answerMixed: {
+					title: 'Add and Subtract Like Fractions',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\row'
+							+'{\\frac{\\str{$firstNum}}{\\str{$firstDen}}}'
+							+'{\\sign{$operation}}'
+							+'{\\frac{\\str{$secondNum}}{\\str{$secondDen}}',
+							answer: '\\grp{\\sign{=}}{\\mixed{\\input{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\row{\\frac{\\str{$firstNum}}{\\str{$firstDen}}}'
+								+'{\\sign{$operation}}'
+								+'{\\frac{\\str{$secondNum}}{\\str{$secondDen}}}'
+								+'{\\sign{ = }}'
+								+'{\\frac{\\str{$helpNum}}{\\str{$answerDen}}}'
+								+'{\\sign{ = }}'
+								+'{\\mixed{\\str{$answerWhole}}{\\frac{\\str{$answerNum}}{\\str{$answerDen}}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{Answer:  \xA0}}'
+								+'{\\mixed{\\fracstr{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}}'
+								+'{help-answer-text}}'
+							}
+						}]
+					}]
+			   },
+			   answerMixedSimplified: {
+					title: 'Add and Subtract Like Fractions',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\row'
+							+'{\\frac{\\str{$firstNum}}{\\str{$firstDen}}}'
+							+'{\\sign{$operation}}'
+							+'{\\frac{\\str{$secondNum}}{\\str{$secondDen}}',
+							answer: '\\grp{\\sign{=}}{\\mixed{\\input{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\row{\\frac{\\str{$firstNum}}{\\str{$firstDen}}}'
+								+'{\\sign{$operation}}'
+								+'{\\frac{\\str{$secondNum}}{\\str{$secondDen}}}'
+								+'{\\sign{ = }}'
+								+'{\\frac{\\str{$helpNum}}{\\str{$firstDen}}}'
+								+'{\\sign{ = }}'
+								+'{\\mixed{\\str{$answerWhole}}{\\frac{\\str{$answerNum}}{\\str{$answerDen}}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{Answer:  \xA0}}'
+								+'{\\mixed{\\fracstr{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}}'
+								+'{help-answer-text}}'
+							}
+						}]
+					}]
+			   }
+		},
+        data = [
+			{ firstNum: '2', firstDen: '4', secondNum: '1', secondDen: '4', operation: '&minus;', answerNum: '1', answerDen: '4', template: 'answerDirect' },
+			{ firstNum: '6', firstDen: '8', secondNum: '2', secondDen: '8', operation: '&minus;', helpNum: '4', answerNum: '1', answerDen: '2', template: 'answerSimplified' },
+			{ firstNum: '6', firstDen: '9', secondNum: '2', secondDen: '9', operation: '+', answerNum: '8', answerDen: '9', template: 'answerDirect' },
+			{ firstNum: '15', firstDen: '25', secondNum: '9', secondDen: '25', operation: '+', answerNum: '24', answerDen: '25', template: 'answerDirect' },
+			{ firstNum: '2', firstDen: '6', secondNum: '3', secondDen: '6', operation: '+', answerNum: '5', answerDen: '6', template: 'answerDirect' },
+			{ firstNum: '12', firstDen: '13', secondNum: '4', secondDen: '13', operation: '&minus;', answerNum: '8', answerDen: '13', template: 'answerDirect' },
+			{ firstNum: '28', firstDen: '42', secondNum: '7', secondDen: '42', operation: '&minus;', helpNum: '21', answerNum: '1', answerDen: '2', template: 'answerSimplified' },
+			{ firstNum: '40', firstDen: '66', secondNum: '15', secondDen: '66', operation: '&minus;', answerNum: '25', answerDen: '66', template: 'answerDirect' },
+			{  firstNum: '3', firstDen: '8', secondNum: '7', secondDen: '8', operation: '+', answerWhole: '1', answerNum: '1', answerDen: '4', helpNum: '10', template: 'answerMixedSimplified' },
+			{  firstNum: '30', firstDen: '49', secondNum: '30', secondDen: '49', operation: '+', answerWhole: '1', answerNum: '11', answerDen: '49', helpNum: '60', template: 'answerMixed' },
+        ];
+
+	return dataUtils.build(desc, template, data);
+}]);
+
+
