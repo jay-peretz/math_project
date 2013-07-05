c6e75c0f4443cd07cf061010dc64fc5abc484c67
markafoote <mark@getthatwebsite.com>
2013-07-04 20:39:03 -0700

    convert 12.1 & 12.2 to use dataUtils.build

--- app/data371/data12_1.js
+++ app/data371/data12_1.js
@@ -1,137 +1,39 @@
 /*global angular */
 
-angular.module('mathSkills').value('data12_1', {
-    title: '12.1 Place Value Number System of Decimal Numbers',
-    path: '12.1-place-value-number-system-of-decimal-numbers',
-    children: [{
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{2.97551}{4}}}',
-                answer: '\\select{Hundredths}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Hundredths"}{2.97551}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Hundredths}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{37928.46101}{9}}}',
-                answer: '\\select{Thousands}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Thousands"}{37928.46101}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Thousands}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{0.1569}{2}}}',
-                answer: '\\select{Thousandths}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Thousandths"}{0.1569}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Thousandths}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{86.38456}{6}}}',
-                answer: '\\select{Ones}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Ones"}{86.38456}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Ones}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{9956.52}{2}}}',
-                answer: '\\select{Tenths}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Tenths"}{9956.52}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Tenths}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{83.20524}{1}}}',
-                answer: '\\select{Hundred-thousandths}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Hundred-thousandths"}{2.97551}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Hundred-thousandths}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{972.6571}{1}}}',
-                answer: '\\select{Ten-thousandths}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Ten-thousandths"}{972.6571}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Ten-thousandths}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{0.659}{4}}}',
-                answer: '\\select{Ones}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Ones"}{0.659}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Ones}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{2.78929}{3}}}',
-                answer: '\\select{Thousandths}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Thousandths"}{2.78929}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Thousandths}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Decimal Number Place Value',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{0.85237}{4}}}',
-                answer: '\\select{Hundredths}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\showplacedecimal{"Hundredths"}{0.85237}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  Hundredths}}{label_like margin-left-small}}}}'
-                }
-            }]
-        }]
-    }, ]
-});
+angular.module('mathSkills').service('data12_1', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '12.1 Place Value Number System of Decimal Numbers',
+			path: '12.1-place-value-number-system-of-decimal-numbers',
+            children: []
+        },
+	        template = {
+            main: {
+                title: 'Place Value Number System of Decimal Numbers',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp{\\row{\\str{Identify the place value of the indicated digit: }}}{\\row{\\placevalue{$problemNumber}{$placeNumber}}}',
+						answer: '\\select{$placeValue}{["Millionths", "Hundred-thousandths", "Ten-thousandths", "Thousandths", "Hundredths", "Tenths", "Ones", "Tens", "Hundreds", "Thousands", "Ten Thousands", "Hundred Thousands", "Millions", "Ten Millions", "Hundred Millions", "Billions", "Ten Billions", "Hundred Billions"]}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp{\\row{\\showplacedecimal{"$placeValue"}{$problemNumber}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{answer:  $placeValue}}{label_like margin-left-small}}}}'
+						}
+                    }]
+                }]
+            }
+        },
+        data = [
+            { problemNumber: '2.97551', placeNumber: '4', placeValue: 'Hundredths', template: 'main' },
+			{ problemNumber: '37928.46101', placeNumber: '9', placeValue: 'Thousands', template: 'main' },
+            { problemNumber: '0.1569', placeNumber: '2', placeValue: 'Thousandths', template: 'main' },
+			{ problemNumber: '86.38456', placeNumber: '6', placeValue: 'Ones', template: 'main' },
+            { problemNumber: '9956.52', placeNumber: '2', placeValue: 'Tenths', template: 'main' },
+			{ problemNumber: '83.20524', placeNumber: '1', placeValue: 'Hundred-thousandths', template: 'main' },
+            { problemNumber: '972.6571', placeNumber: '1', placeValue: 'Ten-thousandths', template: 'main' },
+			{ problemNumber: '0.659', placeNumber: '4', placeValue: 'Ones', template: 'main' },
+            { problemNumber: '2.78929', placeNumber: '3', placeValue: 'Thousandths', template: 'main' },
+			{ problemNumber: '0.85237', placeNumber: '4', placeValue: 'Hundredths', template: 'main' }
+        ];
+
+    return dataUtils.build(desc, template, data);
+}]);
--- app/data371/data12_2.js
+++ app/data371/data12_2.js
@@ -1,137 +1,138 @@
 /*global angular */
 
-angular.module('mathSkills').value('data12_2', {
-    title: '12.2 Change Decimals to Fractions and Mixed Numbers',
-    path: '12.2-change-decimals-to-fractions-and-mixed-numbers',
-    children: [{
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{0.6}}}}',
-                answer: '\\frac{\\input{3}}{\\input{5}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\fracstr{6}}{\\fracstr{10}}}{\\str{ \xA0 \xA0 = \xA0 \xA0 }}{\\frac{\\fracstr{3}}{\\fracstr{5}}}{\\str{ \xA0 \xA0 \xA0 \xA0 }}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{3}}{\\fracstr{5}}}{help-answer-text-tight}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{0.3}}}}',
-                answer: '\\frac{\\input{3}}{\\input{10}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{3}}{\\fracstr{10}}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{0.57}}}}',
-                answer: '\\frac{\\input{57}}{\\input{100}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{57}}{\\fracstr{100}}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{8.4}}}}',
-                answer: '\\mixed{\\input{8}}{\\frac{\\input{2}}{\\input{5}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\fracstr{84}}{\\fracstr{10}}}{\\str{ \xA0 \xA0 = \xA0 \xA0 }}{\\mixed{\\str{8}}{\\frac{\\fracstr{2}}{\\fracstr{5}}}}{\\row{css{\\str{Answer:  \xA0 \xA0}}{help-answer-text}}{css{\\mixed{\\str{8}}{\\frac{\\fracstr{2}}{\\fracstr{5}}}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{0.9}}}}',
-                answer: '\\frac{\\input{9}}{\\input{10}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{9}}{\\fracstr{10}}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{2.69}}}}',
-                answer: '\\mixed{\\input{2}}{\\frac{\\input{69}}{\\input{100}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\fracstr{269}}{\\fracstr{100}}}{\\str{ \xA0 \xA0 = \xA0 \xA0 }}{\\mixed{\\str{2}}{\\frac{\\fracstr{69}}{\\fracstr{100}}}}{\\row{css{\\str{Answer:  \xA0 \xA0}}{help-answer-text}}{css{\\mixed{\\str{2}}{\\frac{\\fracstr{69}}{\\fracstr{100}}}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{0.43}}}}',
-                answer: '\\frac{\\input{43}}{\\input{100}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{43}}{\\fracstr{100}}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{0.68}}}}',
-                answer: '\\frac{\\input{17}}{\\input{25}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\fracstr{68}}{\\fracstr{100}}}{\\str{ \xA0 \xA0 = \xA0 \xA0 }}{\\frac{\\fracstr{17}}{\\fracstr{25}}}{\\str{ \xA0 \xA0 \xA0 \xA0 }}{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{17}}{\\fracstr{25}}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{0.17}}}}',
-                answer: '\\frac{\\input{17}}{\\input{100}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{css{\\str{Answer: \xA0 \xA0}}{help-answer-text}}{css{\\frac{\\fracstr{17}}{\\fracstr{100}}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Decimals to Fractions and Mixed Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}}{\\row{\\str{2.87}}}}',
-                answer: '\\mixed{\\input{2}}{\\frac{\\input{87}}{\\input{100}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{Write the place value name as a fraction;}}}{\\row{\\str{reduce if necessary.}}}{\\row{\\str{\xA0}}}{\\row{\\frac{\\fracstr{287}}{\\fracstr{100}}}{\\str{ \xA0 \xA0 = \xA0 \xA0 }}{\\mixed{\\str{2}}{\\frac{\\fracstr{87}}{\\fracstr{100}}}}{\\row{css{\\str{Answer:  \xA0 \xA0}}{help-answer-text}}{css{\\mixed{\\str{2}}{\\frac{\\fracstr{87}}{\\fracstr{100}}}}{help-answer-text-tight}}}'
-                }
-            }]
-        }]
-    }, ]
-});
+angular.module('mathSkills').service('data12_2', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '12.2 Change Decimals to Fractions and Mixed Numbers',
+			path: '12.2-change-decimals-to-fractions-and-mixed-numbers',
+            children: []
+        },
+	        template = {
+				fractionDirect: {
+					title: 'Change Decimals to Fractions and Mixed Numbers',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\rowgrp'
+							+'{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\str{$problemDecimal}}',
+							answer: '\\frac{\\input{$answerNum}}{\\input{$answerDen}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\str{Write the place value name as a fraction;}}'
+								+'{\\str{reduce if necessary.}}'
+								+'{\\html{&nbsp;}}'
+								+'{\\row{\\str{$problemDecimal}}'
+								+'{\\sign{\xA0 = \xA0}}'
+								+'{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{Answer:  \xA0}}'
+								+'{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}'
+								+'{help-answer-text}}'							}
+						}]
+					}]
+			   },
+			fractionSimplified: {
+					title: 'Change Decimals to Fractions and Mixed Numbers',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\rowgrp'
+							+'{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\str{$problemDecimal}}',
+							answer: '\\frac{\\input{$answerNum}}{\\input{$answerDen}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\str{Write the place value name as a fraction;}}'
+								+'{\\str{reduce if necessary.}}'
+								+'{\\html{&nbsp;}}'
+								+'{\\row{\\str{$problemDecimal}}'
+								+'{\\sign{\xA0 = \xA0}}'
+								+'{\\frac{\\fracstr{$helpNum}}{\\fracstr{$helpDen}}}'
+								+'{\\sign{\xA0 = \xA0}}'
+								+'{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{Answer:  \xA0}}'
+								+'{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}'
+								+'{help-answer-text}}'		
+							}
+						}]
+					}]
+			   },
+			   mixedDirect: {
+					title: 'Change Decimals to Fractions and Mixed Numbers',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\rowgrp'
+							+'{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\str{$problemDecimal}}',
+							answer: '\\mixed{\\input{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\str{Write the place value name as a fraction;}}'
+								+'{\\str{reduce if necessary.}}'
+								+'{\\html{&nbsp;}}'
+								+'{\\row{\\str{$problemDecimal}}'
+								+'{\\sign{\xA0 = \xA0}}'
+								+'{\\mixed{\\str{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{Answer:  \xA0}}'
+								+'{\\mixed{\\str{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}}'
+								+'{help-answer-text}}'							}
+						}]
+					}]
+			   },
+			mixedSimplified: {
+					title: 'Change Decimals to Fractions and Mixed Numbers',
+					children: [{
+						title: 'Main Answer',
+						children: [{
+							problem: '\\rowgrp'
+							+'{\\str{What is the fraction of this decimal (reduced to lowest terms)?}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\str{$problemDecimal}}',
+							answer: '\\mixed{\\input{$answerWhole}}{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}',
+							controls: {
+								"checkAnswer": true,
+								"help": '\\rowgrp'
+								+'{\\str{Write the place value name as a fraction;}}'
+								+'{\\str{reduce if necessary.}}'
+								+'{\\html{&nbsp;}}'
+								+'{\\row{\\str{$problemDecimal}}'
+								+'{\\sign{\xA0 = \xA0}}'
+								+'{\\mixed{\\str{$answerWhole}}{\\frac{\\fracstr{$helpNum}}{\\fracstr{$helpDen}}}}'
+								+'{\\sign{\xA0 = \xA0}}'
+								+'{\\mixed{\\str{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}}'
+								+'{\\html{&nbsp;}}'
+								+'{css{\\row{\\str{Answer:  \xA0}}'
+								+'{\\mixed{\\str{$answerWhole}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}}'
+								+'{help-answer-text}}'		
+							}
+						}]
+					}]
+			   }
+		},
+        data = [
+			{ problemDecimal: '0.3', answerNum: '3', answerDen: '10', template: 'fractionDirect' },
+			{ problemDecimal: '0.6', answerNum: '3', answerDen: '5', helpNum: '6', helpDen: '10', template: 'fractionSimplified' },
+			{ problemDecimal: '0.57', answerNum: '57', answerDen: '100', template: 'fractionDirect' },
+			{ problemDecimal: '8.4', answerWhole: '8', answerNum: '2', answerDen: '5', helpNum: '4', helpDen: '10',template: 'mixedSimplified' },
+			{ problemDecimal: '3.9', answerWhole: '3', answerNum: '9', answerDen: '10', template: 'mixedDirect' },
+			{ problemDecimal: '2.69', answerWhole: '2', answerNum: '69', answerDen: '100', template: 'mixedDirect' },
+			{ problemDecimal: '0.44', answerNum: '11', answerDen: '25', helpNum: '44', helpDen: '100', template: 'fractionSimplified' },
+			{ problemDecimal: '0.43', answerNum: '43', answerDen: '100', template: 'fractionDirect' },
+			{ problemDecimal: '3.125', answerWhole: '3', answerNum: '1', answerDen: '8', helpNum: '125', helpDen: '1000',template: 'mixedSimplified' },
+			{ problemDecimal: '2.101', answerWhole: '2', answerNum: '101', answerDen: '1000',template: 'mixedDirect' },
+        ];
+
+	return dataUtils.build(desc, template, data);
+}]);
+
+
+
