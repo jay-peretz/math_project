67596e715dbdedbde1e95501f76ada2430ea6ecd
markafoote <mark@getthatwebsite.com>
2013-07-06 15:44:27 -0700

    9.3, convert to dataUtils.build format

--- app/data371/data9_3.js
+++ app/data371/data9_3.js
@@ -1,247 +1,63 @@
 /*global angular */
 
-angular.module('mathSkills').value('data9_3', {
-    title: '9.3 Build an Equivalent Fraction',
-    path: '9.3-build-an-equivalent-fraction',
-    children: [{
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{3}}{\\str{5}}}}{\\row{\\str{ and build an equivalent fraction that has a denominator of 20.}}}',
-                answer: '\\frac{\\input{12}}{\\fracstr{20}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{3}}{\\str{5}}}{\\frac{\\input{4}}{\\input{4}}}}{\\frac{\\input{12}}{\\fracstr{20}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{3}}{\\fracstr{5}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{4}}{\\fracstr{4}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{12}}{\\fracstr{20}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{7}}{\\str{6}}}}{\\row{\\str{ and build an equivalent fraction that has a numerator of 21.}}}',
-                answer: '\\frac{\\input{21}}{\\fracstr{18}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{7}}{\\str{6}}}{\\frac{\\input{3}}{\\input{3}}}}{\\frac{\\input{21}}{\\fracstr{18}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{7}}{\\fracstr{6}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{3}}{\\fracstr{3}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{21}}{\\fracstr{18}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{11}}{\\str{9}}}}{\\row{\\str{ and build an equivalent fraction that has a numerator of 22.}}}',
-                answer: '\\frac{\\input{22}}{\\fracstr{18}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{11}}{\\str{9}}}{\\frac{\\input{2}}{\\input{2}}}}{\\frac{\\input{22}}{\\fracstr{18}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{11}}{\\fracstr{9}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{2}}{\\fracstr{2}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{22}}{\\fracstr{18}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{11}}{\\str{8}}}}{\\row{\\str{ and build an equivalent fraction that has a denominator of 24.}}}',
-                answer: '\\frac{\\input{33}}{\\fracstr{24}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{11}}{\\str{8}}}{\\frac{\\input{3}}{\\input{3}}}}{\\frac{\\input{33}}{\\fracstr{24}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{11}}{\\fracstr{8}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{3}}{\\fracstr{3}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{33}}{\\fracstr{24}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{3}}{\\str{4}}}}{\\row{\\str{ and build an equivalent fraction that has a denominator of 12.}}}',
-                answer: '\\frac{\\input{9}}{\\fracstr{12}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{3}}{\\str{4}}}{\\frac{\\input{3}}{\\input{3}}}}{\\frac{\\input{9}}{\\fracstr{12}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{3}}{\\fracstr{4}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{3}}{\\fracstr{3}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{9}}{\\fracstr{12}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{3}}{\\str{8}}}}{\\row{\\str{ and build an equivalent fraction that has a numerator of 12.}}}',
-                answer: '\\frac{\\input{12}}{\\fracstr{32}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{3}}{\\str{8}}}{\\frac{\\input{4}}{\\input{4}}}}{\\frac{\\input{12}}{\\fracstr{32}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{3}}{\\fracstr{8}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{4}}{\\fracstr{4}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{12}}{\\fracstr{32}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{5}}{\\str{10}}}}{\\row{\\str{ and build an equivalent fraction that has a numerator of 35.}}}',
-                answer: '\\frac{\\input{35}}{\\fracstr{70}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{5}}{\\str{10}}}{\\frac{\\input{7}}{\\input{7}}}}{\\frac{\\input{35}}{\\fracstr{70}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{5}}{\\fracstr{10}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{7}}{\\fracstr{7}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{35}}{\\fracstr{70}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{10}}{\\str{15}}}}{\\row{\\str{ and build an equivalent fraction that has a denominator of 90.}}}',
-                answer: '\\frac{\\input{60}}{\\fracstr{90}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{10}}{\\str{15}}}{\\frac{\\input{6}}{\\input{6}}}}{\\frac{\\input{60}}{\\fracstr{90}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{10}}{\\fracstr{15}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{6}}{\\fracstr{6}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{60}}{\\fracstr{90}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{9}}{\\str{12}}}}{\\row{\\str{ and build an equivalent fraction that has a numerator of 18.}}}',
-                answer: '\\frac{\\input{18}}{\\fracstr{24}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{9}}{\\str{12}}}{\\frac{\\input{2}}{\\input{2}}}}{\\frac{\\input{18}}{\\fracstr{24}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{9}}{\\fracstr{12}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{2}}{\\fracstr{2}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{18}}{\\fracstr{24}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Build an Equivalent Fraction',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Start with the fraction \xA0}}{\\frac{\\str{6}}{\\str{25}}}}{\\row{\\str{ and build an equivalent fraction that has a denominator of 100.}}}',
-                answer: '\\frac{\\input{24}}{\\fracstr{100}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\row{\\str{Multiply numerator and denominator by the same number: \xA0}}}',
-                answer: '\\row{\\buildequivfracs{\\frac{\\str{6}}{\\str{25}}}{\\frac{\\input{4}}{\\input{4}}}}{\\frac{\\input{24}}{\\fracstr{100}}}}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\row{\\frac{\\fracstr{6}}{\\fracstr{25}}}{\\str{\xA0 times \xA0}}{\\frac{\\fracstr{4}}{\\fracstr{4}}}{\\str{\xA0 equals \xA0}}{\\frac{\\fracstr{24}}{\\fracstr{100}}}'
-                }
-            }]
-        }]
-    }]
-});
+angular.module('mathSkills').service('data9_3', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '9.3 Build an Equivalent Fraction',
+			path: '9.3-build-an-equivalent-fraction',
+            children: []
+        },
+	        template = {
+            main: {
+                title: 'Build an Equivalent Fraction',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp'
+						+'{\\str{Start with the fraction:}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\frac{\\fracstr{$problemNum}}{\\fracstr{$problemDen}}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\str{ and build an equivalent fraction that has a denominator of $newDen.}}',
+						answer: '\\grp{\\frac{\\fracstr{$problemNum}}{\\fracstr{$problemDen}}}'
+						+'{\\sign{= \xA0}}'
+						+'{\\frac{\\input{$answer}}{\\fracstr{$newDen}}}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\row{\\frac{\\fracstr{$problemNum}}{\\fracstr{$problemDen}}}{\\sign{\xA0 = \xA0}}{\\frac{\\str{?}}{\\fracstr{$newDen}}}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\row{\\str{$problemDen \xA0 times \xA0 ?}}{\\sign{\xA0 = \xA0}}{\\str{ $newDen}}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\str{Multiply numerator and denominator by the number whose product with $problemDen is $newDen:}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\row{\\frac{\\fracstr{$problemNum}}{\\fracstr{$problemDen}}}'
+							+'{\\sign{&dot;}}'
+							+'{\\frac{\\fracstr{$multiplier}}{\\fracstr{$multiplier}}}'
+							+'{\\sign{\xA0 = \xA0}}'
+							+'{\\frac{\\str{$answer}}{\\str{$newDen}}}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\css{\\str{\xA0 Answer: \xA0 $answer}}{help-answer-text}}'
+						}
+                    }]
+                }]
+			}
+		},
+        data = [
+            { problemNum: '3', problemDen: '5', newDen: '20', multiplier: '4', answer: '12', template: 'main' },
+			{ problemNum: '7', problemDen: '6', newDen: '18', multiplier: '3', answer: '21', template: 'main' },
+			{ problemNum: '3', problemDen: '4', newDen: '12', multiplier: '3', answer: '9', template: 'main' },
+			{ problemNum: '2', problemDen: '5', newDen: '10', multiplier: '2', answer: '4', template: 'main' },
+			{ problemNum: '3', problemDen: '8', newDen: '32', multiplier: '4', answer: '12', template: 'main' },
+			{ problemNum: '1', problemDen: '2', newDen: '8', multiplier: '4', answer: '4', template: 'main' },
+			{ problemNum: '5', problemDen: '6', newDen: '12', multiplier: '2', answer: '10', template: 'main' },
+			{ problemNum: '1', problemDen: '12', newDen: '60', multiplier: '5', answer: '5', template: 'main' },
+			{ problemNum: '4', problemDen: '9', newDen: '36', multiplier: '4', answer: '16', template: 'main' },
+			{ problemNum: '9', problemDen: '10', newDen: '100', multiplier: '10', answer: '90', template: 'main' }			
+        ];
+
+    return dataUtils.build(desc, template, data);
+}]);
+
+
+
+
