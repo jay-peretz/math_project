8517f28c6ff95f40bbcbb7f4ed893305b073a8df
Justin Doolaege <justindool@sbcglobal.net>
2013-07-06 22:08:41 -0400

    new ms-btn and ms-btn-grp

---
+++ app/js/directives/ms-btn-grp.js
@@ -0,0 +1,64 @@
+'use strict';
+/*global angular, jQuery */
+
+angular.module('mathSkills')
+    .config(['parserProvider', function (parserProvider) {
+        parserProvider.register('btngrp', {
+            directiveTemplate: '<ms-btn-grp expected={{expected}} label={{label}}></ms-btn-grp>'
+        });
+    }])
+  .directive('msBtnGrp', [
+    'parser',
+    function (parser) {
+        return {
+            controller: [
+                'directiveUtils',
+                '$scope',
+                '$element',
+                function (directiveUtils, $scope, $element) {
+                    $scope.controllerId = Math.random().toString();
+                    $scope.correctAnswer = '';
+                    
+                    $scope.$watch('expected', function () {
+                        if ($scope.expected) {
+                            
+                            $scope.childElements = parser.extractTag($scope.expected).args;
+                            $scope.children = $scope.childElements.map(function (val, ii) {
+                                return ii.toString();
+                            });
+    
+                            // Find the correct answer amongst the contained buttons.
+                            parser.find($scope.expected, 'but').forEach(function (button) {
+                                if (button.args[1] === 'T') {
+                                    $scope.correctAnswer = button.args[0];
+                                }
+                            });
+                        }
+                    });
+    
+                    $scope.$on('answer', function (e, data) { //console.log("button answer obj", data);  
+                        // If this event was not fired by us.
+                        if (data.controllerId !== $scope.controllerId) {
+                            // Stop this event from going up the scope chain.
+                            e.stopPropagation();
+    
+                            if (data.clicked === true){
+                                data.expected = '\\but{' + $scope.correctAnswer + '}{T}'; 
+                                data.controllerId = $scope.controllerId;
+                                $scope.$emit('answer', data);
+                            }
+                        }
+                    });
+                    
+                    directiveUtils.routeFocus($scope);
+                    directiveUtils.routeHelp($scope);
+                }
+            ],
+            restrict: 'E',
+            scope: {
+                expected: '@',
+                label: '@'
+            },
+            template: '<div><table><tr><td ng-repeat="child in childElements"><ms-expression expected={{child}} label={{$index}}></ms-expression></td></tr></table></div>'    
+        };
+    }]);
---
+++ app/js/directives/ms-btn.js
@@ -0,0 +1,101 @@
+'use strict';
+/*global angular */
+
+angular.module('mathSkills')
+    .config(['parserProvider', function (parserProvider) {
+        parserProvider.register('btn', {
+            directiveTemplate: '<ms-btn expected={{expected}} label={{label}}></ms-btn>'
+        });
+    }])
+    .directive('msBtn', [
+        'parser',
+        function (parser) {
+            return {
+                controller: function ($scope, $element) {
+                    $scope.controllerId = Math.random().toString();
+                    $scope.clicked = false;
+
+                    // Extract the args array.
+                    $scope.$watch('expected', function () {
+                        if ($scope.expected){
+                            $scope.args = parser.extractTag($scope.expected).args;
+                        }
+                    });
+                     
+                    // handle check answer event for parent scope.
+                    $scope.$on ('checkAnswer', function (e){
+                        if (e.defaultPrevented === false){
+                            
+                            var data = {
+                                expected: $scope.expected,
+                                answer: '\\but{' + $scope.args[0] + '}{' + $scope.args[1] + '}',
+                                label: $scope.label
+                            };
+                            if ($scope.clicked === true){
+                                if ($scope.args[1] === "T") {
+                                    data.result = 'correct';
+                                    $scope.class = 'success';
+                                } else {
+                                    data.result = 'incorrect';
+                                    $scope.class = 'danger';
+                                }
+                                data.clicked = true;
+                            }else{
+                                data.clicked = false;
+                                $scope.class = '';
+                            }
+                            data.controllerId = $scope.controllerId;
+                            $scope.clicked = false;
+                            $scope.$emit('answer', data);
+                        }
+                    });
+                    
+                    $scope.$on('checkFocus', function (e) { 
+                        // If this event has not been marked as ignored.
+                        if (e.defaultPrevented === false) {
+                            // handle check focus event for parent scope.
+                            // set focus on button element, fire a focused event.
+                            $element.find('.btn').focus();  console.log('ran check focus', $scope.args[0]);
+                            $scope.$emit('focused', {
+                                controllerId: $scope.controllerId
+                            });
+                        }
+                    });
+
+                    $scope.$on('checkHelp', function (e) {
+                        // If this event has not been marked as ignored.
+                        if (e.defaultPrevented === false) {
+                            // handle check help event for parent scope.
+                            // button does not use help, fire a notHelped event.
+                            
+                            if ($scope.args[1] === "T") {
+                                $scope.class = 'success';
+                                $scope.clicked = false;
+                                $scope.$emit('notHelped', {
+                                    controllerId: $scope.controllerId
+                                });
+                                
+                            } else {
+                                $scope.class = '';
+                                $scope.clicked = false;
+                                $scope.$emit('notHelped', {
+                                    controllerId: $scope.controllerId
+                                });
+                            }
+                        }
+                    });
+
+                    $element.on('click', 'button', function (event) {
+                        $scope.clicked = true;
+                        $scope.$apply($scope.$emit('triggerCheckAnswer'));
+                    });
+                },
+                restrict: 'E',
+                scope: {
+                    expected: '@',
+                    label: '@'
+                },
+                template: '<button class="btn btn-{{class}}" type=button>{{args[0]}}</button>'
+            };
+        }
+    ]);
