8a33dcfb7d63523a72bdb508b78e5ab7a7bc7db0
markafoote <mark@getthatwebsite.com>
2013-07-09 16:20:27 -0700

    convert 13.2 to dataUtils.build format

--- app/data371/data13_2.js
+++ app/data371/data13_2.js
@@ -1,137 +1,46 @@
 /*global angular */
 
-angular.module('mathSkills').value('data13_2', {
-    title: '13.2 Subtract Decimal Numbers',
-    path: '13.2-subtract-decimal-numbers',
-    children: [{
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{math}{partial}{17.3}{8.22}}}',
-                answer: '\\input{9.08}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{17.3}{8.22}}{\\css{\\str{Answer:  9.08}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{math}{partial}{3}{0.12}}}',
-                answer: '\\input{2.88}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{3}{0.12}}{\\css{\\str{Answer:  2.88}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{math}{partial}{8.42}{5.75}}}',
-                answer: '\\input{2.67}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{8.42}{5.75}}{\\css{\\str{Answer:  2.67}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{english}{partial}{8.7}{5.33}}}',
-                answer: '\\input{3.37}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{8.7}{5.33}}{\\css{\\str{Answer:  3.37}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{math}{partial}{27.15}{2.75}}}',
-                answer: '\\input{24.4}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{27.15}{2.75}}{\\css{\\str{Answer:  24.4}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{english}{partial}{22.1}{3.8}}}',
-                answer: '\\input{18.3}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{22.1}{3.8}}{\\css{\\str{Answer:  18.3}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{math}{partial}{458.5}{84.158}}}',
-                answer: '\\input{374.342}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{458.5}{84.158}}{\\css{\\str{Answer:  374.342}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{english}{partial}{292.653}{6.32}}}',
-                answer: '\\input{286.333}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{292.653}{6.32}}{\\css{\\str{Answer:  286.333}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{english}{partial}{9.28}{5.84}}}',
-                answer: '\\input{3.39}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{9.28}{5.84}}{\\css{\\str{Answer:  3.39}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Subtract Decimal Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Find the difference of the decimal numbers:}}}{\\row{\\subtractdecimals{math}{partial}{955.707}{87.998}}}',
-                answer: '\\input{867.709}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\subtractdecimals{math}{complete}{955.707}{87.998}}{\\css{\\str{Answer:  867.709}}{help-answer-text}}}'
-                }
-            }]
-        }]
-    }]
-});
+angular.module('mathSkills').service('data13_2', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '13.2 Subtract Decimal Numbers',
+			path: '13.2-subtract-decimal-numbers',
+            children: []
+        },
+	    template = {
+            twoDecimals: {
+                title: 'Subtract Decimal Numbers',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp'
+						+'{\\str{Find the difference of the decimal numbers:}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\subtractdecimals{$questionLayout}{partial}{$firstDecimal}{$secondDecimal}}',
+						answer: '\\input{$answer}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\html{&nbsp;}}'
+							+'{\\subtractdecimals{math}{complete}{$firstDecimal}{$secondDecimal}}'
+							+'{\\css{\\grp{\\str{Answer: \xA0}}{\\str{$answer}}}{help-answer-text}}'
+						}
+                    }]
+                }]
+            }
+        },
+        data = [
+            { questionLayout: 'math', firstDecimal: '17.3', secondDecimal: '8.22', answer: '9.08', template: 'twoDecimals' },
+			{ questionLayout: 'math', firstDecimal: '3', secondDecimal: '0.12', answer: '2.88', template: 'twoDecimals' },
+			{ questionLayout: 'math', firstDecimal: '8.42', secondDecimal: '5.75', answer: '2.67', template: 'twoDecimals' },
+			{ questionLayout: 'english', firstDecimal: '8.7', secondDecimal: '5.33', answer: '3.37', template: 'twoDecimals' },
+			{ questionLayout: 'math', firstDecimal: '27.15', secondDecimal: '2.75', answer: '24.4', template: 'twoDecimals' },
+			{ questionLayout: 'english', firstDecimal: '22.1', secondDecimal: '3.8', answer: '18.3', template: 'twoDecimals' },
+			{ questionLayout: 'math', firstDecimal: '458.5', secondDecimal: '84.158', answer: '374.342', template: 'twoDecimals' },
+			{ questionLayout: 'english', firstDecimal: '292.653', secondDecimal: '6.32', answer: '286.333', template: 'twoDecimals' },
+			{ questionLayout: 'math', firstDecimal: '9.28', secondDecimal: '5.24', answer: '4.04', template: 'twoDecimals' },
+			{ questionLayout: 'math', firstDecimal: '955.707', secondDecimal: '87.998', answer: '867.709', template: 'twoDecimals' },
+        ];
+
+    return dataUtils.build(desc, template, data);
+}]);
+ 
