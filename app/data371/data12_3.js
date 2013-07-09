1ecccc5ea5d60994a4b684c483a30f53a523e0d6
markafoote <mark@getthatwebsite.com>
2013-07-09 12:35:59 -0700

    convert 12.3 to dataUtils.build format

--- app/data371/data12_3.js
+++ app/data371/data12_3.js
@@ -1,137 +1,45 @@
 /*global angular */
 
-angular.module('mathSkills').value('data12_3', {
-    title: '12.3 Round Decimals',
-    path: '12.3-round-decimals',
-    children: [{
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Hundredths place}}}{\\row{\\str{0.96139}}}}',
-                answer: '\\input{0.96}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{0.96139}{4}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Tenths place}}}{\\row{\\str{5.52887}}}}',
-                answer: '\\input{5.5}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{5.52887}{5}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Hundredths place}}}{\\row{\\str{6.2412}}}}',
-                answer: '\\input{6.24}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{6.2412}{3}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Ten-Thousandths place}}}{\\row{\\str{1.96139}}}}',
-                answer: '\\input{1.9614}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{1.96139}{2}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Ones place}}}{\\row{\\str{1.68}}}}',
-                answer: '\\input{2}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{1.68}{3}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Tenths place}}}{\\row{\\str{7.19824}}}}',
-                answer: '\\input{7.2}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{7.19824}{5}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Tenths place}}}{\\row{\\str{10.9563}}}}',
-                answer: '\\input{11.0}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{10.9563}{4}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Tens place}}}{\\row{\\str{49.1}}}}',
-                answer: '\\input{50}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{49.1}{3}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Hundredths place}}}{\\row{\\str{16.4118}}}}',
-                answer: '\\input{16.41}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{16.4118}{3}{showanswer}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Round Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Round this decimal number to the indicated place value: Nearest Thousandths place}}}{\\row{\\str{7.7195}}}}',
-                answer: '\\input{7.720}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\placevalue{7.7195}{2}{showanswer}'
-                }
-            }]
-        }]
-    }, ]
-});
+angular.module('mathSkills').service('data12_3', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '12.3 Round Decimals',
+			path: '12.3-round-decimals',
+            children: []
+        },
+	        template = {
+            main: {
+                title: 'Round Decimals',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp'
+						+'{\\str{Round this decimal number to the indicated place value:}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\str{$problemNumber}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\str{Nearest $placeValue place}}',
+						answer: '\\input{$answer}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\placevalue{$problemNumber}{$placeNumber}{showanswer}'
+						}
+                    }]
+                }]
+            }
+        },
+        data = [
+            { problemNumber: '0.96139', placeNumber: '4', placeValue: 'Hundredths', answer: '0.96', template: 'main' },
+			{ problemNumber: '5.52887', placeNumber: '5', placeValue: 'Tenths', answer: '5.5', template: 'main' },
+			{ problemNumber: '6.2412', placeNumber: '3', placeValue: 'Hundredths', answer: '6.24', template: 'main' },
+			{ problemNumber: '1.96139', placeNumber: '2', placeValue: 'Ten-Thousandths', answer: '1.9614', template: 'main' },
+			{ problemNumber: '1.68', placeNumber: '3', placeValue: 'Ones', answer: '2', template: 'main' },
+			{ problemNumber: '7.19824', placeNumber: '5', placeValue: 'Tenths', answer: '7.2', template: 'main' },
+			{ problemNumber: '10.9563', placeNumber: '4', placeValue: 'Tenths', answer: '11.0', template: 'main' },
+			{ problemNumber: '49.1', placeNumber: '3', placeValue: 'Tens', answer: '50', template: 'main' },
+			{ problemNumber: '7.7195', placeNumber: '2', placeValue: 'Thousandths', answer: '7.720', template: 'main' },
+			{ problemNumber: '16.4118', placeNumber: '3', placeValue: 'Hundredths', answer: '16.41', template: 'main' }
+        ];
+
+    return dataUtils.build(desc, template, data);
+}]);
+              
