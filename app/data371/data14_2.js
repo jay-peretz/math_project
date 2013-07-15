1b6dbdec55ef1cb6e37ab1d32d6d4498c6ca402b
markafoote <mark@getthatwebsite.com>
2013-07-15 12:25:43 -0700

    convert 14.2 to dataUtils.build format

--- app/data371/data14_2.js
+++ app/data371/data14_2.js
@@ -1,137 +1,39 @@
 /*global angular */
+ 
+angular.module('mathSkills').service('data14_2', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '14.2 Multiply by Powers of Ten',
+			path: '14.2-multiply-by-powers-of-ten',
+            children: []
+        },
+        template = {
+			main: {
+                title: 'Multiply by Powers of Ten',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{$number \($multiplier\)}}}',
+						answer: '\\input{$answer}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp{\\row{\\multiplydecpowten{$number}{$multiplier}}{\\css{\\str{Answer:  $helpAnswer}}{help-answer-text help-answer-margin-right}}}'
+						}
+                    }]
+                }]
+            }
+        },
+        data = [
+            { number: '833', multiplier: '100', answer: '83300', helpAnswer: '83,300', template: 'main' },
+			{ number: '722', multiplier: '10', answer: '7220', helpAnswer: '7,220', template: 'main' },
+			{ number: '0.1', multiplier: '1000', answer: '100', helpAnswer: '100', template: 'main' },
+			{ number: '3.36', multiplier: '1000', answer: '3360', helpAnswer: '3,360', template: 'main' },
+			{ number: '82', multiplier: '0.001', answer: '0.082', helpAnswer: '0.082', template: 'main' },
+			{ number: '0.1', multiplier: '0.001', answer: '0.0001', helpAnswer: '0.0001', template: 'main' },
+			{ number: '38.5', multiplier: '0.001', answer: '0.0385', helpAnswer: '0.0385', template: 'main' },
+			{ number: '80.62', multiplier: '0.00001', answer: '0.0008062', helpAnswer: '0.0008062', template: 'main' },
+			{ number: '988', multiplier: '1000', answer: '988000', helpAnswer: '988,000', template: 'main' },
+			{ number: '7.6', multiplier: '0.01', answer: '0.076', helpAnswer: '0.076', template: 'main' }       
+       ];
 
-angular.module('mathSkills').value('data14_2', {
-    title: '14.2 Multiply by Powers of Ten',
-    path: '14.2-multiply-by-powers-of-ten',
-    children: [{
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{833 \(100\)}}}',
-                answer: '\\input{83300}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{833}{100}}{\\css{\\str{Answer:  83300}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{722 \(10\)}}}',
-                answer: '\\input{7220}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{722}{10}}{\\css{\\str{Answer:  7220}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{0.1 \(1000\)}}}',
-                answer: '\\input{100}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{0.1}{1000}}{\\css{\\str{Answer:  100}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{3.36 \(1000\)}}}',
-                answer: '\\input{3360}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{3.36}{1000}}{\\css{\\str{Answer:  3360}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{82 \(0.001\)}}}',
-                answer: '\\input{0.082}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{82}{0.001}}{\\css{\\str{Answer:  0.082}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{0.1 \(0.001\)}}}',
-                answer: '\\input{0.0001}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{0.1}{0.001}}{\\css{\\str{Answer:  0.0001}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{38.5 \(0.001\)}}}',
-                answer: '\\input{0.0385}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{38.5}{0.001}}{\\css{\\str{Answer:  0.0385}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{80.62 \(0.00001\)}}}',
-                answer: '\\input{0.0008062}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{80.62}{0.00001}}{\\css{\\str{Answer:  0.0008062}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{988 \(1000\)}}}',
-                answer: '\\input{988000}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{988}{1000}}{\\css{\\str{Answer:  988000}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Multiply by Powers of Ten',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Determine the product of:}}}{\\row{\\str{\xA0}}}{\\row{\\str{7.6 \(0.01\)}}}',
-                answer: '\\input{0.076}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\multiplydecpowten{7.6}{0.01}}{\\css{\\str{Answer:  0.076}}{help-answer-text help-answer-margin-right}}}'
-                }
-            }]
-        }]
-    }]
-});
+    return dataUtils.build(desc, template, data);
+}]);
