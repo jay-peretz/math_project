cb5429155c2d67ae21c007f4c470712be0759da0
markafoote <mark@getthatwebsite.com>
2013-07-15 18:29:16 -0700

    convert 15.1 to dataUtils.build format

--- app/data371/data15_1.js
+++ app/data371/data15_1.js
@@ -1,140 +1,61 @@
 /*global angular */
+ 
+angular.module('mathSkills').service('data15_1', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '15.1 Change Fractions to Decimals',
+    		path: '15.1-change-fractions-to-decimals',
+            children: []
+        },
+        template = {
+			main: {
+                title: 'Change Fractions to Decimals',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\row{\\str{Change \xA0}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}{\\str{\xA0 to a decimal:}}',
+						answer: '\\input{$answer}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\str{$fractionNum divided by $fractionDen equals $answer}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\css{\\str{Answer:  $answer}}{help-answer-text}}'
+						}
+                    }]
+                }]
+            },
+			rounded: {
+                title: 'Change Fractions to Decimals',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp{\\row{\\str{Change \xA0}}{\\frac{\\fracstr{$fractionNum}}{\\fracstr{$fractionDen}}}{\\str{\xA0 to a decimal;}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round to the hundredths place if necessary.}}}',
+						answer: '\\input{$answer}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\str{$fractionNum divided by $fractionDen equals $unrounded}}'
+							+'{\\str{$unrounded rounded to the hundredths place equals:}}'
+							+'{\\str{$answer}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\css{\\str{Answer:  $answer}}{help-answer-text}}'
+						 }
+                    }]
+                }]
+            }
+        },
+        data = [
+            { fractionNum: '3', fractionDen: '10', answer: '0.3', template: 'main' },
+			{ fractionNum: '4', fractionDen: '8', answer: '0.5', template: 'main' },
+			{ fractionNum: '6', fractionDen: '8', answer: '0.75', template: 'main' },
+			{ fractionNum: '14', fractionDen: '22', unrounded: '0.633...', answer: '0.64', template: 'rounded' },
+			{ fractionNum: '14', fractionDen: '24', unrounded: '0.583...', answer: '0.58', template: 'rounded' },
+			{ fractionNum: '17', fractionDen: '34', answer: '0.5', template: 'main' },
+			{ fractionNum: '10', fractionDen: '15', unrounded: '0.666...', answer: '0.67', template: 'rounded' },
+			{ fractionNum: '12', fractionDen: '21', unrounded: '0.571...', answer: '0.57', template: 'rounded' },
+			{ fractionNum: '28', fractionDen: '52', unrounded: '0.538...', answer: '0.54', template: 'rounded' },
+			{ fractionNum: '3', fractionDen: '9', unrounded: '0.333...', answer: '0.33', template: 'rounded' }
+       ];
 
-angular.module('mathSkills').value('data15_1', {
-    title: '15.1 Change Fractions to Decimals',
-    path: '15.1-change-fractions-to-decimals',
-    children: [{
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\str{Change \xA0}}{\\frac{\\fracstr{3}}{\\fracstr{10}}}{\\str{\xA0 to a decimal:}}',
-                answer: '\\input{0.3}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{3}}{\\str{\xA0 divided by \xA0}}{\\str{10}}{\\str{\xA0 equals \xA0}}{\\str{0.3}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.3}}}{help-answer-text help-answer-margin-right}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\str{Change \xA0}}{\\frac{\\fracstr{4}}{\\fracstr{8}}}{\\str{\xA0 to a decimal:}}',
-                answer: '\\input{0.5}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{4}}{\\str{\xA0 divided by \xA0}}{\\str{8}}{\\str{\xA0 equals \xA0}}{\\str{0.5}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.5}}}{help-answer-text help-answer-margin-right}}}}'
-
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\str{Change \xA0}}{\\frac{\\fracstr{6}}{\\fracstr{8}}}{\\str{\xA0 to a decimal:}}',
-                answer: '\\input{0.75}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{6}}{\\str{\xA0 divided by \xA0}}{\\str{8}}{\\str{\xA0 equals \xA0}}{\\str{0.75}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.75}}}{help-answer-text help-answer-margin-right}}}}'
-
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change \xA0}}{\\frac{\\fracstr{14}}{\\fracstr{22}}}{\\str{\xA0 to a decimal;}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round to the hundredths place if necessary.}}}',
-                answer: '\\input{0.64}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{14}}{\\str{\xA0 divided by \xA0}}{\\str{22}}{\\str{\xA0 equals \xA0}}{\\str{0.636...}}}{\\row{\\str{0.636...}}{\\str{ rounded to the hundredths place  }}{\\str{\xA0 equals \xA0}}{\\str{0.64}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.64}}}{help-answer-text help-answer-margin-right}}}}'
-
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change \xA0}}{\\frac{\\fracstr{14}}{\\fracstr{24}}}{\\str{\xA0 to a decimal;}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round to the hundredths place if necessary.}}}',
-                answer: '\\input{0.58}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{14}}{\\str{\xA0 divided by \xA0}}{\\str{24}}{\\str{\xA0 equals \xA0}}{\\str{0.583...}}}{\\row{\\str{0.583...}}{\\str{ rounded to the hundredths place  }}{\\str{\xA0 equals \xA0}}{\\str{0.58}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.58}}}{help-answer-text help-answer-margin-right}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\row{\\str{Change \xA0}}{\\frac{\\fracstr{17}}{\\fracstr{34}}}{\\str{\xA0 to a decimal:}}',
-                answer: '\\input{0.5}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{17}}{\\str{\xA0 divided by \xA0}}{\\str{34}}{\\str{\xA0 equals \xA0}}{\\str{0.5}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.5}}}{help-answer-text help-answer-margin-right}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change \xA0}}{\\frac{\\fracstr{10}}{\\fracstr{15}}}{\\str{\xA0 to a decimal;}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round to the hundredths place if necessary.}}}',
-                answer: '\\input{0.67}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{10}}{\\str{\xA0 divided by \xA0}}{\\str{15}}{\\str{\xA0 equals \xA0}}{\\str{0.666...}}}{\\row{\\str{0.666...}}{\\str{ rounded to the hundredths place  }}{\\str{\xA0 equals \xA0}}{\\str{0.67}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.67}}}{help-answer-text help-answer-margin-right}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change \xA0}}{\\frac{\\fracstr{12}}{\\fracstr{21}}}{\\str{\xA0 to a decimal;}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round to the hundredths place if necessary.}}}',
-                answer: '\\input{0.57}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{12}}{\\str{\xA0 divided by \xA0}}{\\str{21}}{\\str{\xA0 equals \xA0}}{\\str{0.571...}}}{\\row{\\str{0.571...}}{\\str{ rounded to the hundredths place  }}{\\str{\xA0 equals \xA0}}{\\str{0.57}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.57}}}{help-answer-text help-answer-margin-right}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change \xA0}}{\\frac{\\fracstr{28}}{\\fracstr{52}}}{\\str{\xA0 to a decimal;}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round to the hundredths place if necessary.}}}',
-                answer: '\\input{0.54}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{28}}{\\str{\xA0 divided by \xA0}}{\\str{52}}{\\str{\xA0 equals \xA0}}{\\str{0.538...}}}{\\row{\\str{0.538...}}{\\str{ rounded to the hundredths place  }}{\\str{\xA0 equals \xA0}}{\\str{0.54}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.54}}}{help-answer-text help-answer-margin-right}}}}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Change Fractions to Decimals',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\rowgrp{\\row{\\str{Change \xA0}}{\\frac{\\fracstr{6}}{\\fracstr{9}}}{\\str{\xA0 to a decimal;}}}{\\row{\\str{\xA0}}}{\\row{\\str{Round to the hundredths place if necessary.}}}',
-                answer: '\\input{0.67}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\rowgrp{\\row{\\str{6}}{\\str{\xA0 divided by \xA0}}{\\str{9}}{\\str{\xA0 equals \xA0}}{\\str{0.666...}}}{\\row{\\str{0.666...}}{\\str{ rounded to the hundredths place  }}{\\str{\xA0 equals \xA0}}{\\str{0.67}}}{\\row{\\str{\xA0}}}{\\css{\\row{\\str{Answer:  0.67}}}{help-answer-text help-answer-margin-right}}}}'
-                }
-            }]
-        }]
-    }, ]
-});
+    return dataUtils.build(desc, template, data);
+}]);
