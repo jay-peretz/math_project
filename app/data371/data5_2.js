54e2533dc0185cb19770c1c22c190990568c1dc7
Justin Doolaege <justindool@sbcglobal.net>
2013-07-16 23:14:46 -0400

    5.2 with new data format and ms-chkbtn

--- app/data371/data5_2.js
+++ app/data371/data5_2.js
@@ -1,137 +1,39 @@
 /*global angular */
 
-angular.module('mathSkills').value('data5_2', {
-    title: '5.2 Identifying Factors of a Given Number',
-    path: '5.2-identifying-factors-of-a-given-number',
-    children: [{
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 95 .}',
-                answer: '\\css{\\grp{\\chk{\\str{1}}{T}}{\\chk{\\str{2}}{F}}{\\chk{\\str{3}}{F}}{\\chk{\\str{5}}{T}}{\\chk{\\str{7}}{F}}{\\chk{\\str{10}}{F}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": '\\str{1 & 5}'
-                }
-            }]
-        }]
-    }, {
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 136 .}',
-                answer: '\\css{\\grp{\\chk{\\str{1}}{T}}{\\chk{\\str{2}}{T}}{\\chk{\\str{3}}{F}}{\\chk{\\str{5}}{F}}{\\chk{\\str{7}}{F}}{\\chk{\\str{10}}{F}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }, {
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 190 .}',
-                answer: '\\css{\\grp{\\chk{\\str{1}}{T}}{\\chk{\\str{2}}{T}}{\\chk{\\str{3}}{F}}{\\chk{\\str{5}}{T}}{\\chk{\\str{7}}{F}}{\\chk{\\str{10}}{T}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }, {
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 160 .}',
-				answer: '\\css{\\grp{\\chk{\\str{1}}{T}}{\\chk{\\str{2}}{T}}{\\chk{\\str{3}}{F}}{\\chk{\\str{5}}{T}}{\\chk{\\str{7}}{F}}{\\chk{\\str{10}}{T}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }, {
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 81 .}',
-                answer: '\\css{\\grp{\\chk{1}{T}}{\\chk{2}{F}}{\\chk{3}{T}}{\\chk{5}{F}}{\\chk{7}{F}}{\\chk{10}{F}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }, {
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 59 .}',
-				answer: '\\css{\\grp{\\chk{\\str{1}}{T}}{\\chk{\\str{2}}{F}}{\\chk{\\str{3}}{F}}{\\chk{\\str{5}}{F}}{\\chk{\\str{7}}{F}}{\\chk{\\str{10}}{F}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }, {
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 48 .}',
-				answer: '\\css{\\grp{\\chk{\\str{1}}{T}}{\\chk{\\str{2}}{T}}{\\chk{\\str{3}}{T}}{\\chk{\\str{5}}{F}}{\\chk{\\str{7}}{F}}{\\chk{\\str{10}}{F}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }, {
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 145 .}',
-				answer: '\\css{\\grp{\\chk{\\str{1}}{T}}{\\chk{\\str{2}}{F}}{\\chk{\\str{3}}{F}}{\\chk{\\str{5}}{T}}{\\chk{\\str{7}}{F}}{\\chk{\\str{10}}{F}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }, {
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 60 .}',
-				answer: '\\css{\\grp{\\chk{\\str{1}}{T}}{\\chk{\\str{2}}{T}}{\\chk{\\str{3}}{T}}{\\chk{\\str{5}}{T}}{\\chk{\\str{7}}{F}}{\\chk{\\str{10}}{T}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }, {
-        title: 'Choose the Factors of Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\html{Select the given divisors of 94 .}',
-				answer: '\\css{\\grp{\\chk{\\str{1}}{T}}{\\chk{\\str{2}}{T}}{\\chk{\\str{3}}{F}}{\\chk{\\str{5}}{F}}{\\chk{\\str{7}}{F}}{\\chk{\\str{10}}{F}}}{factors}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }]
-});
+angular.module('mathSkills').service('data5_2', ['dataUtils', function (dataUtils) {
+    var ret = {
+            title: '5.2 Identifying Factors of a Given Number',
+            path: '5.2-identifying-factors-of-a-given-number',
+            children: []
+        },
+        template = {
+            main: {
+                title: 'Choose the Factors of Whole Numbers',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+                        problem: '\\html{Select the given divisors of $number .}',
+                        answer: '\\grp{\\chkbtn{1}{$btn1}}{\\chkbtn{2}{$btn2}}{\\chkbtn{3}{$btn3}}{\\chkbtn{5}{$btn5}}{\\chkbtn{7}{$btn7}}{\\chkbtn{10}{$btn0}}',
+                        controls: {
+                            "checkAnswer": true,
+                            "help": true
+                        }
+                    }]
+                }]
+            }
+        },
+        data = [
+            { number: '95', btn1:'T', btn2:'F', btn3:'F', btn5:'T', btn7:'F', btn0:'F', template: 'main'},
+            { number: '136', btn1:'T', btn2:'T', btn3:'F', btn5:'F', btn7:'F', btn0:'F', template: 'main'},
+            { number: '190', btn1:'T', btn2:'T', btn3:'F', btn5:'T', btn7:'F', btn0:'T', template: 'main'},
+            { number: '160', btn1:'T', btn2:'T', btn3:'F', btn5:'T', btn7:'F', btn0:'T', template: 'main'},
+            { number: '81', btn1:'T', btn2:'F', btn3:'T', btn5:'F', btn7:'F', btn0:'F', template: 'main'},
+            { number: '59', btn1:'T', btn2:'F', btn3:'F', btn5:'F', btn7:'F', btn0:'F', template: 'main'},
+            { number: '48', btn1:'T', btn2:'T', btn3:'T', btn5:'F', btn7:'F', btn0:'F', template: 'main'},
+            { number: '145', btn1:'T', btn2:'F', btn3:'F', btn5:'T', btn7:'F', btn0:'F', template: 'main'},
+            { number: '60', btn1:'T', btn2:'T', btn3:'T', btn5:'T', btn7:'F', btn0:'T', template: 'main'},
+            { number: '94', btn1:'T', btn2:'T', btn3:'F', btn5:'F', btn7:'F', btn0:'F', template: 'main'}
+        ];
+
+    return dataUtils.build(ret, template, data);
+}]);
