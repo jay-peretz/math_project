74386eb22c0cc8b167bfde0a9d4a7d1ad783c7c4
Jay Peretz <jay@mytowncontractor.com>
2013-07-29 11:20:13 -0700

    Completed 372 problem C.1

--- app/data372/data_c_1.js
+++ app/data372/data_c_1.js
@@ -4,6 +4,244 @@ angular.module('mathSkills').value('data_c_1', {
     title: 'C.1 Prime Factorization',
     path: 'C.1-prime-factorization',
     children: [{
-        title: 'Prime Factorization',
-        children: [] }]
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 143}',
+                answer: '\\grp{\\input{11}}{\\sign{·}}{\\input{13}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 143}',
+                answer: '\\primefac{143}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    },{
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 22}',
+                answer: '\\grp{\\input{2}}{\\sign{·}}{\\input{11}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 22}',
+                answer: '\\primefac{22}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    },{
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 177}',
+                answer: '\\grp{\\input{3}}{\\sign{·}}{\\input{59}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 177}',
+                answer: '\\primefac{177}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    }, {
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 105}',
+                answer: '\\grp{\\input{3}}{\\sign{·}}{\\input{5}}{\\sign{·}}{\\input{7}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 105}',
+                answer: '\\primefac{105}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    }, {
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 165}',
+                answer: '\\grp{\\input{3}}{\\sign{·}}{\\input{5}}{\\sign{·}}{\\input{11}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 165}',
+                answer: '\\primefac{165}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    }, {
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 1331}',
+                answer: '\\exp{\\input{11}}{\\input{3}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 1331}',
+                answer: '\\primefac{1331}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    }, {
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 200}',
+                answer: '\\grp{\\exp{\\input{2}}{\\input{3}}}{\\sign{·}}{\\exp{\\input{5}}{\\input{2}}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 200}',
+                answer: '\\primefac{200}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    }, {
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 539}',
+                answer: '\\grp{\\exp{\\input{7}}{\\input{2}}}{\\sign{·}}{\\input{11}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 539}',
+                answer: '\\primefac{539}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    }, {
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 63}',
+                answer: '\\grp{\\exp{\\input{3}}{\\input{2}}}{\\sign{·}}{\\input{7}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 63}',
+                answer: '\\primefac{63}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    }, {
+        title: 'Writing Prime Factorization of Whole Numbers',
+        children: [{
+            title: 'Main Answer',
+            children: [{
+                problem: '\\str{Write the prime factorization of 375}',
+                answer: '\\grp{\\input{3}}{\\sign{·}}{\\exp{\\input{5}}{\\input{3}}}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": false,
+                    "workbook": true
+                }
+            }]
+        }, {
+            title: 'Workbook',
+            children: [{
+                problem: '\\str{Prime factor 375}',
+                answer: '\\primefac{375}',
+                controls: {
+                    "checkAnswer": true,
+                    "help": true
+                }
+            }]
+        }]
+    }]
 });
