80f5ddd7c99d73454215237bb988f11574d5e044
markafoote <mark@getthatwebsite.com>
2013-08-07 11:09:10 -0700

    7.1, change &times; to &sdot;

--- app/data371/data7_1.js
+++ app/data371/data7_1.js
@@ -15,7 +15,7 @@ angular.module('mathSkills').service('data7_1', ['dataUtils', function (dataUtil
                         problem: '\\grp'
 						+'{\\html{Solve &nbsp;}}'
 						+'{\\frac{\\str{$n1}}{\\str{$d1}}}'
-						+'{\\sign{&times;}}'
+						+'{\\sign{&sdot;}}'
 						+'{\\frac{\\str{$n2}}{\\str{$d2}}}',
                         answer: '\\frac{\\input{$na}}{\\input{$da}}',
                         controls: {
@@ -30,7 +30,7 @@ angular.module('mathSkills').service('data7_1', ['dataUtils', function (dataUtil
                         problem: '\\rowgrp'+
                                 '{\\str{Reduce the fractions by canceling.}}' 
 								+'{\\html{&nbsp;}}'
-                                +'{\\grp{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&times;}}{\\frac{\\str{$n2}}{\\str{$d2}}}}',
+                                +'{\\grp{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&sdot;}}{\\frac{\\str{$n2}}{\\str{$d2}}}}',
                         answer: '\\cancelfracs{[$n1,$d1]}{[$n2,$d2]}',
                         controls: {
                             "checkAnswer": true,
@@ -54,7 +54,7 @@ angular.module('mathSkills').service('data7_1', ['dataUtils', function (dataUtil
                         problem: '\\rowgrp'
 						+'{\\html{Solve:}}'
 						+'{\\html{&nbsp;}}'
-						+'{\\grp{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&times;}}{\\frac{\\str{$n2}}{\\str{$d2}}}{\\sign{&times;}}{\\frac{\\str{$n3}}{\\str{$d3}}}}',
+						+'{\\grp{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&sdot;}}{\\frac{\\str{$n2}}{\\str{$d2}}}{\\sign{&sdot;}}{\\frac{\\str{$n3}}{\\str{$d3}}}}',
                         answer: '\\frac{\\input{$na}}{\\input{$da}}',
                         controls: {
                             "checkAnswer": true,
@@ -68,7 +68,7 @@ angular.module('mathSkills').service('data7_1', ['dataUtils', function (dataUtil
                         problem: '\\rowgrp'
                                 +'{\\str{Reduce the fractions by canceling.}}' 
 								+'{\\html{&nbsp;}}'
-                                +'{\\grp{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&times;}}{\\frac{\\str{$n2}}{\\str{$d2}}}{\\sign{&times;}}{\\frac{\\str{$n3}}{\\str{$d3}}}}',
+                                +'{\\grp{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&sdot;}}{\\frac{\\str{$n2}}{\\str{$d2}}}{\\sign{&sdot;}}{\\frac{\\str{$n3}}{\\str{$d3}}}}',
                         answer: '\\cancelfracs{[$n1,$d1]}{[$n2,$d2]}{[$n3,$d3]}',
                         controls: {
                             "checkAnswer": true,
