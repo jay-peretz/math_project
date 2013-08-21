62d4c576c8b38c95d1087be9eb6f491b1e50b6f9
markafoote <mark@getthatwebsite.com>
2013-08-20 18:03:52 -0700

    reduce ms-sign.css bigger to 125%, add a bigger-no-margin class to style &bull; with the same margin but no size increase- Matt likes the bullet without a size increase and thought 150% was too big on +.

--- app/data371/data4_2.js
+++ app/data371/data4_2.js
@@ -1,35 +1,4 @@
 /*global angular */
-/*
- 
- 
-        title: 'Order Of Operations With Whole Numbers',
-        children: [{
-            title: 'Main Answer',
-            children: [{
-                problem: '\\str{Solve 27 / 9 + 6 * 8 - (10 + 25)}',
-                answer: '\\input{16}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": false,
-                    "workbook": true
-                }
-            }]
-        }, {
-            title: 'Workbook',
-            children: [{
-                problem: '\\str{Solve 27 / 9 + 6 * 8 - (10 + 25)}',
-                answer: '\\ordops{27 / 9 + 6 * 8 - (10 + 25)}',
-                controls: {
-                    "checkAnswer": true,
-                    "help": true
-                }
-            }]
-        }]
-    }, ]
-});/*
-
-
-/*global angular */
 
 angular.module('mathSkills').service('data4_2', ['dataUtils', function (dataUtils) {
     var desc = {
@@ -71,16 +40,16 @@ angular.module('mathSkills').service('data4_2', ['dataUtils', function (dataUtil
 			}	
         },
         data = [
-            { problemStatement: '7 <span class=bigger>&sdot;</span> 2 <span class=bigger>+</span> 4', ordopsStatement: '7 * 2 + 4', answer: '18', template: 'first' },
+            { problemStatement: '7 <span class=margin-not-bigger>&bull;</span> 2 <span class=bigger>+</span> 4', ordopsStatement: '7 * 2 + 4', answer: '18', template: 'first' },
 			{ problemStatement: '( 15 <span class=bigger>&minus;</span> 10 ) <span class=bigger>&divide;</span> 5', ordopsStatement: '( 15 - 10 ) / 5', answer: '1', template: 'first' },
 			{ problemStatement: '4 <span class=bigger>&divide;</span> 2 <span class=bigger>+</span> 9', ordopsStatement: '4 / 2 + 9', answer: '11', template: 'first' },
 			{ problemStatement: '6 <span class=bigger>&minus;</span> ( 6 <span class=bigger>&divide;</span> 3 )', ordopsStatement: '6 - (6 / 3)', answer: '4', template: 'first' },
 			{ problemStatement: '7 <span class=bigger>+</span> 15 <span class=bigger>&divide;</span> 3', ordopsStatement: '7 + 15 / 3', answer: '12', template: 'first' },
-			{ problemStatement: '3 <span class=bigger>&divide;</span> ( 3 <span class=bigger>&sdot;</span> 3 <span class=bigger>&divide;</span> 3 )', ordopsStatement: '3 / (3 * 3 / 3)', answer: '1', template: 'first' },
-			{ problemStatement: '15 <span class=bigger>&minus;</span> ( ( 7 <span class=bigger>&minus;</span> 4 ) <span class=bigger>&sdot;</span> 2 )', ordopsStatement: '15 - ((7 - 4) * 2)', answer: '9', template: 'first' },
-			{ problemStatement: '15 <span class=bigger>&minus;</span> 8 <span class=bigger>&sdot;</span> 2 <span class=bigger>&divide;</span> 4', ordopsStatement: '15 - 8 * 2 / 4', answer: '11', template: 'first' },
+			{ problemStatement: '3 <span class=bigger>&divide;</span> ( 3 <span class=margin-not-bigger>&bull;</span> 3 <span class=bigger>&divide;</span> 3 )', ordopsStatement: '3 / (3 * 3 / 3)', answer: '1', template: 'first' },
+			{ problemStatement: '15 <span class=bigger>&minus;</span> ( ( 7 <span class=bigger>&minus;</span> 4 ) <span class=margin-not-bigger>&bull;</span> 2 )', ordopsStatement: '15 - ((7 - 4) * 2)', answer: '9', template: 'first' },
+			{ problemStatement: '15 <span class=bigger>&minus;</span> 8 <span class=margin-not-bigger>&bull;</span> 2 <span class=bigger>&divide;</span> 4', ordopsStatement: '15 - 8 * 2 / 4', answer: '11', template: 'first' },
 			{ problemStatement: '( 28 <span class=bigger>+</span> 15 <span class=bigger>&divide;</span> 5 ) <span class=bigger>&minus;</span> 14', ordopsStatement: '(28 + 15 / 5) - 14', answer: '17', template: 'first' },
-			{ problemStatement: '27 <span class=bigger>&divide;</span> 9 <span class=bigger>+</span> 6 <span class=bigger>&sdot;</span> 8 <span class=bigger>&minus;</span> 35', ordopsStatement: '27 / 9 + 6 * 8 - 35)', answer: '16', template: 'first' },
+			{ problemStatement: '27 <span class=bigger>&divide;</span> 9 <span class=bigger>+</span> 6 <span class=margin-not-bigger>&bull;</span> 8 <span class=bigger>&minus;</span> 35', ordopsStatement: '27 / 9 + 6 * 8 - 35)', answer: '16', template: 'first' },
         ];
 
 	return dataUtils.build(desc, template, data);
