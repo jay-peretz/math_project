fe9de985ed3672dd3b4333730e46714601a30571
markafoote <mark@getthatwebsite.com>
2013-09-17 13:50:02 -0700

    8.5 prob 2

--- app/data371/data8_5.js
+++ app/data371/data8_5.js
@@ -7,13 +7,13 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
             children: []
         },
         template = {
-            firstFraction: {
+            housesDiv1: {
                 title: 'Mixed Fraction Application Problems',
                 children: [{
                     title: 'Main Answer',
                     children: [{
                         problem: '\\image{$illustration}',
-                        answer: '\\wb{\\rowgrp{\\ins{A single Vegburger weighs 8 ounces.  If there are 7 Vegburgers on a tray, what is the <u>total number of ounces</u> of Vegburger on the tray?}}{\\input{$number}}',
+                        answer: '\\col{\\ins{There are houses worth a total of &#36;$firstunitwhole <sup>$firstunitn</sup>&#8260;<sub>$firstunitd</sub> million dollars in a subdivision. If each house is worth <sup>$secondunitn</sup>&#8260;<sub>$secondunitd</sub> of a million dollars, how many houses are in the subdivision?}}{\\input{$number}}',
                         controls: {
                             "checkAnswer": true,
                             "help": false,
@@ -24,9 +24,7 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                     title: 'Workbook',
                     children: [{
                         problem: '\\image{$illustration}',
-                        answer: '\\wb'
-						+'{\\rowgrp'
-						+'{\\ins{To find out the <u>total</u> number of ounces of Vegburger on the tray, what operation must we use?}}{\\select{Multiplication}{["Addition", "Subtraction", "Multiplication", "Division"]}}}',
+                        answer: '\\wb{\\rowgrp{\\ins{You have been given the <strong>total</strong> value of the houses in the subdivision.  What is the total value of <u>one</u> house?}}{\\grp{\\frac{\\input{$secondunitn}}{\\input{$secondunitd}}}{html{million dollars}}}}',
                         controls: {
                             "checkAnswer": true,
                             "help": true
@@ -34,9 +32,14 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                     }, {
                         problem: '\\image{$illustration}',
                         answer:
-                            '\\rowgrp' 
-							+'{\\ins{We must multiply to solve this problem. The number of ounces in a single Vegburger must be multiplied by the number of burgers on the tray to get the total number of ounces of Vegburger on the tray.  Write a setup that will solve for the number of ounces of Vegburger on the tray.}}' +    
-                            '{\\grp{\\input{$firstInput}}{\\html{ ounces }}{\\sign{&bull;}}{\\input{$secondInput}}{\\html{ Vegburgers }}}',
+                            '\\wb{' +
+                                '\\rowgrp' + 
+                                    '{\\choose{[' +
+                                        '\\ins{Yes, each house is valued at <sup>$secondunitn</sup>&#8260;<sub>$secondunitd</sub> million dollars. To find out how many houses (each worth <sup>$secondunitn</sup>&#8260;<sub>$secondunitd</sub> million dollars) are in the subdivision, what operation must we use?},' +
+                                        '\\ins{No, we are told that each house is valued at <sup>$secondunitn</sup>&#8260;<sub>$secondunitd</sub> million dollars. To find out how many houses (each worth <sup>$secondunitn</sup>&#8260;<sub>$secondunitd</sub> million dollars) are in the subdivision , what operation must we use?}' +    
+                                    ']}{$$previousCorrect}}' +
+                                    '{\\select{Division}{["Addition", "Subtraction", "Multiplication", "Division"]}}' +
+                            '}',
                         controls: {
                             "checkAnswer": true,
                             "help": true
@@ -44,11 +47,27 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                     }, {
                         problem: '\\image{$illustration}',
                         answer:
-                            '\\rowgrp'
-                            	+'{\\ins{Great! Now solve the problem.}}' 
-                                +'{\\grp{\\html{$firstInput}}{\\html{ ounces }}{\\sign{&bull;}}{\\html{$secondInput}}{\\html{ Vegburgers }}}'
-								+'{\\html{&nbsp;}}'
-								+'{grp{\\sign{=}}{\\input{$number}}{\\html{ ounces of Vegburger on the tray.}}}',
+                            '\\wb{' +
+                                '\\rowgrp' +
+                                    '{\\choose{[' +
+                                        '\\ins{Yes. Write a setup that will solve for the total number of houses in the subdivision.},' +
+                                        '\\ins{We must divide to solve this problem. The given total value has been divided up into equal-sized parts&mdash;each worth <sup>$secondunitn</sup>&#8260;<sub>$secondunitd</sub> million dollars.  Each <sup>$secondunitn</sup>&#8260;<sub>$secondunitd</sub> million dollars represents one house. Write a setup that will solve for the number of houses in the subdivision.}' +    
+                                    ']}{$$previousCorrect}}' +
+                                    '{\\grp{\\html{&#36;}}{\\mixed{\\input{$firstunitwhole}}{\\frac{\\input{$firstunitn}}{\\input{$firstunitd}}}}{\\html{million dollars}}{\\sign{&div;}}{\\frac{\\input{$secondunitn}}{\\input{$secondunitd}}}{\\html{million dollars}}}' + 
+                            '}',
+                        controls: {
+                            "checkAnswer": true,
+                            "help": true
+                        }
+                    }, {
+                        problem: '\\image{$illustration}',
+                        answer:
+                            '\\rowgrp' +
+                                '{\\choose{[' +
+                                    '\\ins{Great! Now solve the problem.},' +
+                                    '\\ins{This is a division problem.  When we set up a division problem, the <u>first number must be the total</u> that is being divided up.  In this case the total being divided up is the value of the subdivision (&#36;$firstunitwhole <sup>$firstunitn</sup>&#8260;<sub>$firstunitd</sub> million dollars), so the value of the subdivision is placed first in the division set up.  The correct setup is below. Now solve the problem.}' +    
+                                ']}{$$previousCorrect}}' +
+                                '{\\grp{\\html{&#36;}}{\\mixed{\\fracstr{$firstunitwhole}}{\\frac{\\fracstr{$firstunitn}}{\\fracstr{$firstunitd}}}}{\\html{million dollars}}{\\sign{&div;}}{\\frac{\\fracstr{$secondunitn}}{\\fracstr{$secondunitd}}}{\\html{million dollars}}{\\sign{=}}{\\input{$number}}}',
                         controls: {
                             "checkAnswer": true,
                             "help": true
@@ -61,8 +80,8 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                 children: [{
                     title: 'Main Answer',
                     children: [{
-                        problem: '\\html{<img src=img/illustrations/$illustration>}',
-                        answer: '\\col{\\ins{$number houses are to be built on $total acres of land.  What will be the size of each parcel of land (know that each parcel will be the same size)?}}{\\frac{\\input{$unitn}}{\\input{$unitd}}}',
+                        problem: '\\image{$illustration}',
+                        answer: '\\col{\\ins{$number houses in a subdivision are worth a total of &#36;$firstunitwhole <sup>$firstunitn</sup>&#8260;<sub>$firstunitd</sub> million dollars.  What is the value of each house (know that each house is priced exactly the same)?}}{\\frac{\\input{$secondunitn}}{\\input{$secondunitd}}}',
                         controls: {
                             "checkAnswer": true,
                             "help": false,
@@ -72,25 +91,25 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                 }, {
                     title: 'Workbook',
                     children: [{
-                        problem: '\\html{<img src=img/illustrations/$illustration>}',
+                        problem: '\\image{$illustration}',
                         answer: 
                             '\\wb{' +
                                 '\\rowgrp' +
-                                    '{\\ins{You have been <u>given the total</u> number of acres of land where houses will be built.  How many houses are being built?}}' +
-                                    '{\\input{$number}}' +
+                                    '{\\ins{You have been <u>given the total</u> number of houses in the subdivision.  How many houses are in the subdivision?}}' +
+                                    '{\\grp{\\input{$number}}{\\html{houses}}}' +
                             '}',
                         controls: {
                             "checkAnswer": true,
                             "help": true
                         }
                     }, {
-                        problem: '\\html{<img src=img/illustrations/$illustration>}',
+                        problem: '\\image{$illustration}',
                         answer: 
                             '\\wb{' +
                                 '\\rowgrp' +
                                     '{\\choose{[' +
-                                        '\\ins{Yes, $number houses will be built. To find out the size of each parcel of land, what operation must we use?},' +
-                                        '\\ins{No, we are told $number houses will be built. To find out the size of each parcel of land, what operation must we use?}' +    
+                                        '\\ins{Yes, there are $number houses in the subdivision. To find out the price of each house, what operation must we use?},' +
+                                        '\\ins{No, we are told there are $number houses in the subdivision. To find out the price of each house, what operation must we use?}' +    
                                     ']}{$$previousCorrect}}' +
                                     '{\\select{Division}{["Addition", "Subtraction", "Multiplication", "Division"]}}' +
                             '}',
@@ -99,34 +118,34 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                             "help": true
                         }
                     }, {
-                        problem: '\\html{<img src=img/illustrations/$illustration>}',
+                        problem: '\\image{$illustration}',
                         answer:
                             '\\wb{' +
                                 '\\rowgrp' +
                                     '{\\choose{[' +
                                         '\\ins{Yes, now write a setup that will solve this problem.},' +
-                                        '\\ins{We must divide to solve this problem.  The <u>given total land</u> is being divided up into $number equal-sized parts of land.  Write a set up that will solve for the size of each parcel of land where a house will be built.}' +    
-                                    ']}{$$previousCorrect}}' +
-                                    '{\\grp{\\frac{\\input{$total}}{\\input{1}}}{\\sign{&div;}}{\\frac{\\input{$number}}{\\input{1}}}}' +
-                            '}',
+                                        '\\ins{We must divide to solve this problem.  The <u>given total value of the subdivision</u> is being divided up into $number identically-priced houses.  Write a set up that will solve for the value of each house.}' +    
+                                    ']}{$$previousCorrect}}' +                                    
+									'{\\grp{\\select{$firstunitwhole $firstunitn/$firstunitd million dollars}{["$firstunitwhole $firstunitn/$firstunitd million dollars", "$number"]}}{\\sign{&div;}}{\\select{$number}{["$firstunitwhole $firstunitn/$firstunitd million dollars", "$number"]}}}' +
+                            	'}',
                         controls: {
                             "checkAnswer": true,
                             "help": true
                         }
                     }, {
-                        problem: '\\html{<img src=img/illustrations/$illustration>}',
+                        problem: '\\image{$illustration}',
                         answer: 
                             '\\wb{' +
                                 '\\rowgrp' +
                                     '{\\choose{[' +
                                         '\\ins{Great! Now give the solution.},' +
-                                        '\\ins{This is a division problem.  When we set up a division problem, the <u>first number must be the total</u> that is being divided up.  In this case the $total acres is the total and is placed first in the division set up.  The correct setup is shown below.<br><br>Now solve the problem.}' +
+                                        '\\ins{This is a division problem.  When we set up a division problem, the <u>first number must be the total</u> that is being divided up.  In this case the $total is the total value of the subdivision (&#36;$firstunitwhole <sup>$firstunitn</sup>&#8260;<sub>$firstunitd</sub> million dollars) and is placed first in the division set up.  The correct setup is shown below.<br><br>Now solve the problem.}' +
                                     ']}{$$previousCorrect}}' +
-                                    '{\\grp{\\frac{\\str{$total}}{\\str{1}}}{\\sign{&div;}}{\\frac{\\str{$number}}{\\str{1}}}{\\sign{=}}{\\frac{\\input{$unitn}}{\\input{$unitd}}}}' +
+                                    '{\\grp{\\frac{\\str{$total}}{\\str{1}}}{\\sign{&div;}}{\\frac{\\str{$number}}{\\str{1}}}{\\sign{=}}{\\frac{\\input{$firstunitn}}{\\input{$firstunitd}}}{\\html{million dollars}}}' +
                             '}',
                         controls: {
                             "checkAnswer": true,
-                            "help": '\\row{\\frac{\\str{$total}}{\\str{1}}}{\\sign{&div;}}{\\frac{\\str{$number}}{\\str{1}}}{\\sign{=}}{\\frac{\\str{$unitn}}{\\str{$unitd}}}'
+                            "help": true
                         }
                     }]
                 }]
@@ -369,10 +388,10 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
             }
         },
         data = [
-            { firstInput: 8, secondInput: 7, number: 56, illustration: '8.5-08_Veggie_Burger/400x300/VegBurger_c.jpg', template: 'firstFraction' },
-            { total: 15, unitn: 3, unitd: 4, number: 20, illustration: 'Acres_Gplane_15b.jpg', template: 'housesDiv2' },
+            { total: '2.7',firstunitwhole: '2', firstunitn: '7', firstunitd: '10', secondunitn: '3', secondunitd: '10', number: '9', illustration: '8.5-11_Houses_Money/400x300/Millions_houses_a.jpg', template: 'housesDiv1' },
+            { total: '2.7',firstunitwhole: '2', firstunitn: '7', firstunitd: '10', secondunitn: '3', secondunitd: '10', number: '9', illustration: '8.5-11_Houses_Money/400x300/Millions_houses_b.jpg', template: 'housesDiv2' },
             { total: 15, unitn: 3, unitd: 4, number: 20, illustration: 'Acres_Gplane_15c.jpg', template: 'housesMul' },
-            { total: 21, unitn: 1, unitd: 3, number: 63, illustration: 'Acres_Gplane_21a.jpg', template: 'firstFraction' },
+            { total: 21, unitn: 1, unitd: 3, number: 63, illustration: 'Acres_Gplane_21a.jpg', template: 'housesDiv1' },
             { total: 21, unitn: 1, unitd: 3, number: 63, illustration: 'Acres_Gplane_21b.jpg', template: 'housesDiv2' },
             { total: 21, unitn: 1, unitd: 3, number: 63, illustration: 'Acres_Gplane_21c.jpg', template: 'housesMul' },
             { totaln: 2, totald: 3, unitn: 1, unitd: 9, number: 6, illustration: 'Acres_Gplane_2.3a.jpg', template: 'housesDivFrac1' },
