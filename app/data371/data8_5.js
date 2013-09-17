7e29f4cae809568194c2198a1c6627abf26a562c
markafoote <mark@getthatwebsite.com>
2013-09-17 16:08:27 -0700

    8.5, prob 3

--- app/data371/data8_5.js
+++ app/data371/data8_5.js
@@ -123,10 +123,10 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                             '\\wb{' +
                                 '\\rowgrp' +
                                     '{\\choose{[' +
-                                        '\\ins{Yes, now write a setup that will solve this problem.},' +
-                                        '\\ins{We must divide to solve this problem.  The <u>given total value of the subdivision</u> is being divided up into $number identically-priced houses.  Write a set up that will solve for the value of each house.}' +    
+                                        '\\rowgrp{\\css{\\ins{Yes, now write a setup that will solve this problem.}}{width400}}{\\html{<br><br>}},' +
+                                        '\\css{\\ins{We must divide to solve this problem.  The <u>given total value of the subdivision</u> is being divided up into $number identically-priced houses.  Pick the set up that will solve for the value of each house.}}{width400}' +    
                                     ']}{$$previousCorrect}}' +                                    
-									'{\\grp{\\select{$firstunitwhole $firstunitn/$firstunitd million dollars}{["$firstunitwhole $firstunitn/$firstunitd million dollars", "$number"]}}{\\sign{&div;}}{\\select{$number}{["$firstunitwhole $firstunitn/$firstunitd million dollars", "$number"]}}}' +
+									'{\\grp{\\select{$firstunitwhole $firstunitn/$firstunitd million dollars}{["$firstunitwhole $firstunitn/$firstunitd million dollars", "$number houses"]}}{\\sign{&div;}}{\\select{$number houses}{["$firstunitwhole $firstunitn/$firstunitd million dollars", "$number houses"]}}}' +
                             	'}',
                         controls: {
                             "checkAnswer": true,
@@ -138,10 +138,10 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                             '\\wb{' +
                                 '\\rowgrp' +
                                     '{\\choose{[' +
-                                        '\\ins{Great! Now give the solution.},' +
-                                        '\\ins{This is a division problem.  When we set up a division problem, the <u>first number must be the total</u> that is being divided up.  In this case the $total is the total value of the subdivision (&#36;$firstunitwhole <sup>$firstunitn</sup>&#8260;<sub>$firstunitd</sub> million dollars) and is placed first in the division set up.  The correct setup is shown below.<br><br>Now solve the problem.}' +
+                                        '\\css{\\ins{Great! Now give the solution.}}{width400},' +
+                                        '\\css{\\ins{This is a division problem.  When we set up a division problem, the <u>first number must be the total</u> that is being divided up.  In this case the total is the total value of the subdivision (&#36;$firstunitwhole <sup>$firstunitn</sup>&#8260;<sub>$firstunitd</sub> million dollars) and is placed first in the division set up.  The correct setup is shown below.<br><br>Now solve the problem.}}{width400}' +
                                     ']}{$$previousCorrect}}' +
-                                    '{\\grp{\\frac{\\str{$total}}{\\str{1}}}{\\sign{&div;}}{\\frac{\\str{$number}}{\\str{1}}}{\\sign{=}}{\\frac{\\input{$firstunitn}}{\\input{$firstunitd}}}{\\html{million dollars}}}' +
+                                    '{\\rowgrp{\\grp{\\html{&#36;}}{\\mixed{\\fracstr{$firstunitwhole}}{\\frac{\\fracstr{$firstunitn}}{\\fracstr{$firstunitd}}}}{\\html{million dollars}}{\\sign{&div;}}{\\frac{\\fracstr{$number}}{\\fracstr{1}}}{\\html{houses}}}{\\html{&nbsp;}}{\\grp{\\sign{=}}{\\grp{\\frac{\\input{$secondunitn}}{\\input{$secondunitd}}}{\\html{million dollars}}{\\html{/}}{\\html{house}}}}}' +
                             '}',
                         controls: {
                             "checkAnswer": true,
@@ -155,8 +155,11 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                 children: [{
                     title: 'Main Answer',
                     children: [{
-                        problem: '\\html{<img src=img/illustrations/$illustration>}',
-                        answer: '\\col{\\ins{Each house will occupy <sup>$unitn</sup>&#8260;<sub>$unitd</sub> of an acre.  If $number houses are to be built, how many acres of land are needed?}}{\\input{$total}}',
+                        problem: '\\image{$illustration}',
+                        answer: '\\rowgrp' + 
+									'{\\ins{If $number houses have been built, and each house is priced at <sup>$secondunitn</sup>&#8260;<sub>$secondunitd</sub> million dollars, what is the total value of all the houses in the subdivision combined?}}' +
+									'{\\html{&nbsp;}}' + 
+									'{\\grp{\\html{&#36;}}{\\mixed{\\input{$firstunitwhole}}{\\frac{\\input{$firstunitn}}{\\input{$firstunitd}}}}{\\html{million dollars}}}',
                         controls: {
                             "checkAnswer": true,
                             "help": false,
@@ -166,29 +169,54 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
                 }, {
                     title: 'Workbook',
                     children: [{
-                        problem: '\\col{\\html{<img src=img/illustrations/$illustration>}}{\\str{You are searching for the total number of acres that are needed.  How many acres are needed to build one house?}}',
-                        answer: '\\frac{\\input{$unitn}}{\\input{$unitd}}',
+                        problem: '\\image{$illustration}',
+                        answer: '\\wb{' +
+									'\\rowgrp' +
+										'{\\ins{You have been <u>given the total</u> number of houses in the subdivision.  How many houses are in the subdivision?}}' +
+										'{\\grp{\\input{$number}}{\\html{houses}}}' +
+								'}',
                         controls: {
                             "checkAnswer": true,
                             "help": true
                         }
                     }, {
-                        problem: '\\col{\\html{<img src=img/illustrations/$illustration>}}{\\row{\\str{How many houses are being built?}}}',
-                        answer: '\\input{$number}',
+                        problem: '\\image{$illustration}',
+                        answer: '\\wb{' +
+									'\\rowgrp' +
+										'{\\choose{[' +
+											'\\ins{Yes, there are $number houses in the subdivision. To find out the total combined value of all the houses in the subdivision, what operation must we use?},' +
+											'\\ins{No, we are told there are $number houses in the subdivision. To find out the total combined value of all the houses in the subdivision, what operation must we use?}' +    
+										']}{$$previousCorrect}}' +
+										'{\\select{Multiplication}{["Addition", "Subtraction", "Multiplication", "Division"]}}' +
+								'}',
                         controls: {
                             "checkAnswer": true,
                             "help": true
                         }
                     }, {
-                        problem: '\\col{\\html{<img src=img/illustrations/$illustration>}}{\\str{Write a setup that will solve for the total amount of land that is needed to build all $number houses.}}',
-                        answer: '\\grp{\\frac{\\input{$unitn}}{\\input{$unitd}}}{\\sign{&times;}}{\\frac{\\input{$number}}{\\input{1}}}',
+                        problem: '\\image{$illustration}',
+                        answer: '\\wb{' +
+									'\\rowgrp' +
+										'{\\choose{[' +
+											'\\rowgrp{\\css{\\ins{Yes, now write a setup that will solve this problem.}}{width400}}{\\html{<br><br>}},' +
+											'\\css{\\ins{We must multiply to solve this problem.  The <u>given value of each house in the subdivision</u> is being multiplied by $number houses.  Pick the set up that will solve for the total combined value of the houses in the subdivision.}}{width400}' +    
+										']}{$$previousCorrect}}' +                                    
+										'{\\grp{\\select{$secondunitn/$secondunitd million dollars}{["$secondunitn/$secondunitd million dollars", "$number houses"]}}{\\sign{&bull;}}{\\select{$number houses}{["$secondunitn/$secondunitd million dollars", "$number houses"]}}}' +
+                            	'}',
                         controls: {
                             "checkAnswer": true,
                             "help": true
                         }
                     }, {
-                        problem: '\\col{\\html{<img src=img/illustrations/$illustration>}}{\\str{Now solve the problem.}}',
-                        answer: '\\grp{\\frac{\\str{$unitn}}{\\str{$unitd}}}{\\sign{&div;}}{\\frac{\\str{$number}}{\\str{1}}}{\\sign{=}}{\\input{$total}}',
+                        problem: '\\image{$illustration}',
+                        answer: '\\wb{' +
+                                '\\rowgrp' +
+                                    '{\\choose{[' +
+                                        '\\rowgrp{\\css{\\ins{Now solve the problem.}}{width400}}{\\html{<br><br>}},' +
+                                        '\\css{\\ins{This is a repeated addition or multiplication problem.  The value of each house, <sup>$secondunitn</sup>&#8260;<sub>$secondunitd</sub> million dollars, needs to be repeated $number times. The correct setup is shown below. Now solve the problem.}}{width400}' +
+                                    ']}{$$previousCorrect}}' +
+                                    '{\\grp{\\frac{\\str{$secondunitn}}{\\str{$secondunitd}}}{\\html{million dollars}}{\\sign{&bull;}}{\\html{$number}}{\\html{houses}}{\\sign{=}}{\\grp{\\html{&#36;}}{\\mixed{\\input{$firstunitwhole}}{\\frac{\\input{$firstunitn}}{\\input{$firstunitd}}}}{\\html{million dollars}}}}' +
+                            '}',
                         controls: {
                             "checkAnswer": true,
                             "help": true
@@ -390,13 +418,13 @@ angular.module('mathSkills').service('data8_5', ['dataUtils', function (dataUtil
         data = [
             { total: '2.7',firstunitwhole: '2', firstunitn: '7', firstunitd: '10', secondunitn: '3', secondunitd: '10', number: '9', illustration: '8.5-11_Houses_Money/400x300/Millions_houses_a.jpg', template: 'housesDiv1' },
             { total: '2.7',firstunitwhole: '2', firstunitn: '7', firstunitd: '10', secondunitn: '3', secondunitd: '10', number: '9', illustration: '8.5-11_Houses_Money/400x300/Millions_houses_b.jpg', template: 'housesDiv2' },
-            { total: 15, unitn: 3, unitd: 4, number: 20, illustration: 'Acres_Gplane_15c.jpg', template: 'housesMul' },
-            { total: 21, unitn: 1, unitd: 3, number: 63, illustration: 'Acres_Gplane_21a.jpg', template: 'housesDiv1' },
+            { total: '2.7',firstunitwhole: '2', firstunitn: '7', firstunitd: '10', secondunitn: '3', secondunitd: '10', number: '9', illustration: '8.5-11_Houses_Money/400x300/Millions_houses_c.jpg', template: 'housesMul' },
+            /*{ total: 21, unitn: 1, unitd: 3, number: 63, illustration: 'Acres_Gplane_21a.jpg', template: 'housesDiv1' },
             { total: 21, unitn: 1, unitd: 3, number: 63, illustration: 'Acres_Gplane_21b.jpg', template: 'housesDiv2' },
             { total: 21, unitn: 1, unitd: 3, number: 63, illustration: 'Acres_Gplane_21c.jpg', template: 'housesMul' },
             { totaln: 2, totald: 3, unitn: 1, unitd: 9, number: 6, illustration: 'Acres_Gplane_2.3a.jpg', template: 'housesDivFrac1' },
             {  totaln: 2, totald: 3, unitn: 1, unitd: 9, number: 6, illustration: 'Acres_Gplane_2.3b.jpg', template: 'housesDivFrac2' },
-            {  totaln: 2, totald: 3, unitn: 1, unitd: 9, number: 6, illustration: 'Acres_Gplane_2.3c.jpg', template: 'housesMulFrac' }
+            {  totaln: 2, totald: 3, unitn: 1, unitd: 9, number: 6, illustration: 'Acres_Gplane_2.3c.jpg', template: 'housesMulFrac' }*/
         ];
     
     return dataUtils.build(desc, template, data);
