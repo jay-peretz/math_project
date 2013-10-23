096094159e637ed848bd5bffdce18b3c3d175311
markafoote <mark@getthatwebsite.com>
2013-10-23 13:42:31 -0700

    14.2, enlarge decimal in blue answer section

--- app/data371/data14_2.js
+++ app/data371/data14_2.js
@@ -16,7 +16,7 @@ angular.module('mathSkills').service('data14_2', ['dataUtils', function (dataUti
 						answer: '\\input{$answer}',
 						controls: {
 							"checkAnswer": true,
-							"help": '\\rowgrp{\\row{\\multiplydecpowten{$number}{$multiplier}}{\\css{\\str{Answer:  $helpAnswer}}{help-answer-text help-answer-margin-right}}}'
+							"help": '\\rowgrp{\\row{\\multiplydecpowten{$number}{$multiplier}}{\\css{\\html{Answer:  $helpAnswer}}{help-answer-text help-answer-margin-right}}}'
 						}
                     }]
                 }]
@@ -27,12 +27,12 @@ angular.module('mathSkills').service('data14_2', ['dataUtils', function (dataUti
 			{ number: '722', multiplier: '10', answer: '7220', helpAnswer: '7,220', template: 'main' },
 			{ number: '0.1', multiplier: '1000', answer: '100', helpAnswer: '100', template: 'main' },
 			{ number: '3.36', multiplier: '1000', answer: '3360', helpAnswer: '3,360', template: 'main' },
-			{ number: '82', multiplier: '0.001', answer: '0.082', helpAnswer: '0.082', template: 'main' },
-			{ number: '0.1', multiplier: '0.001', answer: '0.0001', helpAnswer: '0.0001', template: 'main' },
-			{ number: '38.5', multiplier: '0.001', answer: '0.0385', helpAnswer: '0.0385', template: 'main' },
-			{ number: '80.62', multiplier: '0.00001', answer: '0.0008062', helpAnswer: '0.0008062', template: 'main' },
+			{ number: '82', multiplier: '0.001', answer: '0.082', helpAnswer: '0<span class=largeDecimal>.</span>082', template: 'main' },
+			{ number: '0.1', multiplier: '0.001', answer: '0.0001', helpAnswer: '0<span class=largeDecimal>.</span>0001', template: 'main' },
+			{ number: '38.5', multiplier: '0.001', answer: '0.0385', helpAnswer: '0<span class=largeDecimal>.</span>0385', template: 'main' },
+			{ number: '80.62', multiplier: '0.00001', answer: '0.0008062', helpAnswer: '0<span class=largeDecimal>.</span>0008062', template: 'main' },
 			{ number: '988', multiplier: '1000', answer: '988000', helpAnswer: '988,000', template: 'main' },
-			{ number: '7.6', multiplier: '0.01', answer: '0.076', helpAnswer: '0.076', template: 'main' }       
+			{ number: '7.6', multiplier: '0.01', answer: '0.076', helpAnswer: '0<span class=largeDecimal>.</span>076', template: 'main' }       
        ];
 
     return dataUtils.build(desc, template, data);
