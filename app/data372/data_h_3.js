8f32bf729213695881dfceacfe2706b9c8fa4e18
kavithaselvaraj <selvaraj.kavi@gmail.com>
2013-11-01 15:48:13 -0700

    Copied few lines from data 10_4.js to data_h_3.js

--- app/data372/data_h_3.js
+++ app/data372/data_h_3.js
@@ -1,9 +1,22 @@
 /*global angular */
 
-angular.module('mathSkills').value('data_h_3', {
+angular.module('mathSkills').service('data10_4', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '10.4 Subtracting Mixed Numbers',
+			path: '10.4-subtracting-mixed-numbers',
+            children: []
+        },
+/*angular.module('mathSkills').value('data_h_3', {
     title: 'H.3 Subtraction of Mixed Numbers',
     path: 'H.3-subtraction-of-mixed-numbers',
     children: [{
         title: 'Subtraction of Mixed Numbers',
         children: [] }]
 });
+
+angular.module('mathSkills').service('data10_4', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: '10.4 Subtracting Mixed Numbers',
+			path: '10.4-subtracting-mixed-numbers',
+            children: []
+        },*/
