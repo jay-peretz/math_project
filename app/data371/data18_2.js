815f0f614851e7cf8daa7afe85d87509061d1f6a
markafoote <mark@getthatwebsite.com>
2013-11-01 12:48:48 -0700

    18.2, begin add second step problems 6-10

--- app/data371/data18_2.js
+++ app/data371/data18_2.js
@@ -23,18 +23,102 @@ angular.module('mathSkills').service('data18_2', ['dataUtils', function (dataUti
 						}
                     }]
                 }]
+            },
+			twoStepNoReduce: {
+                title: 'Change a Decimal to a Percent',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+                         problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{$startNumber}}}',
+						answer: '\\grp{\\input{$answer}}{\\str{%}}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\percentarrows{$startNumber}{100}{decimal}{percent}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\css{\\grp{\\str{Answer:  $answer}}{\\str{%}}}{help-answer-text help-answer-margin-right}}'
+						}
+                    },
+					{
+                         problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{$startNumber}}}',
+						answer: '\\css{\\rowgrp'
+						+'{\\grp{\\html{$answer}}{\\str{%}}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\css{\\str{Now change the decimal percent into a fraction or mixed number percent:}}{alert alert-info}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\grp{\\mixed{\\input{$finalWhole}}{\\frac{\\input{$finalNum}}{\\input{$finalDen}}}}{\\str{%}}}'						
+						+'}{well}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\css{\\str{Write the decimal place value as the denominator of the fraction:}}{label_like}}'
+							+'{\\grp{\\html{$answer}}{\\str{%}}'
+							+'{\\sign{\xA0 \xA0 = \xA0 \xA0}}'
+							+'{\\mixed{\\str{$finalWhole}}{\\frac{\\fracstr{$finalNum}}{\\fracstr{$finalDen}}}}{\\str{%}}}'
+							+'{\\html{&nbsp;}}'
+							+'{css{\\grp{\\str{Answer: \xA0 \xA0}}{\\mixed{\\str{$finalWhole}}{\\frac{\\fracstr{$finalNum}}{\\fracstr{$finalDen}}}}{\\str{%}}}{help-answer-text}}'
+						}
+                    }]
+                }]
+            },
+			twoStepReduce: {
+                title: 'Change a Decimal to a Percent',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+                         problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{$startNumber}}}',
+						answer: '\\grp{\\input{$answer}}{\\str{%}}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\percentarrows{$startNumber}{100}{decimal}{percent}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\css{\\grp{\\str{Answer:  $answer}}{\\str{%}}}{help-answer-text help-answer-margin-right}}'
+						}
+                    },
+					{
+                         problem: '\\rowgrp{\\row{\\str{Change this decimal into a percent:}}}{\\row{\\str{\xA0}}}{\\row{\\str{$startNumber}}}',
+						answer: '\\css{\\rowgrp'
+						+'{\\grp{\\html{$answer}}{\\str{%}}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\css{\\str{Now change the decimal percent into a fraction or mixed number percent:}}{alert alert-info}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\grp{\\mixed{\\input{$finalWhole}}{\\frac{\\input{$finalNum}}{\\input{$finalDen}}}}{\\str{%}}}'						
+						+'}{well}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\css{\\str{Write the decimal place value as the denominator of the fraction:}}{label_like}}'
+							+'{\\grp{\\html{$answer}}{\\str{%}}'
+							+'{\\sign{\xA0 \xA0 = \xA0 \xA0}}'
+							+'{\\mixed{\\str{$finalWhole}}{\\frac{\\fracstr{$helpNum}}{\\fracstr{$helpDen}}}}{\\str{%}}}'
+							+'{\\html{&nbsp;}}'
+							+'{css{\\grp{\\str{Answer: \xA0 \xA0}}{\\mixed{\\str{$finalWhole}}{\\frac{\\fracstr{$finalNum}}{\\fracstr{$finalDen}}}}{\\str{%}}}{help-answer-text}}'
+						}
+                    }]
+                }]
             }
         },
         data = [
+			// problem 1
             { startNumber: '8.41', answer: '841', template: 'main'  },
+			// problem 2
 			{ startNumber: '0.6', answer: '60', template: 'main'  },
-			{ startNumber: '0.66', answer: '0.66', template: 'main'  },
+			// problem 3
+			{ startNumber: '0.66', answer: '66', template: 'main'  },
+			// problem 4
 			{ startNumber: '0.54', answer: '54', template: 'main'  },
+			// problem 5
 			{ startNumber: '7.4', answer: '740', template: 'main'  },
-			{ startNumber: '0.7758', answer: '77.58', template: 'main'  },
+			// problem 6
+			{ startNumber: '0.7758', answer: '77.58', finalWhole: '77', finalNum: '29', finalDen: '50', helpNum: '58', helpDen: '100', template: 'twoStepReduce'  },
+			// problem 7
 			{ startNumber: '0.463', answer: '46.3', template: 'main'  },
+			// problem 8
 			{ startNumber: '0.8769', answer: '87.69', template: 'main'  },
+			// problem 9
 			{ startNumber: '1.432', answer: '143.2', template: 'main'  },
+			// problem 10
 			{ startNumber: '0.5', answer: '50', template: 'main'  }
         ];
 
