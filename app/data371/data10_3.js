8fd56969c7a17ae4e1e22dd9f8644de5f39d7cb3
markafoote <mark@getthatwebsite.com>
2013-11-05 12:37:21 -0800

    10.3 & 10.4, align fractions in step 4 or step 6 of some templates

--- app/data371/data10_3.js
+++ app/data371/data10_3.js
@@ -125,7 +125,7 @@ angular.module('mathSkills').service('data10_3', ['dataUtils', function (dataUti
 										+'{\\html{&nbsp;}}'
 										+'{\\grp{\\sign{+}}{\\grp{\\css{\\mixed{\\str{$whole2}}{\\frac{\\str{$numnew2}}{\\str{$answerDen}}}}{border-bottom-with-padding}}}}'
 										+'{\\html{&nbsp;}}'
-										+'{\\grp{\\html{<span style="font-size:300%;">&nbsp;</span>}}{\\mixed{\\fracstr{$wholeanswer1}}{\\frac{\\fracstr{$numanswer1}}{\\fracstr{$answerDen}}}}}'
+										+'{\\grp{\\html{<span style="font-size:550%;">&nbsp;</span>}}{\\mixed{\\fracstr{$wholeanswer1}}{\\frac{\\fracstr{$numanswer1}}{\\fracstr{$answerDen}}}}}'
 										+'{\\html{&nbsp;}}'
 										+'{\\ins{Rewrite the answer as a mixed number with a proper fraction:}}'
 										+'{\\grp{\\mixed{\\str{$wholeanswer1}}{\\frac{\\str{$numanswer1}}{\\str{$answerDen}}}}{\\sign{\xA0 \xA0 = \xA0 \xA0}}{\\mixed{\\input{$wholeanswer2}}{\\frac{\\input{$numanswer2}}{\\input{$answerDen}}}}}'
@@ -820,15 +820,25 @@ angular.module('mathSkills').service('data10_3', ['dataUtils', function (dataUti
             },
         },
         data = [
+			// problem 1
             { whole1: 5, num1: 4, den1: 5, whole2: 2, num2: 3, den2: 4, wholeanswer1: 7, wholeanswer2: 8, numanswer1: 31,  numanswer2: 11,answerDen: 20, numnew1: 16, numnew2: 15, template: 'main' },
+			// problem 2
 			{ whole1: 2, num1: 5, den1: 6, whole2: 1, num2: 8, den2: 9, wholeanswer1: 3, wholeanswer2: 4, numanswer1: 31,  numanswer2: 13,answerDen: 18, numnew1: 15, numnew2: 16, template: 'main' },
+			// problem 3
 			{ whole1: 2, num1: 2, den1: 3, whole2: 1, num2: 3, den2: 5, wholeanswer1: 3, wholeanswer2: 4, numanswer1: 19,  numanswer2: 4,answerDen: 15, numnew1: 10, numnew2: 9, template: 'main' },
+			// problem 4
 			{ whole1: 2, num1: 1, den1: 10, whole2: 1 , num2: 5, den2: '\xA06\xA0', wholeanswer1: 3, numanswer1: 28, answerDen1: 30, numnew1: 3, numnew2: 25, wholeanswer2: 3, numanswer2: 14, answerDen2: 15, template: 'third' },
+			// problem 5
 			{ whole1: 2, num1: 3, den1: 8, whole2: 1, num2: 1, den2: 5, wholeanswer1: 3, wholeanswer2: 3, numanswer1: 23,  numanswer2: 23,answerDen: 40, numnew1: 15, numnew2: '\xA08', template: 'second' },
-			{ whole1: 2, num1: 7, den1: 12, whole2: 1 , num2: 1, den2: '\xA06\xA0', wholeanswer1: 3, numanswer1: 9, answerDen1: 12, numnew1: 7, numnew2: 2, wholeanswer2: 3, numanswer2: 3, answerDen2: 4, template: 'third' },		
+			// problem 6
+			{ whole1: 2, num1: 7, den1: 12, whole2: 1 , num2: 1, den2: '\xA06\xA0', wholeanswer1: 3, numanswer1: 9, answerDen1: 12, numnew1: 7, numnew2: 2, wholeanswer2: 3, numanswer2: 3, answerDen2: 4, template: 'third' },
+			// problem 7
 			{ whole1: 2, num1: 2, den1: 9, whole2: 1, num2: 5, den2: 8, wholeanswer1: 3, wholeanswer2: 3, numanswer1: 61,  numanswer2: 61,answerDen: 72, numnew1: 16, numnew2: 45, template: 'second' },
+			// problem 8
 			{ whole1: 2, num1: 1, den1: 12, whole2: 1, num2: 3, den2: '\xA04\xA0', whole3: 4,  num3: 5, den3: '\xA06\xA0', wholeanswer1: 7, numanswer1: 20, answerDen1: 12, numnew1: '\xA01', numnew2: '\xA09', numnew3: 10, wholeanswer2: 8, numanswer2: 5, answerDen2: 3, numanswer3: 2, template: 'fourth' },
+			// problem 9
 			{ whole1: 2, num1: 5, den1: '\xA08\xA0', whole2: 1, num2: 1, den2: 10, whole3: 1, num3: 1, den3: '\xA04\xA0', wholeanswer1: 4, numanswer1: 39, answerDen1: 40, numnew1: 25, numnew2: '\xA04', numnew3: 10, wholeanswer2: 4, numanswer2: 39, answerDen2: 40, template: 'fifth' },
+			// problem 10
 			{ whole1: 2, num1: 1, den1: 6, whole2: 3, num2: 1, den2: 4, whole3: 1, num3: 1, den3: 4, wholeanswer1: 6, numanswer1: 8, answerDen1: 12, numnew1: 2, numnew2: 3, numnew3: 3, wholeanswer2: 6, numanswer2: 2, answerDen2: 3, template: 'sixth' },
         ];
 
