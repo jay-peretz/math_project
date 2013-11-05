62e7c47eaa94fb9eaeb572faf0907326e5c5ba62
markafoote <mark@getthatwebsite.com>
2013-11-05 12:48:21 -0800

    apply 10.4 changes to H.3

--- app/data372/data_h_3.js
+++ app/data372/data_h_3.js
@@ -232,7 +232,7 @@ angular.module('mathSkills').service('data_h_3', ['dataUtils', function (dataUti
 										+'{\\html{&nbsp;}}'
 										+'{\\grp{\\sign{-}}{\\grp{\\css{\\mixed{\\str{$whole2}}{\\frac{\\fracstr{$numnew2}}{\\str{$denanswer1}}}}{border-bottom-with-padding}}}}'
 										+'{\\html{&nbsp;}}'
-										+'{\\grp{\\html{<span style="font-size:300%;">&nbsp;</span>}}{\\mixed{\\fracstr{$wholeanswer}}{\\frac{\\fracstr{$numanswer1}}{\\fracstr{$denanswer1}}}}}'
+										+'{\\grp{\\html{<span style="font-size:550%;">&nbsp;</span>}}{\\mixed{\\fracstr{$wholeanswer}}{\\frac{\\fracstr{$numanswer1}}{\\fracstr{$denanswer1}}}}}'
 										+'{\\html{&nbsp;}}'
 										+'{\\reducefrac{mixed{\\str{$wholeanswer}}{\\frac{\\fracstr{$numanswer1}}{\\fracstr{$denanswer1}}}}}'
 									+'}{well}',
@@ -637,7 +637,7 @@ fourth: {
 									+'{\\html{&nbsp;}}'
 									+'{\\grp{\\sign{-}}{\\grp{\\css{\\mixed{\\str{$whole2}}{\\frac{\\str{$numnew2}}{\\str{$dennew1}}}}{border-bottom-with-padding}}}}'
 									+'{\\html{&nbsp;}}'
-									+'{\\grp{\\html{<span style="font-size:300%;">&nbsp;</span>}}{\\mixed{\\fracstr{$wholeanswer}}{\\frac{\\fracstr{$numnew4}}{\\fracstr{$dennew1}}}}}'
+									+'{\\grp{\\html{<span style="font-size:550%;">&nbsp;</span>}}{\\mixed{\\fracstr{$wholeanswer}}{\\frac{\\fracstr{$numnew4}}{\\fracstr{$dennew1}}}}}'
 									+'{\\html{&nbsp;}}'
 									+'{\\reducefrac{mixed{\\str{$wholeanswer}}{\\frac{\\fracstr{$numnew4}}{\\fracstr{$dennew1}}}}}'
 								+'}{well}',
@@ -650,15 +650,25 @@ fourth: {
             },
         },
         data = [
+			// problem 1
             { whole1: 2, num1: 4, den1: 5, whole2: 1, num2: 3, den2: 4, wholeanswer: 1, numanswer: 1, denanswer: 20, numnew1: 16, numnew2: 15, template: 'main' },
+			// problem 2
 			{ whole1: 3, num1: 8, den1: 9, whole2: 2, num2: 5, den2: 6, wholeanswer: 1, numanswer: 1, denanswer: 18, numnew1: 16, numnew2: 15, template: 'main' },
+			// problem 3
 			{ whole1: 5, num1: 2, den1: 3, whole2: 3, num2: 3, den2: 5, wholeanswer: 2, numanswer: 1, denanswer: 15, numnew1: 10, numnew2: 9, template: 'main' },
+			// problem 4
 			{ whole1: 6, num1: 9, den1: 10, whole2: 2, num2: 5, den2: '\xA06\xA0', wholeanswer: 4, numanswer1: 2, denanswer1: 30, numnew1: 27, numnew2: 25, numanswer2: 1, denanswer2: 15, template: 'second' },
+			// problem 5
 			{ whole1: 5, num1: 3, den1: 8, whole2: 2, num2: 3, den2: 5, wholeanswer: 2, numanswer: 31, denanswer: 40, numnew1: 15,  dennew1: 40, numnew2: 24, wholeReduced: 4, numnew3: 55, template: 'third' },
+			// problem 6
 			{ whole1: 3, num1: 5, den1: 12, whole2: 1, num2: 3, den2: '\xA04\xA0', wholeanswer: 1, numanswer: 2, denanswer: 3, numnew1: 5,   dennew1: 12, numnew2: 9, wholeReduced: 2, numnew3: 17, numnew4: 8, template: 'fourth' },
+			// problem 7
 			{ whole1: 6, num1: 7, den1: 9, whole2: 5, num2: 2, den2: 3, wholeanswer: 1, numanswer: 1, denanswer: 9, numnew1: 7, numnew2: 6, template: 'main' },
+			// problem 8
 			{ whole1: 4, num1: 1, den1: '\xA06\xA0', whole2: 1, num2: 3, den2: 10, wholeanswer: 2, numanswer: 13, denanswer: 15, numnew1: 5,   dennew1: 30, numnew2: 9, wholeReduced: 3, numnew3: 35, numnew4: 26, template: 'fourth' },
+			// problem 9
 			{ whole1: 5, num1: 9, den1: 10, whole2: 2, num2: 2, den2: '\xA05\xA0', wholeanswer: 3, numanswer1: 5, denanswer1: 10, numnew1: 9, numnew2: 4, numanswer2: 1, denanswer2: 2, template: 'second' },
+			// problem 10
 			{ whole1: 7, num1: 5, den1: 8, whole2: 5, num2: 4, den2: 5, wholeanswer: 1, numanswer: 33, denanswer: 40, numnew1: 25,  dennew1: 40, numnew2: 32, wholeReduced: 6, numnew3: 65, template: 'third' },
         ];
 
