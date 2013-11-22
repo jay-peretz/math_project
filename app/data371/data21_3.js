d64106a93d418ef7e78769a9e3aea5386c617c38
markafoote <mark@getthatwebsite.com>
2013-11-22 11:08:38 -0800

    21.3, corrections per Matt

--- app/data371/data21_3.js
+++ app/data371/data21_3.js
@@ -33,9 +33,9 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                                         '\\ins{Set up a percent proportion to solve the problem (use "X" for an unknown value).}'+
                                     '}{'+
                                         '\\grp'+
-                                        '{\\frac{\\input{[\"$$ln\",\"$$rn\"]}}{\\input{[\"$$ld\",\"$$rd\"]}}}'+
+                                        '{\\frac{\\input{[\"$ln\",\"$rn\"]}}{\\input{[\"$ld\",\"$rd\"]}}}'+
                                         '{\\sign{=}}'+
-                                        '{\\frac{\\input{[\"$$rn\",\"$$ln\"]}}{\\input{[\"$$rd\",\"$$ld\"]}}}'+
+                                        '{\\frac{\\input{[\"$rn\",\"$ln\"]}}{\\input{[\"$rd\",\"$ld\"]}}}'+
                                     '}'+
                                 '}{well}',
                         controls: {
@@ -145,7 +145,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
             }
         },
         data = [
-            { //1a
+            { //1a problem 1
                 problem: 'Monique sells $4,500 worth of clothing in one week. If she earns a 2% commission on her total sales, how much commission did she earn?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '90.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -158,7 +158,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //1b
+            { //1b problem 2
                 problem: 'Monique earns a 2% connission on her total sales. If she earned $90 in commissions last week, what were her total sales?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '4500.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -171,7 +171,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //1c
+            { //1c problem 3
                 problem: 'Monique sold $4,500 worth of clothing last week and earned $90 in commission. What is her commission rate?',
                 answerIn: dataUtils.pre('\\input{$$answer}'),
                 answer: '2', template: 'main', pre_symbol: '', post_symbol: '%', 
@@ -185,7 +185,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
             
-            { //2a
+            { //2a problem 4
                 problem: 'At a sporting goods store, Yani sells $6,200 worth of merchandise one week. If she earns a 3% commission on her total sales, how much commission did she earn?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '186.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -198,7 +198,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //2b
+            { //2b problem 5
                 problem: 'Yani earns a 3% commission on her total sales. If she earned $186 in commission last week, what were her total sales?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '6200.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -211,7 +211,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //2c
+            { //2c problem 6
                 problem: 'Yani sold $6,200 at a sporting goods store last week and earned $186 in commission. What is her commission rate?',
                 answerIn: dataUtils.pre('\\input{$$answer}'),
                 answer: '3', template: 'main', pre_symbol: '', post_symbol: '%', 
@@ -225,7 +225,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
             
-            { //3a
+            { //3a problem 7
                 problem: 'Brian sells $14,000 worth of jewelry one week. If he earns a 2.5% commission on his total sales, how much commission did he earn?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '350.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -238,8 +238,8 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //3b
-                problem: 'Brian earns a 3% commission on his total sales. If he earned $186 in commission last week, what were his total sales?',
+            { //3b problem 8
+                problem: 'Brian earns a 3% commission on his total sales. If he earned $350 in commission last week, what were his total sales?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '14000.00', template: 'main', pre_symbol: '$', post_symbol: '', 
                 key: 'rd', keyf: 'ld', 
@@ -251,7 +251,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //3c
+            { //3c problem 9
                 problem: 'At a jewelry store Brian sold $14,000 worth of merchandise last week and earned $350 in commission. What is his commission rate?',
                 answerIn: dataUtils.pre('\\input{$$answer}'),
                 answer: '2.5', template: 'main', pre_symbol: '', post_symbol: '%', 
@@ -265,7 +265,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
             
-            { //4a
+            { //4a problem 10
                 problem: 'At a used car dealership Justin sold a car for $4,800. If he earns an 8% commission on his total sales, how much commission did he earn?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '384.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -278,7 +278,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //4b
+            { //4b problem 11
                 problem: 'At a used car dealership Justin earns an 8% commission on his total sales. If he earned $384 in commission on a car he sold, for how much did he sell the car?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '4800.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -291,7 +291,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //4c
+            { //4c problem 12
                 problem: 'At a used car dealership Justin sold a car for $4,800 and earned $384 in commission. What was his commission rate?',
                 answerIn: dataUtils.pre('\\input{$$answer}'),
                 answer: '8', template: 'main', pre_symbol: '', post_symbol: '%', 
@@ -305,7 +305,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
             
-            { //5a
+            { //5a problem 13
                 problem: 'At a shoe store Vijay sold $8,850 worth of shoes. If he earns a 5% commission on his total sales, how much commission did he earn?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '442.50', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -318,7 +318,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //5b
+            { //5b problem 14
                 problem: 'At a shoe store Vijay earns a 5% commission on his total sales. If he earned $442.50 in commission last month, what were his total sales?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '8850.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -331,7 +331,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //5c
+            { //5c problem 15
                 problem: 'At a shoe store Vijay sold $8,850 worth of shoes and earned $442.50 in commission. What was his commission rate?',
                 answerIn: dataUtils.pre('\\input{$$answer}'),
                 answer: '5', template: 'main', pre_symbol: '', post_symbol: '%', 
@@ -345,7 +345,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
             
-            { //6a
+            { //6a problem 16
                 problem: 'At a computer security firm, Rosemary sold $82,460 worth of software. If she earns a 20% commission on her total sales, how much commission did she earn?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '16492.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -358,7 +358,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //6b
+            { //6b problem 17
                 problem: 'Rosemary earns a 20% commission on her total sales of computer security software. If she earned $16,492 in commission last week, what were her total software sales?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '82460.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -371,7 +371,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //6c
+            { //6c problem 18
                 problem: 'At a computer security firm, Rosemary sold $82,460 worth of software last month, and earned $16,492 in commission. What is her commission rate?',
                 answerIn: dataUtils.pre('\\input{$$answer}'),
                 answer: '20', template: 'main', pre_symbol: '', post_symbol: '%', 
@@ -385,7 +385,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
             
-            { //7a
+            { //7a problem 19
                 problem: 'Brian sells $11,300 worth of jewelry one week. If he earns a 4% commission on his total sales, how much commission did he earn?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '452.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -398,7 +398,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //7b
+            { //7b problem 20
                 problem: 'Brian earns a 4% commission on his total sales. If he earned $452 in commission last week, what were his total sales?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '11300.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -411,7 +411,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //7c
+            { //7c problem 21
                 problem: 'At a jewelry store Brian sold $11,300 worth of merchandise last week and earned $452 in commission. What is his commission rate?',
                 answerIn: dataUtils.pre('\\input{$$answer}'),
                 answer: '4', template: 'main', pre_symbol: '', post_symbol: '%', 
@@ -425,7 +425,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
             
-            { //8a
+            { //8a problem 22
                 problem: 'At a used car dealership Paula sold a car for $7,500. If she earns a 6% commission on her total sales, how much commission did she earn?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '450.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -438,7 +438,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //8b
+            { //8b problem 23
                 problem: 'At a used car dealership Paula earns a 6% commission on her total sales. If she earned $450 in commission on a car she sold, for how much did she sell the car?',
                 answerIn: dataUtils.pre('\\inputcash{$$answer}'),
                 answer: '7500.00', template: 'main', pre_symbol: '$', post_symbol: '', 
@@ -451,7 +451,7 @@ angular.module('mathSkills').service('data21_3', ['dataUtils', function (dataUti
                 previousCorrect: true,
                 flip: [[1], ["ln", "rn"], ["ld", "rd"], ["key", "keyf"]],
             },
-            { //8c
+            { //8c problem 24
                 problem: 'At a used car dealership Paula sold a car for $7,500 and earned $450 in commission. What was her commission rate?',
                 answerIn: dataUtils.pre('\\input{$$answer}'),
                 answer: '6', template: 'main', pre_symbol: '', post_symbol: '%', 
