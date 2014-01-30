956efcc35c76cf8fd3ec296fa4f96447e0287ab2
Shruti Agrawal <shruti14agrawal@gmail.com>
2014-01-29 20:41:19 -0800

    L-2 Add problems 1thru 10

--- app/data372/data_l_2.js
+++ app/data372/data_l_2.js
@@ -7,95 +7,100 @@ angular.module('mathSkills').service('data_l_2', ['dataUtils', function (dataUti
     		path: 'L.2-dividing-integers-decimals-fractions',
             children: []
         },
-	        template = {
-			// third template, two number operation, no parenthetical expression
-			third: {
+        template = {
+			// first template, integers operation
+			first: {
                 title: 'Evaluate Signed Expressions',
                 children: [{
                     title: 'Main Answer',
                     children: [{
 						problem: '\\rowgrp'
-						//+'{\\html{Evaluate:}}'
-						//+'{\\html{&nbsp;}}'
-						+'$prob',
-						answer: '\\input{$answer}',
+								  +'$prob',
+						answer:  '$answer',
 						controls: {
-							"checkAnswer": true,
-							"help": '\\rowgrp'
-							+'$firstHelp'
-							+'{\\html{&nbsp;}}'
-							//+'{\\html{"$secondHelp}}'
-							//+'{\\html{&nbsp;}}'
-							+'{css{\\html{answer:  $textAnswer}}{help-answer-text}}'
-						}
+									"checkAnswer": true,
+									"help": '\\rowgrp'
+											+'$firstHelp'
+											+'{\\html{&nbsp;}}'
+											+'{css{\\grp{\\html{answer: }}$textAnswer}{help-answer-text}}'
+							}
+						}]
 					}]
-				}]
-			},
-			// fourth template, two number operation, no sign inside 2nd term parenthetical expression
-			fourth: {
-                title: 'Evaluate Signed Expressions',
-                children: [{
-                    title: 'Main Answer',
-                    children: [{
-						problem: '\\rowgrp'
-						+'{\\html{Evaluate:}}'
-						+'{\\html{&nbsp;}}'
-						+'{\\grp{\\sign{$firstSign}}{html{$firstAbsNumber}}{\\sign{$secondSign}}{\\html{(}}{html{$secondAbsNumber}}{\\html{)}}}',
-						answer: '\\input{$answer}',
-						controls: {
-							"checkAnswer": true,
-							"help": '\\rowgrp'
-							+'{\\html{$firstHelp}}'
-							+'{\\html{&nbsp;}}'
-							+'{\\html{$secondHelp}}'
-							+'{\\html{&nbsp;}}'
-							+'{css{\\html{answer:  $textAnswer}}{help-answer-text}}'
-						}
-					}]
-				}]
-			},
-			// fifth template, two number operation, has a sign inside 2nd term parenthetical expression
-			fifth: {
-                title: 'Evaluate Signed Expressions',
-                children: [{
-                    title: 'Main Answer',
-                    children: [{
-						problem: '\\rowgrp'
-						+'{\\html{Evaluate:}}'
-						+'{\\html{&nbsp;}}'
-						+'{\\grp{\\sign{$firstSign}}{html{$firstAbsNumber}}{\\sign{$secondSign}}{\\html{(}}{\\sign{$thirdSign}}{html{$secondAbsNumber}}{\\html{)}}}',
-						answer: '\\input{$answer}',
-						controls: {
-							"checkAnswer": true,
-							"help": '\\rowgrp'
-							+'{\\html{$firstHelp}}'
-							+'{\\html{&nbsp;}}'
-							+'{\\html{$secondHelp}}'
-							+'{\\html{&nbsp;}}'
-							+'{css{\\html{answer:  $textAnswer}}{help-answer-text}}'
-						}
-					}]
-				}]
-			}	
+				},
         },
         data = [
 			
-			// problem 11 Division of two integers with the same sign, always results in positive. Just divide the absolute values and make the answer positive.
-			//{ outerSign: '-', innerSign: '+', absNumber: '7', textInnerSign: 'positive', firstHelp: 'The minus sign in front of a parenthesis means to take the opposite sign of the $$textInnerSign $$absNumber inside the parenthesis.', secondHelp: ' means take the opposite sign of the $$textInnerSign $$absNumber inside the parenthesis:', answer: '-7', textAnswer: ' &nbsp; -7', template: 'second' },
-			{ oneparen: '(', twoparen: ')',threeparen: '(', fourparen: ')', firstSign: '-', secondSign: '-', firstAbsNumber: '16', secondAbsNumber: '4',  firstTextSign: 'negative', secondTextSign: 'negative', firstHelp: 'The $$firstAbsNumber is $$firstTextSign and the $$secondAbsNumber is $$secondTextSign. <span class=underline>Division of two integers with the same sign, always results in positive.</span> Just divide the absolute values and make the answer positive.', answer: '4', textAnswer: ' &nbsp; 4', template: 'third',
-			prob: dataUtils.pre('{\\grp{\\html{(}}{\\sign{-}}{html{16}}{\\html{)}}{\\sign{&divide;}}{\\html{(}}{\\sign{-}}{html{4}}{\\html{)}}}'),
-			firstHelp: dataUtils.pre('{\\grp{\\html{The 16 is - and the 4 is -. <span class=underline>Division of two integers with the same sign, always results in positive.</span> Just divide the absolute values and make the answer positive.}}}') },
+			// problem 1
+			{prob: dataUtils.pre('{\\grp{\\html{(}}{\\sign{-}}{html{16}}{\\html{)}}{\\sign{&divide;}}{html{4}}}'),
+			answer: dataUtils.pre('\\grp{\\sign{=}}{\\input{-4}}'),
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{\\sign{-}}{html{4}}'),
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{The 16 is negative and the 4 is positive. <span class=underline>Division of two integers with the different signs, always results in negative.</span> Just divide the absolute values and make the answer negative.}}}') },
 			
-				//The values have <span class=underline>different</span> signs.  Therefore, <span class=underline>take the difference</span> between the two absolute values and <span class=underline>keep the sign of the larger value</span>.', secondHelp: 'The difference between 5 and 3 (5 - 3) is 2.  The $$firstTextSign value is the larger sign, so the answer is $$firstTextSign.', answer: '4', textAnswer: ' &nbsp; 4', template: 'third' },
-			// problem 12
-			{ oneparen: '', twoparen: '',threeparen: '', fourparen: '', firstSign: '', secondSign: '', thirdSign: '-', firstAbsNumber: '4', secondAbsNumber: '11', firstTextSign: 'positive', secondTextSign: 'negative', firstHelp: 'The $$firstAbsNumber is $$firstTextSign and the $$secondAbsNumber is $$secondTextSign.  The values have <span class=underline>different</span> signs.  Therefore, <span class=underline>take the difference</span> between the two absolute values and <span class=underline>keep the sign of the larger value</span>.', secondHelp: 'The difference between 4 and 11 (11 - 4) is 7.  The $$secondTextSign value is the larger sign, so the answer is $$secondTextSign.', answer: '-7', textAnswer: ' &nbsp; -7', template: 'third' },
-			// to be problem 15
-			{ firstSign: '-', secondSign: '-', firstAbsNumber: '6', secondAbsNumber: '4', firstTextSign: 'negative', secondTextSign: 'negative', firstHelp: 'The $$firstAbsNumber is $$firstTextSign and the $$secondAbsNumber becomes $$secondTextSign.  The values have <span class=underline>the same</span> sign.  Therefore, <span class=underline>take the sum</span> of the two absolute values and <span class=underline>keep the sign</span>.', secondHelp: 'The sum of 6 and 4 (6 + 4) is 10.  Both signs are $$secondTextSign, so the answer is $$secondTextSign.', answer: '-10', textAnswer: ' &nbsp; -10', template: 'fourth' },
-        ];
+			// problem 2
+			{prob: dataUtils.pre('{\\grp{\\sign{-}}{html{20}}{\\sign{&divide;}}{\\sign{-}}{html{2}}'),
+			answer: dataUtils.pre('\\grp{\\sign{=}}{\\input{10}}'), 
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{html{10}}'), 
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{The 20 is negative and the 2 is negative. <span class=underline>Division of two integers with the same signs, always results in positive.</span> Just divide the absolute values and make the answer positive.}}}') },
+			
+			// problem 3
+			{prob: dataUtils.pre('{\\grp{\\html{(}}{\\sign{-}}{html{10}}{\\html{)}}{\\sign{&divide;}}{html{5}}{\\sign{&divide;}}{\\html{(}}{\\sign{-}}{html{4}}{\\html{)}}}'),
+			answer: dataUtils.pre('\\grp{\\sign{=}}{\\input{0.5}}'),  
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{html{0.5}}'),
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{The 10 is negative, the 5 is positive and the 4 is negative. <span class=underline>Division of two integers with the same sign, always results in positive.</span> Just divide the absolute values and make the answer positive.}}}') },
+			
+			// problem 4
+			{prob: dataUtils.pre('{\\grp{\\html{(}}{\\sign{-}}{html{180}}{\\html{)}}{\\sign{&divide;}}{\\html{(}}{\\sign{-}}{html{0.3}}{\\html{)}}}'),
+			answer: dataUtils.pre('\\grp{\\sign{=}}{\\input{600}}'), 
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{html{600}}'), 
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{The 180 is negative and the 0.3 is negative. <span class=underline>Division of two numbers with the same signs, always results in positive.</span> Just divide the absolute values and make the answer positive.}}}') },
+			
+			// problem 5
+			{prob: dataUtils.pre('{\\grp{html{130.6}}{\\sign{&divide;}}{\\html{(}}{\\sign{-}}{html{32}}{\\html{)}}}'),
+			answer: dataUtils.pre('\\grp{\\sign{=}}{\\input{-4.05}}'),	 
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{\\sign{-}}{html{4.05}}'),
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{The 130.6 is positive and the 32 is negative. <span class=underline>Division of two numbers with the different sign, always results in negative.</span> Just divide the absolute values and make the answer negative.}}}') },
+			
+			// problem 6
+			{prob: dataUtils.pre('{\\grp{\\html{(}}{\\sign{-}}{\\frac{\\str{3}}{\\str{7}}}{\\html{)}}{\\sign{&divide;}}{html{8}}}'),
+			answer:dataUtils.pre('\\grp{\\sign{=}}{\\frac{\\input{-3}}{\\input{56}}}'),	
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{\\grp{\\sign{-}}{\\frac{\\str{3}}{\\str{56}}}}'), 
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{The 3/7 is negative and the 8 is positive. <span class=underline>Division of two numbers with the different sign, always results in negative.</span> Just divide the absolute values and make the answer negative.}}}') },
+
+			// problem 7
+			{prob: dataUtils.pre('{\\grp{\\html{(}}{\\sign{-}}{\\frac{\\str{5}}{\\str{8}}}{\\html{)}}{\\sign{&divide;}}{\\html{(}}{\\sign{-}}{\\frac{\\str{15}}{\\str{16}}}{\\html{)}}}'),
+			answer:dataUtils.pre('\\grp{\\sign{=}}{\\frac{\\input{2}}{\\input{3}}}'),	
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{\\grp{\\frac{\\str{2}}{\\str{3}}}}'), 
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{The 5/8 is negative and the 15/16 is negative. <span class=underline>Division of two numbers with the same signs, always results in positive.</span> Just divide the absolute values and make the answer positive.}}}') },
+
+			// problem 8
+			{prob: dataUtils.pre('{\\grp{\\html{(}}{\\sign{-}}{\\frac{\\str{3}}{\\str{4}}}{\\html{)}}{\\sign{&divide;}}{\\html{(}}{\\sign{-}}{\\frac{\\str{7}}{\\str{10}}}{\\html{)}}{\\sign{&divide;}}{\\sign{-}}{html{5}}}'),
+			answer:dataUtils.pre('\\grp{\\sign{=}}{\\frac{\\input{-3}}{\\input{14}}}'),	
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{\\grp{\\sign{-}}{\\frac{\\str{3}}{\\str{14}}}}'), 
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{The 3/4 is negative and the 7/10 is negative and the 5 is negative. <span class=underline>Division of two numbers with the same signs, always results in positive.</span> Just divide the absolute values and make the answer positive.}}}')} ,
+
+			// problem 9
+			{prob: dataUtils.pre('{\\grp{\\mixed{\\str{2}}{\\frac{\\fracstr{4}}{\\fracstr{5}}}}{\\sign{&divide;}}{\\html{(}}{\\sign{-}}{\\mixed{\\str{2}}{\\frac{\\fracstr{1}}{\\fracstr{3}}}}{\\html{)}}}'),
+			answer:dataUtils.pre('\\grp{\\sign{=}}{\\frac{\\input{-6}}{\\input{5}}}'),	
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{\\grp{\\sign{-}}{\\frac{\\str{6}}{\\str{5}}}}'), 
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{}}}') },
+//The  is positive and the  is negative. <span class=underline>Division of two numbers with the different signs, always results in negative.</span> Just divide the absolute values and make the answer negative.			
+			// problem 10
+			{prob: dataUtils.pre('{\\grp{html{4}}{\\sign{&divide;}}{\\html{(}}{\\sign{-}}{\\mixed{\\str{2}}{\\frac{\\fracstr{2}}{\\fracstr{3}}}}{\\html{)}}{\\sign{&divide;}}{\\mixed{\\str{2}}{\\frac{\\fracstr{1}}{\\fracstr{4}}}}}'),
+			answer:dataUtils.pre('\\grp{\\sign{=}}{\\frac{\\input{-2}}{\\input{3}}}'),	
+			textAnswer: dataUtils.pre('{\\html{&nbsp;}}{\\grp{\\sign{-}}{\\frac{\\str{2}}{\\str{3}}}}'), 
+			template: 'first',
+			firstHelp: dataUtils.pre('{\\grp{\\html{}}}') },
+//The  is positive and the  is negative. <span class=underline>Division of two numbers with the different signs, always results in a negative.</span> Just divide the absolute values and make the answer negative.
+			];
 
 	return dataUtils.build(desc, template, data);
 }]);
-
-
-
-
