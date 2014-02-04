22f4807dc4bcb4d681ba9c16cbba3a770f22596b
Irina Kramer <irina@sbcglobal.net>
2014-02-03 19:44:36 -0800

    E.1 - updated probs 1-4, 7, 10 with new data from Matt

--- app/data372/data_e_1.js
+++ app/data372/data_e_1.js
@@ -13,7 +13,7 @@ angular.module('mathSkills').service('data_e_1', ['dataUtils', function (dataUti
                     title: 'Main Answer',
                     children: [{
                         problem: '\\grp'
-						+'{\\html{Solve &nbsp;}}'
+						+'{\\html{Find the product of &nbsp;}}'
 						+'{\\frac{\\str{$n1}}{\\str{$d1}}}'
 						+'{\\sign{&times;}}'
 						+'{\\frac{\\str{$n2}}{\\str{$d2}}}',
@@ -28,7 +28,7 @@ angular.module('mathSkills').service('data_e_1', ['dataUtils', function (dataUti
                     title: 'Workbook',
                     children: [{
                         problem: '\\grp'
-						+'{\\html{Solve &nbsp;}}'
+						+'{\\html{Find the product of &nbsp;}}'
 						+'{\\frac{\\str{$n1}}{\\str{$d1}}}'
 						+'{\\sign{&times;}}'
 						+'{\\frac{\\str{$n2}}{\\str{$d2}}}',
@@ -46,7 +46,7 @@ angular.module('mathSkills').service('data_e_1', ['dataUtils', function (dataUti
                     title: 'Main Answer',
                     children: [{
                         problem: '\\rowgrp'
-						+'{\\html{Solve:}}'
+						+'{\\html{Find the product of:}}'
 						+'{\\html{&nbsp;}}'
 						+'{\\grp{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&times;}}{\\frac{\\str{$n2}}{\\str{$d2}}}{\\sign{&times;}}{\\frac{\\str{$n3}}{\\str{$d3}}}}',
                         answer: '\\frac{\\input{$na}}{\\input{$da}}',
@@ -60,7 +60,7 @@ angular.module('mathSkills').service('data_e_1', ['dataUtils', function (dataUti
                     title: 'Workbook',
                     children: [{
                         problem: '\\rowgrp'
-						+'{\\html{Solve:}}'
+						+'{\\html{Find the product of:}}'
 						+'{\\html{&nbsp;}}'
 						+'{\\grp{\\frac{\\str{$n1}}{\\str{$d1}}}{\\sign{&times;}}{\\frac{\\str{$n2}}{\\str{$d2}}}{\\sign{&times;}}{\\frac{\\str{$n3}}{\\str{$d3}}}}',
                         answer: '\\canfrac{[$n1,$d1]}{[$n2,$d2]}{[$n3,$d3]}{[$na,$da]}',
@@ -73,16 +73,16 @@ angular.module('mathSkills').service('data_e_1', ['dataUtils', function (dataUti
             }
         },
         data = [
-            { n1: 1, d1: 2, n2: 2, d2: 3, na: 1, da: 3, template: 'two' },
-            { n1: 1, d1: 4, n2: 2, d2: 6, na: 1, da: 12, template: 'two' },
-            { n1: 6, d1: 8, n2: 10, d2: 9, na: 5, da: 6, template: 'two' },
-            { n1: 3, d1: 6, n2: 6, d2: 2, na: 3, da: 2, template: 'two' },
-            { n1: 42, d1: 8, n2: 12, d2: 14, na: 9, da: 2, template: 'two' },
+            { n1: 7, d1: 5, n2: 3, d2: 5, na: 21, da: 25, template: 'two' },
+            { n1: 3, d1: 10, n2: 5, d2: 7, na: 3, da: 14, template: 'two' },
+            { n1: 5, d1: 8, n2: 8, d2: 15, na: 1, da: 3, template: 'two' },
+            { n1: 8, d1: 9, n2: 3, d2: 4, na: 2, da: 3, template: 'two' },
+            { n1: 9, d1: 20, n2: 14, d2: 3, na: 21, da: 10, template: 'two' },
             { n1: 15, d1: 6, n2: 20, d2: 35, na: 10, da: 7, template: 'two' },
-            { n1: 63, d1: 72, n2: 64, d2: 99, na: 56, da: 99, template: 'two' },
+            { n1: 3, d1: 17, n2: 2, d2: 5, na: 6, da: 85, template: 'two' },
             { n1: 3, d1: 5, n2: 10, d2: 18, n3: 15, d3: 20, na: 1, da: 4, template: 'three' },
             { n1: 12, d1: 24, n2: 36, d2: 6, n3: 18, d3: 32, na: 27, da: 16, template: 'three' },
-            { n1: 26, d1: 77, n2: 22, d2: 52, n3: 14, d3: 6, na: 1, da: 3, template: 'three' }
+            { n1: 10, d1: 3, n2: 6, d2: 25, n3: 1, d3: 9, na: 4, da: 45, template: 'three' }
         ];
     
     return dataUtils.build(desc, template, data);
