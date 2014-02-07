1c4a3ad26335d58eae32711398875555dd22f13a
markafoote <mark@getthatwebsite.com>
2014-02-07 11:21:40 -0800

    j.1 start numberline directive

--- app/data372/data_j_1.js
+++ app/data372/data_j_1.js
@@ -1,9 +1,43 @@
 /*global angular */
 
-angular.module('mathSkills').value('data_j_1', {
-    title: 'J.1 Number Line',
-    path: 'J.1-number-line',
-    children: [{
-        title: 'Number Line',
-        children: [] }]
-});
+angular.module('mathSkills').service('data_j_1', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: 'J.1 Number Line',
+    		path: 'J.1-number-line',
+            children: []
+        },
+	        template = {
+            second: {
+                title: 'Comparing Whole Numbers',
+				children: [{
+					title: 'Main Answer',
+					children: [{
+						problem: '',
+						answer: '\\pan{2}{10}{'+
+                                    '\\rowgrp{'+
+                                        '\\grp'+
+                                            '{\\str{X \xA0}}'+
+                                            '{\\btn{>}{$firstButton}}'+
+                                            '{\\str{\xA0 or \xA0}}'+
+                                            '{\\btn{<}{$secondButton}}'+
+                                            '{\\str{\xA0 Y}}'+
+                                    '}{'+
+                                        '\\rowgrp'+
+                                            '{\\html{<br><br>}}'+
+                                            '{\\numberline{$firstNumber}{$secondNumber}{$startGraph}{$endGraph}}'+
+                                    '}'+
+                                '}',
+						controls: {
+							"checkAnswer": false,
+							"help": true //'\\str{X or $firstNumber is $statement Y or $secondNumber}'
+						}
+					}]
+                }]
+            }
+        },
+        data = [
+			{ firstButton: 'F', secondButton: 'T', firstNumber: '25', secondNumber: '33', startGraph: '20', endGraph: '35', statement: 'LESS THAN', template: 'second', answerBtn: 'noAnswer' },
+        ];
+
+	return dataUtils.build(desc, template, data);
+}]);
---
+++ app/js/directives/ms-numberline.js
@@ -0,0 +1,133 @@
+'use strict';
+/*global angular */
+	
+angular.module('mathSkills') 
+	.config(['parserProvider', function (parserProvider) {
+        parserProvider.register('numberline', {
+            argTemplates: [{
+                name: 'firstnumber'
+            },{
+                name: 'secondnumber'
+            },{
+                name: 'graphminimum'
+            },{
+                name: 'graphmaximum'
+            }],
+            directiveTemplate: '<ms-numberline expected={{expected}}></ms-numberline>'
+        });
+    }])
+  .directive('msNumberline', [
+	'parser',
+	function (parser) {
+		return {
+			restrict: 'E',
+			scope: {
+				expected: '@',
+				label: '@'
+			},
+			controller: function ($scope, $element) {
+                $scope.controllerId = Math.random().toString();
+				$scope.answer = '';
+				$scope.numberLineArray = [];
+                $scope.controllerId = Math.random().toString();
+				var numberFirst = [],
+					numberSecond = [],
+					//graphminimum = [], -unused
+					//graphmaximum = [], -unused
+					minimumGraph = [],
+					maximumGraph = [],
+					numberLineArray = [],
+					numberFirst = 0,
+					numberSecond = 1;
+					
+				numberLineArray[0] = [];
+				numberLineArray[1] = [];
+					
+
+			 // Extract the value/s for $scope.firstnumber & $scope.secondnumber
+				$scope.$watch('expected', function () {
+					if ($scope.expected) {
+						parser.parse($scope.expected, { scope: $scope });
+					}
+				});
+				
+				$scope.$watch('firstnumber', function () {
+					if (typeof $scope.firstnumber === "string") {
+						numberFirst = $scope.firstnumber;
+					}
+				});
+				
+				$scope.$watch('secondnumber', function () {
+					if (typeof $scope.secondnumber === "string") {
+						numberSecond = $scope.secondnumber;
+					}
+				});
+								
+				$scope.$watch('graphminimum', function () {
+					if (typeof $scope.graphminimum === "string") {
+						minimumGraph = $scope.graphminimum;
+					}
+				});
+				
+				$scope.$watch('graphmaximum', function () {
+					if (typeof $scope.graphmaximum === "string") {
+						maximumGraph = $scope.graphmaximum;
+						
+						
+						// below number line display 
+						for (var ii = minimumGraph, jj = 0; ii <= maximumGraph; ii++) {
+								if ((ii != numberFirst)&&(ii != numberSecond)) {
+									numberLineArray[1][jj] = "\xA0"+ii+"\xA0";
+									numberLineArray[0][jj] = "\xA0"+"\xA0"+"|";
+								} else {
+									if (ii == numberFirst) {
+										numberLineArray[1][jj] = "\xA0"+"X"+"\xA0";
+										numberLineArray[0][jj] = "\xA0"+"\xA0"+"|";
+									} else if (ii == numberSecond) {
+										numberLineArray[1][jj] = "\xA0"+"Y"+"\xA0";
+										numberLineArray[0][jj] = "\xA0"+"\xA0"+"|";
+									}
+								}
+								jj++;
+						}
+							
+						$scope.numberLineArray = numberLineArray.slice();	
+					}
+				});
+                
+                // handle check answer event for parent scope "always correct".
+                    $scope.$on('checkAnswer', function () {
+                        $scope.$emit('answer', {
+                            result: 'correct',
+                            expected: $scope.expected,
+                            answer: $scope.expected,
+                            label: $scope.label
+                        });
+                    });
+    
+                    $scope.$on('checkFocus', function (e) {
+                        // If this event has not been marked as ignored.
+                        if (e.defaultPrevented === false) {
+                            // handle check focus event for parent scope.
+                            // comparewholesgraph cannot have focus/tab, fire a notFocused event.
+                            $scope.$emit('notFocused', {
+                                controllerId: $scope.controllerId
+                            });
+                        }
+                    });
+    
+                    $scope.$on('checkHelp', function (e) {
+                        // If this event has not been marked as ignored.
+                        if (e.defaultPrevented === false) {
+                            // handle check help event for parent scope.
+                            // comparewholesgraph does not use help, fire a notHelped event.
+                            $scope.$emit('notHelped', {
+                                controllerId: $scope.controllerId
+                            }); 
+                        }
+                    });
+                    
+			},
+			templateUrl: 'partials/directives/ms-comparewholesgraph.html'
+		};
+	}]);
