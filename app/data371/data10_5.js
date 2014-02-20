55c44f374e02f144978898407a7f7a64ee26828d
markafoote <mark@getthatwebsite.com>
2014-02-19 18:48:38 -0800

    10.5, templates for two problem types

--- app/data371/data10_5.js
+++ app/data371/data10_5.js
@@ -28,45 +28,95 @@ angular.module('mathSkills').service('data10_5', ['dataUtils', function (dataUti
 							"help": '\\rowgrp'
 										+'{\\grp'
 											+'{\\frac{\\fracstr{$firstNum}}{\\fracstr{$firstDen}}}'
-											+'$questionMark'	
+											+'{\\css{\\html{?}}{fakeButton}}'
 											+'{\\frac{\\fracstr{$secondNum}}{\\fracstr{$secondDen}}}'
 										+'}'
 										+'{html{&nbsp;}}'
 										+'{\\grp'
 											+'{\\frac{\\fracstr{$firstNum}}{\\fracstr{$firstDen}}}'
-											+'$questionMark'	
-											+'{\\frac{\\fracstr{$thirdNum}}{\\fracstr{$thirdDen}}}'
+											+'{\\css{\\html{?}}{fakeButton}}'
+											+'{\\frac{\\fracstr{$reduceNum}}{\\fracstr{$reduceDen}}}'
+										+'}'
+										+'{html{&nbsp;}}'
+										+'{\\css{\\html{Answer:}}{help-answer-text}}'
+										+'{html{&nbsp;}}'
+										+'{\\css'
+											+'{\\grp'
+												+'{\\html{Therefore}}'
+												+'{\\html{&nbsp;}}'
+												+'{\\frac{\\fracstr{$firstNum}}{\\fracstr{$firstDen}}}'
+												+'{\\sign{<}}'
+												+'{\\frac{\\fracstr{$reduceNum}}{\\fracstr{$reduceDen}}}'
+											+'}'
+										+'{help-answer-text}}'
+						}
+                    }]
+                }]
+           },
+		   second: {
+                title: 'Compare and Order Fractions',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp'
+									+'{\\html{Arrange from smallest to largest:}}'
+									+'{\\html{&nbsp;}}'
+									+'{\\grp'
+                                        +'{\\frac{\\fracstr{$firstNum}}{\\fracstr{$firstDen}}}'
+                                        +'{\\html{,}}'
+                                        +'{\\frac{\\fracstr{$secondNum}}{\\fracstr{$secondDen}}}'
+                                         +'{\\html{,}}'
+										+'{\\frac{\\fracstr{$thirdNum}}{\\fracstr{$thirdDen}}}'
+									+'}'
+									,
+						answer: '\\grp'
+                                        +'{\\frac{\\input{$firstAnswerNum}}{\\input{$firstAnswerDen}}}'
+                                        +'{\\sign{<}}'
+                                        +'{\\frac{\\input{$secondAnswerNum}}{\\input{$secondAnswerDen}}}'
+                                        +'{\\sign{<}}'
+										+'{\\frac{\\input{$thirdAnswerNum}}{\\input{$thirdAnswerDen}}}',
+						controls: {
+							"checkAnswer": false,
+							"help":'\\rowgrp' 
+										+'{\\grp'
+											+'{\\frac{\\fracstr{$firstNum}}{\\fracstr{$firstDen}}}'
+											+'{\\sign{=}}'
+											+'{\\frac{\\fracstr{$firstLCDNum}}{\\fracstr{$lcdDen}}}'
 										+'}'
 										+'{html{&nbsp;}}'
 										+'{\\grp'
-											+'{\\html{Therefore}}'
-											+'{\\html{&nbsp;}}'
-											+'{\\frac{\\fracstr{$firstNum}}{\\fracstr{$firstDen}}}'
-											+'{\\sign{<}}'
-											+'{\\frac{\\fracstr{$thirdNum}}{\\fracstr{$thirdDen}}}'
+											+'{\\frac{\\fracstr{$secondNum}}{\\fracstr{$secondDen}}}'
+											+'{\\sign{=}}'
+											+'{\\frac{\\fracstr{$secondLCDNum}}{\\fracstr{$lcdDen}}}'
 										+'}'
-										}
+										+'{html{&nbsp;}}'
+										+'{\\grp'
+											+'{\\frac{\\fracstr{$thirdNum}}{\\fracstr{$thirdDen}}}'
+											+'{\\sign{=}}'
+											+'{\\frac{\\fracstr{$thirdLCDNum}}{\\fracstr{$lcdDen}}}'
+										+'}'
+										+'{html{&nbsp;}}'
+										+'{\\css{\\html{Answer:}}{help-answer-text}}'
+										+'{html{&nbsp;}}'
+										+'{\\css'
+											+'{\\grp'
+												+'{\\frac{\\fracstr{$firstAnswerNum}}{\\fracstr{$firstAnswerDen}}}'
+												+'{\\sign{<}}'
+												+'{\\frac{\\fracstr{$secondAnswerNum}}{\\fracstr{$secondAnswerDen}}}'
+												+'{\\sign{<}}'
+												+'{\\frac{\\fracstr{$thirdAnswerNum}}{\\fracstr{$thirdAnswerDen}}}'
+											+'}'
+										+'{help-answer-text}}'
+						}
                     }]
                 }]
            }
         },
         data = [
-            { firstNum: '1', firstDen: '4', secondNum: '2', secondDen: '6', thirdNum: '1', thirdDen: '3', firstButton: 'T', secondButton: 'F', thirdButton: 'F', template: 'first', answerBtn: 'noAnswer',
-			questionMark: dataUtils.pre(
-								'{\\rowgrp'
-									+'{\\grp'
-										+'{\\html{&nbsp;}}'
-										+'{\\html{&nbsp;?}}'
-										+'{\\html{&nbsp;}}'
-									+'}'
-									+'{\\grp'
-										+'{\\html{<}}'
-										+'{\\html{=}}'
-										+'{\\html{>}}'
-									+'}'
-								+'}'
-					)
+            { firstNum: '1', firstDen: '4', secondNum: '2', secondDen: '6', reduceNum: '1', reduceDen: '3', firstButton: 'T', secondButton: 'F', thirdButton: 'F', template: 'first', answerBtn: 'noAnswer'
 			},
+			{ firstNum: '1', firstDen: '2', secondNum: '2', secondDen: '3', thirdNum: '3',thirdDen: '5', firstLCDNum: '15', secondLCDNum: '20', thirdLCDNum: '18', lcdDen: '30', firstAnswerNum: '1', firstAnswerDen: '2', secondAnswerNum: '3', secondAnswerDen: '5', thirdAnswerNum: '2', thirdAnswerDen: '3', template: 'second', answerBtn: 'noAnswer'
+			}
         ];
 
 	return dataUtils.build(desc, template, data);
