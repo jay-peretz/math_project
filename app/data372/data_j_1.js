951d5bdc88ca078991e59fe5657826feeb6e7c6f
Irina Kramer <irina@sbcglobal.net>
2014-03-04 21:47:19 -0800

    J.1 - added steps to solution panel, numberline edits, wip

--- app/data372/data_j_1.js
+++ app/data372/data_j_1.js
@@ -8,13 +8,11 @@ angular.module('mathSkills').service('data_j_1', ['dataUtils', function (dataUti
         },
         	template = {
             main: {
-                title: 'Charts and Bar Graphs', // IK - where does it show?
+                title: 'Charts and Bar Graphs', 
                 children: [{
                     title: 'Main Answer',
                     children: [{
-                        problem:'\\pan{$spanQ}{$spanA}{'+
-                                    '$chart'+
-                                '}', 
+                        problem:'\\pan{12}{11}{$chart}',
                         answer: '\\css{'+
                                     '\\rowgrp'+
                                         '{\\ins{$question}}'+
@@ -22,13 +20,72 @@ angular.module('mathSkills').service('data_j_1', ['dataUtils', function (dataUti
                                 '}{well}',
                         controls: {
                             "checkAnswer": true,
-                            "help": '\\rowgrp'+
-                                        '{\\ins{$question}}'+
-                                        '{\\html{<span class=>Answer:</span><br><br>$answertxt}}'
+                            "help": true
                         }
-                    }]
-                }]    
+                },{
+						problem:'\\pan{12}{11}{$chart}', 
+                        answer: '\\css{'+
+                                    '\\rowgrp'+
+                                        '{\\ins{Identify each value on the number line:}}'+
+                                        '{\\grp{\\str{A = }}{\\input{$val1}}}'+
+                                        '{\\grp{\\str{B = }}{\\input{$val2}}}'+
+                                        '{\\grp{\\str{C = }}{\\input{$val3}}}'+
+                                '}{well}',
+						controls: {
+							"checkAnswer": true,
+							"help": true
+						}
+					}]
+                }]
             },
+            /*main: {
+                title: 'Change a Percent to a Fraction (Directly)',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\rowgrp'
+						+'{\\str{Change this percent into a decimal:}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\grp{\\str{$decimalPercent}}{\\str{%}}}',
+						answer: '\\grp{\\input{$decimal}}',		
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\percentarrows{$decimalPercent}{0.01}{percent}{decimal}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\css{\\str{Answer:  $decimal}}{help-answer-text}}'
+						}
+					}, {
+						problem: '\\rowgrp'
+						+'{\\grp{\\str{Change this percent into a decimal:}}}'
+						+'{\\grp{\\html{&nbsp;}}}'
+						+'{\\grp{\\str{$decimalPercent}}{\\str{%}}}',
+						answer: '\\css{\\rowgrp'
+						+'{\\html{&nbsp;}}'
+						+'{\\grp{\\str{$decimalPercent}}{\\str{%}}{\\sign{=}}{\\str{$decimal}}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\html{&nbsp;}}'
+						+'{css{\\str{Change the decimal into a fraction:}}{alert alert-info}}'
+						+'{\\html{&nbsp;}}'
+						+'{\\grp{\\frac{\\input{$answerNum}}{\\input{$answerDen}}}}}{well}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+							+'{\\str{The decimal $decimal is converted by stating the decimal number out loud.  Use the appropriate place value:}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\grp{\\str{$decimal}}'
+							+'{\\sign{ \xA0 = \xA0 }}'
+							+'{\\str{"$outLoud"}}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\grp{\\str{or: \xA0}}{\\frac{\\fracstr{$helpNum}}{\\fracstr{$helpDen}}}}'
+							+'{\\html{&nbsp;}}'
+							+'{\\str{Reduce whenever possible.}}'
+							+'{\\html{&nbsp;}}'
+							+'{css{\\grp{\\str{Answer: \xA0 \xA0}}{\\frac{\\fracstr{$answerNum}}{\\fracstr{$answerDen}}}}{help-answer-text}}'
+						}
+					}]
+                }]
+            },*/
         },
 	        /*template = {
             second: {
@@ -67,13 +124,12 @@ angular.module('mathSkills').service('data_j_1', ['dataUtils', function (dataUti
 			{  //1 //15 from 2A.2
                 chart: dataUtils.pre('\\graphnum{Title}{[ ["January", 220], ["April", 370], ["July", 445], ["October", 300]]}{500}{Side title}'), 
                 question: 'What is the increment (the distance between any two consecutive ticks) on this graph?', template: 'main', answerBtn: 'noAnswer',
-                answertxt: 'There were approximately 590 deer counted in January and April.',
+                //answertxt: 'There were approximately 590 deer counted in January and April.',
                 answerIn: dataUtils.pre('\\rowgrp{$$button1}{$$button2}{$$button3}{$$button4}'),
                 button1: dataUtils.pre('\\grp{\\btn{A}{F}{marg}}{\\str{\xA0 \xA0 Answer 1}}'),
                 button2: dataUtils.pre('\\grp{\\btn{B}{F}{marg}}{\\str{\xA0 \xA0 Answer 2}}'),
                 button3: dataUtils.pre('\\grp{\\btn{C}{T}{marg}}{\\str{\xA0 \xA0 Answer 3}}'),
-                button4: dataUtils.pre('\\grp{\\btn{D}{F}{marg}}{\\str{\xA0 \xA0 Answer 4}}'),
-                spanQ:'5', spanA:'7'  
+                button4: dataUtils.pre('\\grp{\\btn{D}{F}{marg}}{\\str{\xA0 \xA0 Answer 4}}')
             },
 			/*{  //2 //16 from 2A.2
                 chart: dataUtils.pre('\\horgraph{Title of this graph}{[[0, "1965"], [2, "1965"], null, [0, "1985"], [3, "1985"], null, [0, "2005"], [11, "2005"], null, [0, "2025"],[22, "2025"]]}{Population of Country A}{[[0, "1962"], [3, "1962"], null, [0, "1982"], [5, "1982"], null, [0, "2002"], [9, "2002"], null, [0, "2022"],[18, "2022"]]}{Population Country A can Feed}{Years}{Number (in millions)}'), 
--- app/js/directives/ms-graph-numberline.js
+++ app/js/directives/ms-graph-numberline.js
@@ -82,15 +82,41 @@ angular.module('mathSkills')
 						},
 						xaxis: {
 							//mode: "categories",
-							ticks: 10,
-							min: -5,
-							max: 5,
+							color: "red", // color of the axis
+							tickColor: "blue",
+							tickLength: 10,	
+							//alignTicksWithAxis: 1,
+							ticks: function tickGenerator(axis) {
+							    var res = [], i = axis.min;
+							    do {
+							        var v = i * axis.tickSize;
+							        res.push([v, i ]);							        
+							        if (i == 0)
+							        	res.push(0);
+							        ++i;
+							    } while (v < axis.max);
+							    return res;
+							},					
+							//ticks: 10,
+							tickSize: 2,							
+							min: -10,
+							max: 10,
 							tickDecimals: 0
 						},
-						 yaxis: {ticks: 0, min: -1, max: 1 },
+						 yaxis: {ticks: 1, min: -1, max: 1 },
 						 
 						 grid: {
-							backgroundColor: { colors: [ "#fff", "#eee" ] },
+							show: true,
+							color: "#000", // color of the grid lines specified in xaxis
+							//backgroundColor: "#fff", 
+							//markings: [ { xaxis: { from: 0, to: 0 }, color: "#bb0000" } ],
+							markings: function (axes) {
+							    var markings = [];
+							    for (var x = axes.xaxis.min + 1; x < axes.xaxis.max; x++)
+							        markings.push({ xaxis: { from: x, to: x }, color: "999", lineWidth: 1 });
+							    return markings;
+							},
+							backgroundColor: { colors: [ "#fff", "#999" ] },
 							borderWidth: {
 							top: 0,
 							right: 0,
