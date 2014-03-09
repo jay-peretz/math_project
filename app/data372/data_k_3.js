fec0b6d778e38fb3def678c91aacc1ae18e7ff16
markafoote <mark@getthatwebsite.com>
2014-03-08 17:08:51 -0800

    revise k.3 per Matt

--- app/data372/data_k_3.js
+++ app/data372/data_k_3.js
@@ -14,7 +14,7 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
 					children: [{
 						problem: '\\html{'+
                                         'Translate the following words into a math expression:'+
-                                        '<br><br><span class=blue-text>$problemStatement</span>'+
+                                        '<br><br>$problemStatement'+
                                     '}',
 						answer: '\\rowgrp'+
                                     '{\\grp{\\btn{A}{$buttonValueA}{marg}}{\\html{\xA0$buttonLabelA}}}'+
@@ -26,29 +26,89 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
 						    "help": true
                         }
 					}, {
-						problem: '\\rowgrp'
-						+'{\\html{Solve:}}'
-						+'{\\html{&nbsp;}}'
-						+'{\\html{$problemStatement}}',
+						problem: '\\html{'+
+                                        'Translate the following words into a math expression:'+
+                                        '<br><br>$problemStatement'+
+                                    '}',
 						answer: '\\wb'
 									+'{\\rowgrp'
 										+'{\\html{$labelCorrect}}'
 										+'{\\html{&nbsp;}}'
+										+'{\\ins{Now solve the expression:}}'
 										+'{\\grp'
 											+'{\\input{$answer}}'
 										+'}'
 									+'}'
-								+'{well}{end}',
+								+'{well}',
 						controls: {
 							"checkAnswer": true,
 							"help": true
 						}
+					}]
+                }]
+            },
+			doubleNegative: {
+                title: 'Subtracting Integers and Decimals',
+				children: [{
+					title: 'Main Answer',
+					children: [{
+						problem: '\\html{'+
+                                        'Translate the following words into a math expression:'+
+                                        '<br><br>$problemStatement'+
+                                    '}',
+						answer: '\\rowgrp'
+                                    +'{\\grp{\\btn{A}{$buttonValueA}{marg}}{\\html{\xA0$buttonLabelA}}}'
+                                    +'{\\grp{\\btn{B}{$buttonValueB}{marg}}{\\html{\xA0$buttonLabelB}}}'
+                                    +'{\\grp{\\btn{C}{$buttonValueC}{marg}}{\\html{\xA0$buttonLabelC}}}'
+                                    +'{\\grp{\\btn{D}{$buttonValueD}{marg}}{\\html{\xA0$buttonLabelD}}}',
+						controls: {
+							"checkAnswer": true,
+						    "help": true
+                        }
 					}, {
-						problem: '\\rowgrp'
-						+'{\\html{Solve:}}'
-						+'{\\html{&nbsp;}}'
-						+'{\\html{$problemStatement}}',
-						answer: '$preAnswer',
+						problem: '\\html{'+
+                                        'Translate the following words into a math expression:'+
+                                        '<br><br>$problemStatement'+
+                                    '}',
+						answer: '\\css'
+									+'{\\rowgrp'
+										+'{\\html{$labelCorrect}}'
+										+'{\\html{&nbsp;}}'
+										+'{\\ins{What is the expression without double negatives?}}'
+										+'{\\grp{\\btn{A}{$buttonValueE}{marg}}{\\html{\xA0$buttonLabelE}}}'
+										+'{\\grp{\\btn{B}{$buttonValueF}{marg}}{\\html{\xA0$buttonLabelF}}}'
+										+'{\\grp{\\btn{C}{$buttonValueG}{marg}}{\\html{\xA0$buttonLabelG}}}'
+										+'{\\grp{\\btn{D}{$buttonValueH}{marg}}{\\html{\xA0$buttonLabelH}}}'
+									+'}'
+								+'{well}',
+						controls: {
+							"checkAnswer": true,
+							"help": '\\rowgrp'
+										+'{\\html{$correctLeftAligned}}'
+										+'{\\html{&nbsp;}}'
+										+'{\\html{The minus sign in front of a parenthesis means to take the opposite sign of the negative $absNumber inside the parenthesis.}}'
+										+'{\\html{&nbsp;}}'
+										+'{\\html{The expression without double negatives is:}}'
+										+'{\\html{&nbsp;}}'
+										+'{\\css{\\html{$noDoubleNegs}}{blue-text}}',
+						}
+					}, {
+						problem: '\\html{'+
+                                        'Translate the following words into a math expression:'+
+                                        '<br><br>$problemStatement'+
+                                    '}',
+						answer: '\\css'
+									+'{\\rowgrp'
+										+'{\\html{$correctLeftAligned}}'
+										+'{\\html{&nbsp;}}'
+										+'{\\html{$noDoubleNegs}}'
+										+'{\\html{&nbsp;}}'
+										+'{\\ins{Now solve the expression:}}'
+										+'{\\grp'
+											+'{\\input{$answer}}'
+										+'}'
+									+'}'
+								+'{well}',
 						controls: {
 							"checkAnswer": true,
 							"help": true
@@ -59,117 +119,22 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
         },
         data = [
 			//problem 1
-			{ problemStatement: 'Subtract 6 from 8', buttonValueA: 'F', buttonLabelA: '<span class=bigger>-</span> 6 <span class=bigger>-</span> 8', buttonValueB: 'T', buttonLabelB: '<span class=bigger>&nbsp;</span> 8 <span class=bigger>-</span> 6', buttonValueC: 'F', buttonLabelC: '<span class=bigger>-</span> 8 <span class=bigger>-</span> 6', buttonValueD: 'F', buttonLabelD: '<span class=bigger>&nbsp;</span> 6 <span class=bigger>-</span> 8', answer: '2',
+			{ problemStatement: 'Subtract 6 from 8', buttonValueA: 'F', buttonLabelA: '<span class=bigger>-</span> 6 <span class=bigger>-</span> 8', buttonValueB: 'T', buttonLabelB: '<span class=bigger>&nbsp;</span> 8 <span class=bigger>-</span> 6', buttonValueC: 'F', buttonLabelC: '<span class=bigger>-</span> 8 <span class=bigger>-</span> 6', buttonValueD: 'F', buttonLabelD: '<span class=bigger>&nbsp;</span> 6 <span class=bigger>-</span> 8',
+			answer: '2',
             labelCorrect: '<span class=bigger>&nbsp;</span> 8 <span class=bigger>-</span> 6',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{8}}{\\btn{-}{T}{bigger}}{\\html{6}},'
-											+'\\input{2},'
-										+'}{Evaluate the operator.}'
-									+'}{well}'
-								),
 			template: 'main'},
 			//problem 2
 			{ problemStatement: 'Subtract <span class=bigger>-</span>12 from 30', buttonValueA: 'F', buttonLabelA: '<span class=bigger>-</span>12 <span class=bigger>-</span> 30', buttonValueB: 'T', buttonLabelB: '<span class=bigger>&nbsp;</span>30 <span class=bigger>-</span> ( <span class=bigger>-</span>12 )', buttonValueC: 'F', buttonLabelC: '<span class=bigger>&nbsp;</span>30 <span class=bigger>-</span> 12', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>12 + 30', answer: '42',
             labelCorrect: '<span class=bigger>&nbsp;</span>30 <span class=bigger>-</span> ( <span class=bigger>-</span>12 )',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{30}}{\\btn{-}{T}{bigger}}{\\html{(}}{\\sign{-}}{\\html{12}}{\\html{)}},' 
-                        					+'\\input{+},' 
-											+'\\grp{\\html{30}}{\\btn{+}{T}{bigger}}{\\html{12}},' 
-                        					+'\\input{42},' 
-                    					+'}{Enter an operator.}{Evaluate the operator.}'
-									+'}{well}'
-									),
-			template: 'main'},
+			buttonValueE: 'F', buttonLabelE: '<span class=bigger>-</span>12 <span class=bigger>-</span> 30', buttonValueF: 'T', buttonLabelF: '<span class=bigger>&nbsp;</span>30 <span class=bigger>+</span> 12', buttonValueG: 'F', buttonLabelG: '<span class=bigger>&nbsp;</span>30 <span class=bigger>-</span> 12', buttonValueD: 'H', buttonLabelH: '<span class=bigger>-</span>12 + 30',
+			correctLeftAligned: '30 <span class=bigger>-</span> ( <span class=bigger>-</span>12 )',
+			noDoubleNegs: '30 <span class=bigger>+</span> 12',
+			absNumber: '12',
+			template: 'doubleNegative'},
 			//problem 3
 			{ problemStatement: 'Find the difference of <span class=bigger>-</span>25 and 10', buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>25 <span class=bigger>-</span> 10', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>10 <span class=bigger>-</span> 25', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>25 <span class=bigger>-</span> (<span class=bigger>-</span>10)', answer: '-35',
             labelCorrect: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{<span class=bigger>-</span>25}}{\\btn{-}{T}{bigger}}{\\html{10}},' 
-                        					+'\\input{-35},' 
-                    					+'}{Evaluate the operator.}'
-									+'}{well}'
-									),
-			template: 'main'},
-			//problem 4
-			{ problemStatement: 'Find the difference of <span class=bigger>-</span>25 and 10', buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>25 <span class=bigger>-</span> 10', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>10 <span class=bigger>-</span> 25', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>25 <span class=bigger>-</span> (<span class=bigger>-</span>10)', answer: '-35',
-            labelCorrect: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{<span class=bigger>-</span>25}}{\\btn{-}{T}{bigger}}{\\html{10}},' 
-                        					+'\\input{-35},' 
-                    					+'}{Enter an operator.}{Evaluate the operator.}'
-									+'}{well}'
-									),
-			template: 'main'},
-			//problem 5
-			{ problemStatement: 'Find the difference of <span class=bigger>-</span>25 and 10', buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>25 <span class=bigger>-</span> 10', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>10 <span class=bigger>-</span> 25', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>25 <span class=bigger>-</span> (<span class=bigger>-</span>10)', answer: '-35',
-            labelCorrect: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{<span class=bigger>-</span>25}}{\\btn{-}{T}{bigger}}{\\html{10}},' 
-                        					+'\\input{-35},' 
-                    					+'}{Enter an operator.}{Evaluate the operator.}'
-									+'}{well}'
-									),
-			template: 'main'},
-			//problem 6
-			{ problemStatement: 'Find the difference of <span class=bigger>-</span>25 and 10', buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>25 <span class=bigger>-</span> 10', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>10 <span class=bigger>-</span> 25', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>25 <span class=bigger>-</span> (<span class=bigger>-</span>10)', answer: '-35',
-            labelCorrect: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{<span class=bigger>-</span>25}}{\\btn{-}{T}{bigger}}{\\html{10}},' 
-                        					+'\\input{-35},' 
-                    					+'}{Enter an operator.}{Evaluate the operator.}'
-									+'}{well}'
-									),
-			template: 'main'},
-			//problem 7
-			{ problemStatement: 'Find the difference of <span class=bigger>-</span>25 and 10', buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>25 <span class=bigger>-</span> 10', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>10 <span class=bigger>-</span> 25', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>25 <span class=bigger>-</span> (<span class=bigger>-</span>10)', answer: '-35',
-            labelCorrect: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{<span class=bigger>-</span>25}}{\\btn{-}{T}{bigger}}{\\html{10}},' 
-                        					+'\\input{-35},' 
-                    					+'}{Enter an operator.}{Evaluate the operator.}'
-									+'}{well}'
-									),
-			template: 'main'},
-			//problem 8
-			{ problemStatement: 'Find the difference of <span class=bigger>-</span>25 and 10', buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>25 <span class=bigger>-</span> 10', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>10 <span class=bigger>-</span> 25', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>25 <span class=bigger>-</span> (<span class=bigger>-</span>10)', answer: '-35',
-            labelCorrect: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{<span class=bigger>-</span>25}}{\\btn{-}{T}{bigger}}{\\html{10}},' 
-                        					+'\\input{-35},' 
-                    					+'}{Enter an operator.}{Evaluate the operator.}'
-									+'}{well}'
-									),
-			template: 'main'},
-			//problem 9
-			{ problemStatement: 'Find the difference of <span class=bigger>-</span>25 and 10', buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>25 <span class=bigger>-</span> 10', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>10 <span class=bigger>-</span> 25', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>25 <span class=bigger>-</span> (<span class=bigger>-</span>10)', answer: '-35',
-            labelCorrect: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{<span class=bigger>-</span>25}}{\\btn{-}{T}{bigger}}{\\html{10}},' 
-                        					+'\\input{-35},' 
-                    					+'}{Enter an operator.}{Evaluate the operator.}'
-									+'}{well}'
-									),
-			template: 'main'},
-			//problem 10
-			{ problemStatement: 'Find the difference of <span class=bigger>-</span>25 and 10', buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>25 <span class=bigger>-</span> 10', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>10 <span class=bigger>-</span> 25', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>25 <span class=bigger>-</span> (<span class=bigger>-</span>10)', answer: '-35',
-            labelCorrect: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10',
-			preAnswer: dataUtils.pre('\\css'
-									 	+'{\\ordopssimple{' 
-											+'\\grp{\\html{<span class=bigger>-</span>25}}{\\btn{-}{T}{bigger}}{\\html{10}},' 
-                        					+'\\input{-35},' 
-                    					+'}{Enter an operator.}{Evaluate the operator.}'
-									+'}{well}'
-									),
-			template: 'main'},
+			template: 'main'}
         ];
 
 	return dataUtils.build(desc, template, data);
