7befc20e31acf9f9f97c2b19f81b6632d514f4ab
markafoote <mark@getthatwebsite.com>
2014-04-03 16:36:56 -0700

    k.3, corrections per Matt

--- app/data372/data_k_3.js
+++ app/data372/data_k_3.js
@@ -33,11 +33,13 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
 										'$problemStatement<br><br>'+
                                         'Choose the expression that is rewritten correctly without any double negatives:'+
                                     '}',
-						answer: '\\rowgrp'+
-                                    '{\\grp{\\btn{A}{$buttonValueA}{marg}}{\\html{\xA0$buttonLabelA}}}'+
-                                    '{\\grp{\\btn{B}{$buttonValueB}{marg}}{\\html{\xA0$buttonLabelB}}}'+
-                                    '{\\grp{\\btn{C}{$buttonValueC}{marg}}{\\html{\xA0$buttonLabelC}}}'+
-                                    '{\\grp{\\btn{D}{$buttonValueD}{marg}}{\\html{\xA0$buttonLabelD}}}',
+						answer: '\\btngrp'+
+									'{\\rowgrp'+
+										'{\\grp{\\btn{A}{$buttonValueA}{marg}}{\\html{\xA0$buttonLabelA}}}'+
+										'{\\grp{\\btn{B}{$buttonValueB}{marg}}{\\html{\xA0$buttonLabelB}}}'+
+										'{\\grp{\\btn{C}{$buttonValueC}{marg}}{\\html{\xA0$buttonLabelC}}}'+
+										'{\\grp{\\btn{D}{$buttonValueD}{marg}}{\\html{\xA0$buttonLabelD}}}'+
+									'}',
 						controls: {
 							"checkAnswer": true,
 						    "help": '\\rowgrp'
@@ -51,8 +53,7 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
                         }
 					}, {
 						problem: '\\html{'+
-                                        'Translate the following words into a math expression:'+
-                                        '<br><br>$problemStatement'+
+                                        '$problemStatement'+
                                     '}',
 						answer: '\\wb'
 									+'{\\rowgrp'
@@ -79,11 +80,13 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
 					title: 'Main Answer',
 					children: [{
 						problem: '\\html{$problemStatement}',
-						answer: '\\rowgrp'+
-                                    '{\\grp{\\btn{A}{$buttonValueA}{marg}}{\\html{\xA0$buttonLabelA}}}'+
-                                    '{\\grp{\\btn{B}{$buttonValueB}{marg}}{\\html{\xA0$buttonLabelB}}}'+
-                                    '{\\grp{\\btn{C}{$buttonValueC}{marg}}{\\html{\xA0$buttonLabelC}}}'+
-                                    '{\\grp{\\btn{D}{$buttonValueD}{marg}}{\\html{\xA0$buttonLabelD}}}',
+						answer: '\\btngrp'+
+									'{\\rowgrp'+
+										'{\\grp{\\btn{A}{$buttonValueA}{marg}}{\\html{\xA0$buttonLabelA}}}'+
+										'{\\grp{\\btn{B}{$buttonValueB}{marg}}{\\html{\xA0$buttonLabelB}}}'+
+										'{\\grp{\\btn{C}{$buttonValueC}{marg}}{\\html{\xA0$buttonLabelC}}}'+
+										'{\\grp{\\btn{D}{$buttonValueD}{marg}}{\\html{\xA0$buttonLabelD}}}'+
+									'}',
 						controls: {
 							"checkAnswer": true,
 						    "help": "\\html{$helpStatement}"
@@ -116,29 +119,32 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
                                         'Choose the correct math translation for the expression:'+
                                         '<br><br>$problemStatement'+
                                     '}',
-						answer: '\\rowgrp'
-                                    +'{\\grp{\\btn{A}{$buttonValueA}{marg}}{\\html{\xA0$buttonLabelA}}}'
-                                    +'{\\grp{\\btn{B}{$buttonValueB}{marg}}{\\html{\xA0$buttonLabelB}}}'
-                                    +'{\\grp{\\btn{C}{$buttonValueC}{marg}}{\\html{\xA0$buttonLabelC}}}'
-                                    +'{\\grp{\\btn{D}{$buttonValueD}{marg}}{\\html{\xA0$buttonLabelD}}}',
+						answer: '\\btngrp'+
+									'{\\rowgrp'+
+										'{\\grp{\\btn{A}{$buttonValueA}{marg}}{\\html{\xA0$buttonLabelA}}}'+
+										'{\\grp{\\btn{B}{$buttonValueB}{marg}}{\\html{\xA0$buttonLabelB}}}'+
+										'{\\grp{\\btn{C}{$buttonValueC}{marg}}{\\html{\xA0$buttonLabelC}}}'+
+										'{\\grp{\\btn{D}{$buttonValueD}{marg}}{\\html{\xA0$buttonLabelD}}}'+
+									'}',
 						controls: {
 							"checkAnswer": true,
 						    "help": "\\html{$helpStatement}"
                         }
 					}, {
 						problem: '\\html{'+
-                                        'Translate the following words into a math expression:'+
-                                        '<br><br>$problemStatement'+
+                                        '$problemStatement'+
                                     '}',
 						answer: '\\css'
-									+'{\\rowgrp'
-										+'{\\html{$labelCorrect}}'
-										+'{\\html{&nbsp;}}'
-										+'{\\ins{What is the expression without double negatives?}}'
-										+'{\\grp{\\btn{A}{$buttonValueE}{marg}}{\\html{\xA0$buttonLabelE}}}'
-										+'{\\grp{\\btn{B}{$buttonValueF}{marg}}{\\html{\xA0$buttonLabelF}}}'
-										+'{\\grp{\\btn{C}{$buttonValueG}{marg}}{\\html{\xA0$buttonLabelG}}}'
-										+'{\\grp{\\btn{D}{$buttonValueH}{marg}}{\\html{\xA0$buttonLabelH}}}'
+									+'{\\btngrp'
+										+'{\\rowgrp'
+											+'{\\html{$labelCorrect}}'
+											+'{\\html{&nbsp;}}'
+											+'{\\ins{What is the expression without double negatives?}}'
+											+'{\\grp{\\btn{A}{$buttonValueE}{marg}}{\\html{\xA0$buttonLabelE}}}'
+											+'{\\grp{\\btn{B}{$buttonValueF}{marg}}{\\html{\xA0$buttonLabelF}}}'
+											+'{\\grp{\\btn{C}{$buttonValueG}{marg}}{\\html{\xA0$buttonLabelG}}}'
+											+'{\\grp{\\btn{D}{$buttonValueH}{marg}}{\\html{\xA0$buttonLabelH}}}'
+										+'}'
 									+'}'
 								+'{well}',
 						controls: {
@@ -211,7 +217,7 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
 			secondHelpText: '&nbsp;',
 			template: 'rewriteDblNegSolve'},
 			//problem 5
-			{ problemStatement: 'Choose the correct math translation for the expression:<br><br>Subtract 6 from 8',
+			{ problemStatement: 'Subtract 6 from 8',
 			  helpStatement: 'In the phrase \\"subtract from\\", the value that comes <span style=\\"text-decoration:underline;\\">after</span> the word \\"from\\" is what we started with.  Here we started with:<br><br>8<br><br>We are then told to \\"subtract 6\\".  This is:<br><br><span class=bigger>-</span>6<br><br>The translated expression is:<br><br><span class=blue-text>8 <span class=bigger>-</span> 6</span>',
 			  buttonValueA: 'F', buttonLabelA: '<span class=bigger>-</span>6 <span class=bigger>-</span> 8', buttonValueB: 'T', buttonLabelB: '<span class=bigger>&nbsp;</span>8 <span class=bigger>-</span> 6', buttonValueC: 'F', buttonLabelC: '<span class=bigger>-</span>8 <span class=bigger>-</span> 6', buttonValueD: 'F', buttonLabelD: '<span class=bigger>&nbsp;</span>6 <span class=bigger>-</span> 8',
 			answer: '2',
@@ -228,20 +234,20 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
 			absNumber: '12',
 			template: 'rewriteSolveDblNeg'},
 			//problem 7
-			{ problemStatement: 'Choose the correct math translation for the expression:<br><br>Find the sum of 6 and <span class=bigger>-</span>14',
+			{ problemStatement: 'Find the sum of 6 and <span class=bigger>-</span>14',
 			  helpStatement: 'The word \\"sum\\" tells us that the symbol connecting the number values is:<br><br><span class=bigger>+</span><br><br>Insert this between the two given values of 6 and <span class=bigger>-</span>14.<br><br>The translated expression is:<br><br><span class=blue-text>6 <span class=bigger>+</span> ( <span class=bigger>-</span>14 )</span><br><br>The second value ( <span class=bigger>-</span>14 ) is often put in a parenthesis when there are two symbols (in this case, the <span class=bigger>+</span> and the <span class=bigger>-</span>) that affect the same number value.  The expression:<br><br>6 <span class=bigger>+</span>( <span class=bigger>-</span>14 )<br><br>is the same as:<br><br>6 <span class=bigger>+</span> <span class=bigger>-</span>14<br><br>and:<br><br>6<span class=bigger>-</span>14',
 			  buttonValueA: 'F', buttonLabelA: '<span class=bigger>-</span> 6 <span class=bigger>+</span> ( <span class=bigger>-</span>14 )', buttonValueB: 'T', buttonLabelB: '<span class=bigger>&nbsp;</span> 6 <span class=bigger>+</span> ( <span class=bigger>-</span>14 )', buttonValueC: 'F', buttonLabelC: '<span class=margin-narrow>&nbsp;</span>14 <span class=bigger>-</span> ( <span class=bigger>-</span>6 )', buttonValueD: 'F', buttonLabelD: '<span class=bigger>&nbsp;</span> 6 <span class=bigger>+</span> ( 14 )',
 			answer: '-8',
             labelCorrect: '6 <span class=bigger>+</span> ( <span class=bigger>-</span>14 )',
 			template: 'rewriteSolve'},
 			//problem 8
-			{ problemStatement: 'Choose the correct math translation for the expression:<br><br>Find the difference of <span class=bigger>-</span>25 and 10',
+			{ problemStatement: 'Find the difference of <span class=bigger>-</span>25 and 10',
 			  helpStatement: 'The word \\"difference\\" tells us that the symbol connecting the number values is:<br><br><span class=bigger>-</span><br><br>Insert this between the two given values of <span class=bigger>-</span>25 and 10.<br><br>The translated expression is:<br><br><span class=blue-text><span class=bigger>-</span>25 <span class=bigger>-</span> 10</span>',
 			  buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>25 <span class=bigger>-</span> 10', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>10 <span class=bigger>-</span> 25', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10', buttonValueD: 'F', buttonLabelD: '<span class=bigger>-</span>25 <span class=bigger>-</span> (<span class=bigger>-</span>10)', answer: '-35',
             labelCorrect: '<span class=bigger>-</span>25 <span class=bigger>-</span> 10',
 			template: 'rewriteSolve'},
 			//problem 9
-			{ problemStatement: 'Choose the correct math translation for the expression:<br><br>Find the difference of 4 and 9',
+			{ problemStatement: 'Find the difference of 4 and 9',
 			  helpStatement: 'The word \\"difference\\" tells us that the symbol connecting the number values is:<br><br><span class=bigger>-</span><br><br>Insert this between the two given values of 4 and 9.<br><br>The translated expression is:<br><br><span class=blue-text>4 <span class=bigger>-</span> 9</span>',
 			  buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>4 <span class=bigger>-</span> ( <span class=bigger>-</span>9 )', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>9 <span class=bigger>-</span> 4', buttonValueC: 'F', buttonLabelC: '<span class=bigger>&nbsp;</span>9 <span class=bigger>-</span> ( <span class=bigger>-</span>4 )', buttonValueD: 'T', buttonLabelD: '<span class=bigger>&nbsp;</span>4 <span class=bigger>-</span> 9', answer: '-5',
             labelCorrect: '4 <span class=bigger>-</span> 9',
@@ -257,7 +263,7 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
 			absNumber: '5',
 			template: 'rewriteSolveDblNeg'},
 			//problem 11
-			{ problemStatement: 'Choose the correct math translation for the expression:<br><br>Find the sum of -0.5 and -3.8',
+			{ problemStatement: 'Find the sum of -0.5 and -3.8',
 			  helpStatement: 'The word \\"sum\\" tells us that the symbol connecting the number values is:<br><br><span class=bigger>+</span><br><br>Insert this between the two given values of <span class=bigger>-</span>0.5 and <span class=bigger>-</span>3.8.<br><br>The translated expression is:<br><br><span class=blue-text><span class=bigger>-</span>0.5 <span class=bigger>+</span> ( <span class=bigger>-</span>3.8 )</span><br><br>The second value ( <span class=bigger>-</span>3.8 ) is often put in a parenthesis when there are two symbols (in this case, the <span class=bigger>+</span> and the <span class=bigger>-</span>) that affect the same number value.  The expression:<br><br><span class=bigger>-</span>0.5 <span class=bigger>+</span>( <span class=bigger>-</span>3.8 )<br><br>is the same as:<br><br><span class=bigger>-</span>0.5 <span class=bigger>+</span> <span class=bigger>-</span>3.8<br><br>and:<br><br><span class=bigger>-</span>0.5 <span class=bigger>-</span>3.8',
 			  buttonValueA: 'T', buttonLabelA: '<span class=bigger>-</span>0.5 <span class=bigger>+</span> ( <span class=bigger>-</span>3.8 )', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>0.5 <span class=bigger>+</span> ( <span class=bigger>-</span>3.8 )', buttonValueC: 'F', buttonLabelC: '<span class=bigger>-</span>3.8 <span class=bigger>-</span> ( <span class=bigger>-</span>0.5 )', buttonValueD: 'F', buttonLabelD: '<span class=bigger>&nbsp;</span>3.8 <span class=bigger>+</span> ( 0.5 )', answer: '-4.3',
             labelCorrect: '<span class=bigger>-</span>0.5 <span class=bigger>+</span> ( <span class=bigger>-</span>3.8 )',
@@ -273,7 +279,7 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
 			absNumber: '12.1',
 			template: 'rewriteSolveDblNeg'},
 			//problem 13
-			{ problemStatement: 'Choose the correct math translation for the expression:<br><br>Subtract 22.4 from 17.3',
+			{ problemStatement: 'Subtract 22.4 from 17.3',
 			  helpStatement: 'In the phrase \\"subtract from\\", the value that comes <span style=\\"text-decoration:underline;\\">after</span> the word \\"from\\" is what we started with.  Here we started with:<br><br>17.3<br><br>We are then told to \\"subtract 22.4\\".  This is:<br><br><span class=bigger>-</span>22.4<br><br>The translated expression is:<br><br><span class=blue-text>17.3 <span class=bigger>-</span> 22.4</span>',
 			  buttonValueA: 'F', buttonLabelA: '<span class=bigger>-</span>22.4 <span class=bigger>-</span> 17.3', buttonValueB: 'F', buttonLabelB: '<span class=bigger>&nbsp;</span>22.4 <span class=bigger>-</span> 17.3', buttonValueC: 'T', buttonLabelC: '<span class=bigger>&nbsp;</span>17.3 <span class=bigger>-</span> 22.4', buttonValueD: 'F', buttonLabelD: '<span class=bigger>&nbsp;</span>17.3 <span class=bigger>-</span> ( <span class=bigger>-</span>22.4 )', answer: '-5.1',
             labelCorrect: '17.3 <span class=bigger>-</span> 22.4',
@@ -304,7 +310,7 @@ angular.module('mathSkills').service('data_k_3', ['dataUtils', function (dataUti
 			template: 'main'},
 			//problem 18
 			{ problemStatement: 'Choose the expression that is rewritten correctly without any double negatives:<br><br><span class=bigger>-</span>( <span class=bigger>-</span>( <span class=bigger>-</span>5.2 ) ) <span class=bigger>-</span> ( <span class=bigger>-</span>3.1 ) <span class=bigger>+</span> ( <span class=bigger>-</span>4 )',
-			  helpStatement: 'tba',
+			  helpStatement: '<span class=bigger>-</span>( <span class=bigger>-</span>( <span class=bigger>-</span>5.2 ) ) <span class=bigger>-</span> ( <span class=bigger>-</span>3.1 ) <span class=bigger>+</span> ( <span class=bigger>-</span>4 )<br><br>The minus sign in front of a parenthesis means to take the opposite sign of the negative number inside the parenthesis, in this case the opposite sign of the negative number 5.2 and the opposite sign of the negative number 3.1.<br><br>Our expression becomes:<br><br><span class=bigger>-</span> 5.2 <span class=bigger>+</span> 3.1 <span class=bigger>-</span> 4',
 			  buttonValueA: 'F', buttonLabelA: '<span class=bigger>&nbsp;</span>5.2 <span class=bigger>+</span> 3.1 <span class=bigger>-</span> 4', buttonValueB: 'F', buttonLabelB: '<span class=bigger>-</span>5.2 <span class=bigger>-</span> 3.1 <span class=bigger>-</span> 4', buttonValueC: 'T', buttonLabelC: '<span class=bigger>-</span>5.2 <span class=bigger>+</span> 3.1 <span class=bigger>-</span> 4', buttonValueD: 'F', buttonLabelD: '<span class=bigger>&nbsp;</span>5.2 <span class=bigger>+</span> 3.1 <span class=bigger>-</span> 4', answer: '-6.1',
             labelCorrect: '<span class=bigger>-</span>5.2 <span class=bigger>+</span> 3.1 <span class=bigger>-</span> 4',
 			template: 'rewriteSolve'}
