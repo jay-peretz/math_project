73edab9f813a044640353f59f71676b418fc5445
markafoote <mark@getthatwebsite.com>
2014-04-17 22:26:14 -0700

    s.1, problem 1, start

--- app/data372/data_s_1.js
+++ app/data372/data_s_1.js
@@ -1,9 +1,59 @@
 /*global angular */
 
-angular.module('mathSkills').value('data_s_1', {
-    title: 'S.1 Perimeter of Rectangles',
-    path: 'S.1-perimeter-of-rectangles',
-    children: [{
-        title: 'Perimeter of Rectangles',
-        children: [] }]
-});
+angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUtils) {
+    var desc = {
+			title: 'S.1 Perimeter of Rectangles',
+    		path: 's.1-perimeter-of-rectangles',
+            children: []
+        },
+        template = {
+            main: {
+                title: 'Perimeter of Rectangles',
+                children: [{
+                    title: 'Main Answer',
+                    children: [{
+						problem: '\\image{$illustration}',
+                        answer: '\\grp'
+									+'{\\input{$finalAnswer}}'
+									+'{\\html{$unitsAnswer}}',
+                        controls: {
+                            "checkAnswer": true,
+                            "help": '\\rowgrp'
+										+'{\\html{Identify the shape:  $identifyShape}}'
+										+'{\\html{&nbsp;}}'
+										+'{\\html{Write the formula: $formula}}'
+										+'{\\html{Substitution: $substitution}}'
+										+'{\\html{Solve: $lineOneSolve}}'
+										+'{\\html{<span style="padding-left:47px;">$lineTwoSolve</span>}}'
+										+'{\\html{&nbsp;}}'
+										+'{\\css'
+											+'{\\grp'
+												+'{\\html{Answer: &nbsp;}}'
+												+'{\\html{$helpAnswer}}'
+												+'{\\html{$unitsAnswer}}'
+											+'}'
+											+'{help-answer-text}'
+										+'}'
+                        }
+                    }]
+                }]
+            }
+        },
+        data = [
+			{ // problem 1
+				illustration: 'S.1/rectangle7x11.jpg',
+				unitsAnswer: 'ft',
+				unitsSelect: '[\\"ft\\",\\"ft<sup>2</sup>\\",\\"ft<sup>3</sup>\\"]',
+				identifyShape: 'rectangle',
+                formula: 'P = 2(L) + 2(W)',
+				substitution: 'P = 2(11) + 2(7)',
+				lineOneSolve: 'P = 22 + 14',
+				lineTwoSolve: 'P = 36',
+				finalAnswer: '36',
+				helpAnswer: '36',
+				template: 'main' 
+			}
+        ];
+
+    return dataUtils.build(desc, template, data);
+}]);
