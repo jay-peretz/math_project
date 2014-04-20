e12bfb171bec9f15c3770beef4216773e6af528b
markafoote <mark@getthatwebsite.com>
2014-04-19 18:11:13 -0700

    s.1 complete except for illustrations

--- app/data372/data_s_1.js
+++ app/data372/data_s_1.js
@@ -14,7 +14,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
                     children: [{
 						problem: '\\image{$illustration}',
                         answer: '\\grp'
-									+'{\\input{$finalAnswer}}'
+									+'{$finalAnswer}'
 									+'{\\select{$unitsPower}{$unitsSelect}{widthFixed120}}',
                         controls: {
                             "checkAnswer": true,
@@ -44,7 +44,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
                     children: [{
 						problem: '\\image{$illustration}',
                         answer: '\\grp'
-									+'{\\input{$finalAnswer}}'
+									+'{$finalAnswer}'
 									+'{\\select{$unitsPower}{$unitsSelect}{widthFixed120}}',
                         controls: {
                             "checkAnswer": true,
@@ -75,7 +75,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
                     children: [{
 						problem: '\\image{$illustration}',
                         answer: '\\grp'
-									+'{\\input{$finalAnswer}}'
+									+'{$finalAnswer}'
 									+'{\\select{$unitsPower}{$unitsSelect}{widthFixed120}}',
                         controls: {
                             "checkAnswer": true,
@@ -112,7 +112,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
 				substitution: 'P = 2(7) + 2(11)',
 				lineOneSolve: 'P = 14 + 22',
 				lineTwoSolve: 'P = 36',
-				finalAnswer: '36',
+				finalAnswer: dataUtils.pre('\\input{36}'),
 				helpAnswer: '36',
 				template: 'twoStepSolution' 
 			}
@@ -127,7 +127,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
 				substitution: 'P = 2(22) + 2(20)',
 				lineOneSolve: 'P = 44 + 40',
 				lineTwoSolve: 'P = 84',
-				finalAnswer: '84',
+				finalAnswer: dataUtils.pre('\\input{84}'),
 				helpAnswer: '84',
 				template: 'twoStepSolution' 
 			}
@@ -142,7 +142,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
 				substitution: 'P = 2(5.4) + 2(6.25)',
 				lineOneSolve: 'P = 10.8 + 12.5',
 				lineTwoSolve: 'P = 23.3',
-				finalAnswer: '23.3',
+				finalAnswer: dataUtils.pre('\\input{23.3}'),
 				helpAnswer: '23.3',
 				template: 'twoStepSolution' 
 			}
@@ -157,7 +157,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
 				substitution: 'P = 2(10.8) + 2(12.5)',
 				lineOneSolve: 'P = 21.6 + 25',
 				lineTwoSolve: 'P = 46.6',
-				finalAnswer: '46.6',
+				finalAnswer: dataUtils.pre('\\input{46.6}'),
 				helpAnswer: '46.6',
 				template: 'twoStepSolution' 
 			}
@@ -171,7 +171,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
                 formula: 'P = 4(S)',
 				substitution: 'P = 4(14)',
 				lineOneSolve: 'P = 56',
-				finalAnswer: '56',
+				finalAnswer: dataUtils.pre('\\input{56}'),
 				helpAnswer: '56',
 				template: 'oneStepSolution' 
 			}
@@ -185,7 +185,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
                 formula: 'P = 4(S)',
 				substitution: 'P = 4(32)',
 				lineOneSolve: 'P = 128',
-				finalAnswer: '128',
+				finalAnswer: dataUtils.pre('\\input{128}'),
 				helpAnswer: '128',
 				template: 'oneStepSolution' 
 			}
@@ -201,7 +201,7 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
 				lineOneSolve: 'P = ( <sup>2</sup>&#8260;<sub>1</sub> &bull; <sup>43</sup>&#8260;<sub>5</sub> ) + ( <sup>2</sup>&#8260;<sub>1</sub> &bull; <sup>25</sup>&#8260;<sub>2</sub> )',
 				lineTwoSolve: 'P = ( 17 <sup>1</sup>&#8260;<sub>5</sub>  ) + ( 25 )',
 				lineThreeSolve: 'P = 42 <sup>1</sup>&#8260;<sub>5</sub>',
-				finalAnswer: '42 <sup>1</sup>&#8260;<sub>5</sub>',
+				finalAnswer: dataUtils.pre('\\mixed{\\input{42}}{\\frac{\\input{1}}{\\input{5}}}'),
 				helpAnswer: '42 <sup>1</sup>&#8260;<sub>5</sub>',
 				template: 'threeStepSolution' 
 			}
@@ -217,11 +217,134 @@ angular.module('mathSkills').service('data_s_1', ['dataUtils', function (dataUti
 				lineOneSolve: 'P = ( <sup>2</sup>&#8260;<sub>1</sub> &bull; <sup>37</sup>&#8260;<sub>2</sub> ) + ( <sup>2</sup>&#8260;<sub>1</sub> &bull; <sup>16</sup>&#8260;<sub>3</sub> )',
 				lineTwoSolve: 'P = ( 37 ) + ( 10 <sup>2</sup>&#8260;<sub>3</sub>)',
 				lineThreeSolve: 'P = 47 <sup>2</sup>&#8260;<sub>3</sub>',
-				finalAnswer: '47 <sup>2</sup>&#8260;<sub>3</sub>',
+				finalAnswer: dataUtils.pre('\\mixed{\\input{47}}{\\frac{\\input{2}}{\\input{3}}}'),
 				helpAnswer: '47 <sup>2</sup>&#8260;<sub>3</sub>',
 				template: 'threeStepSolution' 
 			}
+			,
+			{ // problem 9
+				illustration: '',
+				unitsAnswer: 'in',
+				unitsPower: 'in',
+				unitsSelect: '[\\"in\\",\\"in&sup2;\\",\\"in&sup3\\"]',
+				identifyShape: 'triangle',
+                formula: 'P = s<sub>1</sub>; + s<sub>2</sub>; + s<sub>3</sub>',
+				substitution: 'P = 9 + 12 + 15',
+				lineOneSolve: 'P = 36',
+				finalAnswer: dataUtils.pre('\\input{36}'),
+				helpAnswer: '36',
+				template: 'oneStepSolution' 
+			}
+			,
+			{ // problem 10
+				illustration: '',
+				unitsAnswer: 'ft',
+				unitsPower: 'ft',
+				unitsSelect: '[\\"ft\\",\\"ft&sup2;\\",\\"ft&sup3\\"]',
+				identifyShape: 'triangle',
+                formula: 'P = s<sub>1</sub>; + s<sub>2</sub>; + s<sub>3</sub>',
+				substitution: 'P = 17 + 24 + 22',
+				lineOneSolve: 'P = 63',
+				finalAnswer: dataUtils.pre('\\input{63}'),
+				helpAnswer: '63',
+				template: 'oneStepSolution' 
+			}
+			,
+			{ // problem 11
+				illustration: '',
+				unitsAnswer: 'cm',
+				unitsPower: 'cm',
+				unitsSelect: '[\\"cm\\",\\"cm&sup2;\\",\\"cm&sup3\\"]',
+				identifyShape: 'triangle',
+                formula: 'P = s<sub>1</sub>; + s<sub>2</sub>; + s<sub>3</sub>',
+				substitution: 'P = 8 + 8 + 20',
+				lineOneSolve: 'P = 36',
+				finalAnswer: dataUtils.pre('\\input{36}'),
+				helpAnswer: '36',
+				template: 'oneStepSolution' 
+			}
+			,
+			{ // problem 12
+				illustration: '',
+				unitsAnswer: 'm',
+				unitsPower: 'm',
+				unitsSelect: '[\\"m\\",\\"m&sup2;\\",\\"m&sup3\\"]',
+				identifyShape: 'triangle',
+                formula: 'P = s<sub>1</sub>; + s<sub>2</sub>; + s<sub>3</sub>',
+				substitution: 'P = 4.8 + 3 + 3',
+				lineOneSolve: 'P = 10.8',
+				finalAnswer: dataUtils.pre('\\input{10.8}'),
+				helpAnswer: '10.8',
+				template: 'oneStepSolution' 
+			}
+			,
+			{ // problem 13
+				illustration: '',
+				unitsAnswer: 'in',
+				unitsPower: 'in',
+				unitsSelect: '[\\"in\\",\\"in&sup2;\\",\\"in&sup3\\"]',
+				identifyShape: 'triangle',
+                formula: 'P = s<sub>1</sub>; + s<sub>2</sub>; + s<sub>3</sub>',
+				substitution: 'P = 18.34 + 20.49 + 23.5',
+				lineOneSolve: 'P = 62.33',
+				finalAnswer: dataUtils.pre('\\input{62.33}'),
+				helpAnswer: '62.33',
+				template: 'oneStepSolution' 
+			}
+			,
+			{ // problem 14
+				illustration: '',
+				unitsAnswer: 'in',
+				unitsPower: 'in',
+				unitsSelect: '[\\"in\\",\\"in&sup2;\\",\\"in&sup3\\"]',
+				identifyShape: 'triangle',
+                formula: 'P = s<sub>1</sub>; + s<sub>2</sub>; + s<sub>3</sub>',
+				substitution: 'P = 9.17 + 10.245 + 11.75',
+				lineOneSolve: 'P = 31.165',
+				finalAnswer: '31.165',
+				finalAnswer: dataUtils.pre('\\input{31.165}'),
+				helpAnswer: '31.165',
+				template: 'oneStepSolution' 
+			}
+			,
+			{ // problem 15
+				illustration: '',
+				unitsAnswer: 'in',
+				unitsPower: 'in',
+				unitsSelect: '[\\"in\\",\\"in&sup2;\\",\\"in&sup3\\"]',
+				identifyShape: 'triangle',
+                formula: 'P = s<sub>1</sub>; + s<sub>2</sub>; + s<sub>3</sub>',
+				substitution: 'P = 7 <sup>1</sup>&#8260;<sub>8</sub> + 11 <sup>1</sup>&#8260;<sub>2</sub> + 7 <sup>3</sup>&#8260;<sub>4</sub>',
+				lineOneSolve: 'P = 26 <sup>3</sup>&#8260;<sub>8</sub>',
+				finalAnswer: dataUtils.pre('\\mixed{\\input{26}}{\\frac{\\input{3}}{\\input{8}}}'),
+				helpAnswer: '26 <sup>3</sup>&#8260;<sub>8</sub>',
+				template: 'oneStepSolution' 
+			}
+			,
+			{ // problem 16
+				illustration: '',
+				unitsAnswer: 'ft',
+				unitsPower: 'ft',
+				unitsSelect: '[\\"ft\\",\\"ft&sup2;\\",\\"ft&sup3\\"]',
+				identifyShape: 'triangle',
+                formula: 'P = s<sub>1</sub>; + s<sub>2</sub>; + s<sub>3</sub>',
+				substitution: 'P = 9 <sup>3</sup>&#8260;<sub>5</sub> + 7 <sup>1</sup>&#8260;<sub>5</sub> + 12',
+				lineOneSolve: 'P = 28 <sup>4</sup>&#8260;<sub>5</sub>',
+				finalAnswer: dataUtils.pre('\\mixed{\\input{28}}{\\frac{\\input{4}}{\\input{5}}}'),
+				helpAnswer: '28 <sup>4</sup>&#8260;<sub>5</sub>',
+				template: 'oneStepSolution' 
+			}
         ];
+	
+	/*shuffle = [
+				{ problems: [1,2,3,4,5,6,7,8,9,10], total: 8},
+				{ problems: [11,12], total: 1},
+				{ problems: [13,14,15,16], total: 1}
+		];
+
+    
+    	return dataUtils.build(desc, template, data, shuffle);*/
 
     return dataUtils.build(desc, template, data);
+	
 }]);
