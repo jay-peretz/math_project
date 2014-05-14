91c2f8a78cea4e6ed215702bdded7f721b529499
markafoote <mark@getthatwebsite.com>
2014-05-14 14:43:47 -0700

    add = to operators that are enlarged

--- app/js/filters/enlarge-ops.js
+++ app/js/filters/enlarge-ops.js
@@ -1,15 +1,16 @@
-/*global angular */
+ /*global angular */
 
 angular.module('mathSkills').filter('enlargeOps', function () {
     return function (input) {
         return typeof input === 'string' ?
-            input.replace(/[\*\/\+\-\x]/g, function (char) {
+            input.replace(/[\*\/\+\-\x\=]/g, function (char) {
                 switch (char) {
                     case '*': return '<span class=bigger>&bull;</span>';
 					case 'x': return '<span class=bigger>&bull;</span>';
                     case '/': return '<span class=bigger>&divide;</span>';
 					case '+': return '<span class=bigger>&#43;</span>';
 					case '-': return '<span class=bigger>&#45;</span>';
+					case '=': return '<span class=bigger>=</span>';
                 }
             }) :
             input;
