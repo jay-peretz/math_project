93414fcd61524aad939e22eb7f4b221098876dca
markafoote <mark@getthatwebsite.com>
2014-05-22 22:21:46 -0700

    remove class=bigger from &bull; conversions

--- app/js/filters/enlarge-ops.js
+++ app/js/filters/enlarge-ops.js
@@ -6,8 +6,8 @@ angular.module('mathSkills').filter('enlargeOps', function () {
         return typeof input === 'string' ?
             input.replace(/[\*\/\+\-\=]/g, function (char) {
                 switch (char) {
-                    case '*': return '<span class=bigger>&bull;</span>';
-					case 'x': return '<span class=bigger>&bull;</span>';
+                    case '*': return '<span>&bull;</span>';
+					case 'x': return '<span>&bull;</span>';
                     case '/': return '<span class=bigger>&divide;</span>';
 					case '+': return '<span class=bigger>&#43;</span>';
 					case '-': return '<span class=bigger>&#45;</span>';
